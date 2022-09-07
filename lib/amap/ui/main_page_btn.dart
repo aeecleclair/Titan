d9193009d7625809487b9828f7e0d75f4fa2425c
Maxime ROUCHER <34400034+maximeroucher@users.noreply.github.com>
2022-09-07 10:40:12 +0200

    Fixing amap admin top bar button

--- lib/amap/ui/main_page_btn.dart
+++ lib/amap/ui/main_page_btn.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:myecl/amap/providers/amap_page_provider.dart';
+import 'package:myecl/amap/providers/scroll_controller_provider.dart';
 import 'package:myecl/amap/tools/constants.dart';
 import 'package:myecl/amap/ui/top_button.dart';
 import 'package:myecl/amap/providers/is_amap_admin_provider.dart';
@@ -13,6 +16,10 @@ class MainPageBtn extends HookConsumerWidget {
     final page = ref.watch(amapPageProvider);
     final pageNotifier = ref.watch(amapPageProvider.notifier);
     final isAdmin = ref.watch(isAmapAdminProvider);
+    final hideAnimation = useAnimationController(
+        duration: const Duration(milliseconds: 200), initialValue: 1);
+
+    final scrollController = ref.watch(scrollControllerProvider(hideAnimation));
     List<Widget> btns = [
       TopButton(
           text: AMAPTextConstants.myOrders,
@@ -36,15 +43,80 @@ class MainPageBtn extends HookConsumerWidget {
             pageNotifier.setAmapPage(AmapPage.admin);
           }));
     }
-    return Container(
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-        height: 100,
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: btns,
-          ),
-        ));
+    return isAdmin
+        ? Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            height: 100,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: btns,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 35,
+                  right: -5,
+                  child: FadeTransition(
+                      opacity: hideAnimation,
+                      child: ScaleTransition(
+                        scale: hideAnimation,
+                        child: GestureDetector(
+                            onTap: (() {
+                              hideAnimation.animateTo(0);
+                              scrollController.animateTo(200,
+                                  duration: const Duration(milliseconds: 350),
+                                  curve: Curves.decelerate);
+                            }),
+                            child: Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    colors: const [
+                                      AMAPColorConstants.green1,
+                                      AMAPColorConstants.green2
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: AMAPColorConstants.green2
+                                          .withOpacity(0.4),
+                                      offset: const Offset(2, 3),
+                                      blurRadius: 5)
+                                ],
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(25)),
+                              ),
+                              alignment: Alignment.center,
+                              child: HeroIcon(
+                                HeroIcons.chevronDoubleRight,
+                                size: 15,
+                                color: AMAPColorConstants.background,
+                              ),
+                            )),
+                      )),
+                ),
+              ],
+            ))
+        : Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            height: 100,
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              controller: scrollController,
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: btns,
+              ),
+            ));
   }
 }
