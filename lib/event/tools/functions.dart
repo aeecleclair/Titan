75e343c67a2d1aae9c254aa407880b30071c600b
Maxime ROUCHER <34400034+maximeroucher@users.noreply.github.com>
2022-09-16 22:34:15 +0200

    Improve Event Detail page

--- lib/event/tools/functions.dart
+++ lib/event/tools/functions.dart
@@ -50,3 +50,82 @@ CalendarEventType stringToCalendarEventType(String type) {
       return CalendarEventType.other;
   }
 }
+
+String processDateOnlyHour(DateTime date) {
+  return date.hour.toString().padLeft(2, '0') +
+      ":" +
+      date.minute.toString().padLeft(2, '0');
+}
+
+List<String> parseDate(DateTime date) {
+  return [
+    date.day.toString().padLeft(2, '0') +
+        "/" +
+        date.month.toString().padLeft(2, '0') +
+        "/" +
+        date.year.toString(),
+    date.hour.toString().padLeft(2, '0') +
+        ":" +
+        date.minute.toString().padLeft(2, '0')
+  ];
+}
+
+String formatDates(DateTime dateStart, DateTime dateEnd) {
+  final start = parseDate(dateStart);
+  final end = parseDate(dateEnd);
+  if (start[0] == end[0]) {
+    return "Le " + start[0] + " de " + start[1] + " à " + end[1];
+  } else {
+    return "Du " +
+        start[0] +
+        " à " +
+        start[1] +
+        " au " +
+        end[0] +
+        " à " +
+        end[1];
+  }
+}
+
+String formatDateOnlyHour(DateTime dateStart, DateTime dateEnd) {
+  final start = parseDate(dateStart);
+  final end = parseDate(dateEnd);
+  return "De " + start[1] + " à " + end[1];
+}
+
+String formatDateOnlyDay(DateTime dateStart, DateTime dateEnd) {
+  final start = parseDate(dateStart);
+  final end = parseDate(dateEnd);
+  if (start[0] == end[0]) {
+    return "Le " + start[0];
+  } else {
+    return "Du " + start[0] + " au " + end[0];
+  }
+}
+
+String formatDays(String recurrenceRule) {
+  final listDay = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche"
+  ];
+  final listDayShort = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
+  final days = recurrenceRule.split(";")[1].split("BYDAY=")[1].split(",");
+  String res = "";
+  if (days.length > 1) {
+    for (int i = 0; i < days.length - 1; i++) {
+      res += listDay[listDayShort.indexOf(days[i]) - 1];
+      if (i != days.length - 2) {
+        res += ", ";
+      }
+    }
+    res += " et " + listDay[listDayShort.indexOf(days[days.length - 1]) - 1];
+  } else {
+    res += listDay[listDayShort.indexOf(days[0]) - 1];
+  }
+  return res;
+}
