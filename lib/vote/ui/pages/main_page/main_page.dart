8dc228d5d1f58660464f8a800d1adc74f025a8ef
Maxime Roucher <maximeroucher@gmail.com>
2022-10-17 17:40:06 +0200

    Adding basic interactions

--- lib/vote/ui/pages/main_page/main_page.dart
+++ lib/vote/ui/pages/main_page/main_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/vote/providers/pretendance_provider.dart';
 import 'package:myecl/vote/providers/section_provider.dart';
 import 'package:myecl/vote/tools/constants.dart';
 import 'package:myecl/vote/ui/pages/main_page/pretendance_card.dart';
 import 'package:myecl/vote/ui/pages/main_page/side_item.dart';
+import 'package:myecl/vote/ui/refresh_indicator.dart';
 
 class MainPage extends HookConsumerWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -13,72 +15,166 @@ class MainPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final sections = ref.watch(sectionProvider);
     final pretendances = ref.watch(pretendanceProvider);
-    return Padding(
-        padding: const EdgeInsets.only(left: 30.0),
-        child: Column(children: [
-          const SizedBox(
-            height: 20,
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Text(VoteTextConstants.vote,
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black)),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Expanded(
-            child: sections.when(
-              data: (sectionList) => Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      children: sectionList
-                          .map(
-                            (e) => SideItem(
-                              section: e,
-                              isSelected: sectionList.first == e,
+    final selectedSection = useState(0);
+    final selectedPretendance = useState(-1);
+    final currentPretendance = useState(-1);
+    return VoteRefresher(
+      onRefresh: () async {
+        ref.refresh(sectionProvider);
+        ref.refresh(pretendanceProvider);
+      },
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(
+            parent: BouncingScrollPhysics()),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height - 100,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Column(children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(VoteTextConstants.vote,
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black)),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: sections.when(
+                    data: (sectionList) => Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SingleChildScrollView(
+                          physics: const BouncingScrollPhysics(),
+                          child: Column(
+                            children: sectionList.map((e) {
+                              final index = sectionList.indexOf(e);
+                              return SideItem(
+                                section: e,
+                                isSelected: index == selectedSection.value,
+                                onTap: () {
+                                  selectedSection.value = index;
+                                },
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: pretendances.when(
+                              data: (pretendanceList) => Column(children: [
+                                Container(
+                                  padding: const EdgeInsets.only(left: 20),
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                      sectionList[selectedSection.value].name,
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w700,
+                                          color:
+                                              Color.fromARGB(255, 1, 40, 72))),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                Expanded(
+                                  child: SingleChildScrollView(
+                                    child: Column(
+                                      children: pretendanceList.map((e) {
+                                        final index =
+                                            pretendanceList.indexOf(e);
+                                        return PretendanceCard(
+                                            pretendance: pretendanceList[index],
+                                            isCurrent: index ==
+                                                currentPretendance.value,
+                                            isSelected: index ==
+                                                selectedPretendance.value,
+                                            onTap: () {
+                                              if (currentPretendance.value ==
+                                                  index) {
+                                                currentPretendance.value = -1;
+                                              } else {
+                                                currentPretendance.value =
+                                                    index;
+                                              }
+                                            },
+                                            onVote: () {
+                                              selectedPretendance.value = index;
+                                            });
+                                      }).toList(),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  padding: EdgeInsets.only(right: 30),
+                                  child: GestureDetector(
+                                    onTap: () {},
+                                    child: Container(
+                                      width: 200,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                              begin: Alignment.topLeft,
+                                              end: Alignment.bottomRight,
+                                              colors: selectedPretendance
+                                                          .value !=
+                                                      -1
+                                                  ? const [
+                                                      VoteColorConstants.green5,
+                                                      Color.fromARGB(
+                                                          255, 1, 40, 72),
+                                                    ]
+                                                  : const [
+                                                      Color.fromARGB(
+                                                          150, 32, 83, 116),
+                                                      Color.fromARGB(
+                                                          150, 1, 40, 72),
+                                                    ]),
+                                          borderRadius:
+                                              BorderRadius.circular(15)),
+                                      child: const Center(
+                                        child: Text(
+                                          VoteTextConstants.vote,
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                              loading: () => const Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                              error: (error, stack) => const Center(
+                                child: Text('Error'),
+                              ),
                             ),
-                          )
-                          .toList(),
+                          ),
+                        ),
+                      ],
                     ),
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (error, stack) => const Center(child: Text('Error')),
                   ),
-                  Expanded(
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: pretendances.when(
-                        data: (pretendanceList) => ListView.builder(
-                          itemCount: pretendanceList.length,
-                          itemBuilder: (context, index) {
-                            return PretendanceCard(
-                                pretendance: pretendanceList[index],
-                                isCurrent: index == 0,
-                                isSelected: index == 1);
-                          },
-                        ),
-                        loading: () => const Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                        error: (error, stack) => const Center(
-                          child: Text('Error'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              loading: () => const Center(child: CircularProgressIndicator()),
-              error: (error, stack) => const Center(child: Text('Error')),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ]));
+                ),
+              ])),
+        ),
+      ),
+    );
   }
 }
--- lib/vote/ui/pages/main_page/pretendance_card.dart
+++ lib/vote/ui/pages/main_page/pretendance_card.dart
@@ -8,144 +8,150 @@ class PretendanceCard extends HookConsumerWidget {
   final Pretendance pretendance;
   final bool isCurrent;
   final bool isSelected;
+  final void Function() onTap, onVote;
   const PretendanceCard(
       {super.key,
       required this.pretendance,
       required this.isCurrent,
-      required this.isSelected});
+      required this.isSelected,
+      required this.onTap,
+      required this.onVote});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 40, left: 50),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(20),
-          bottomLeft: Radius.circular(20),
-        ),
-        gradient: const LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            VoteColorConstants.green5,
-            Color.fromARGB(255, 1, 40, 72),
-          ],
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: const Color.fromARGB(255, 1, 40, 72).withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(2, 3), // changes position of shadow
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20, left: 10),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
           ),
-        ],
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              const Expanded(
-                  child: HeroIcon(
-                HeroIcons.cubeTransparent,
-                color: Colors.white,
-                size: 50,
-              )),
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      pretendance.name,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: HeroIcon(
-                  isSelected ? HeroIcons.check : HeroIcons.xMark,
-                  color: Colors.white,
-                ),
-              ),
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              VoteColorConstants.green5,
+              Color.fromARGB(255, 1, 40, 72),
             ],
           ),
-          Text(
-            "Liste ${pretendance.listType.toString().split(".").last}",
-            style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 255, 255, 255),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 1, 40, 72).withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(2, 3), // changes position of shadow
             ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          if (isCurrent)
-            Column(
+          ],
+        ),
+        child: Column(
+          children: [
+            Row(
               children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  pretendance.description,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 255, 255, 255),
+                const Expanded(
+                    child: HeroIcon(
+                  HeroIcons.cubeTransparent,
+                  color: Colors.white,
+                  size: 50,
+                )),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Text(
+                        pretendance.name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
+                Expanded(
+                  child: HeroIcon(
+                    isSelected ? HeroIcons.check : HeroIcons.xMark,
+                    color: Colors.white,
+                  ),
                 ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.white.withOpacity(0.2),
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(
-                                2, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Voter pour cette liste',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 1, 40, 72),
+              ],
+            ),
+            Text(
+              "Liste ${pretendance.listType.toString().split(".").last}",
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            if (isCurrent)
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    pretendance.description,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: GestureDetector(
+                      onTap: onVote,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white.withOpacity(0.2),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: const Offset(
+                                  2, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Voter pour cette liste',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 1, 40, 72),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-              ],
-            ),
-        ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
     );
   }
