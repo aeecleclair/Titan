5236d6f502883fb780947b4459f915cd1936fee9
Maxime Roucher <maximeroucher@gmail.com>
2022-12-12 11:04:28 +0100

    Fixing getting vote count

--- lib/vote/repositories/section_vote_count_repository.dart
+++ lib/vote/repositories/section_vote_count_repository.dart
@@ -3,7 +3,7 @@ import 'package:myecl/tools/repository/repository.dart';
 class SectionVoteCountRepository extends Repository {
   @override
   // ignore: overridden_fields
-  final ext = '/campaign/stats/';
+  final ext = 'campaign/stats/';
 
   Future<int> getSectionVoteCount(String id) async {
     return await getOne(id);
