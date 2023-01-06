7a3ff5451673179da3ded7253f3cfb08affffe11
Maxime Roucher <34400034+maximeroucher@users.noreply.github.com>
2023-01-06 21:53:53 +0100

    Pop up on switching section without saving vote during vote

--- lib/vote/ui/pages/main_page/list_side_item.dart
+++ lib/vote/ui/pages/main_page/list_side_item.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/dialog.dart';
+import 'package:myecl/vote/class/pretendance.dart';
 import 'package:myecl/vote/class/section.dart';
 import 'package:myecl/vote/providers/section_id_provider.dart';
 import 'package:myecl/vote/providers/sections_provider.dart';
 import 'package:myecl/vote/providers/selected_pretendance_provider.dart';
 import 'package:myecl/vote/providers/voted_section_provider.dart';
+import 'package:myecl/vote/tools/constants.dart';
 import 'package:myecl/vote/ui/pages/main_page/side_item.dart';
 
 class ListSideItem extends HookConsumerWidget {
@@ -16,6 +19,7 @@ class ListSideItem extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final sectionIdNotifier = ref.watch(sectionIdProvider.notifier);
+    final selectedPretendance = ref.watch(selectedPretendanceProvider);
     final selectedPretendanceNotifier =
         ref.watch(selectedPretendanceProvider.notifier);
     final section = ref.watch(sectionProvider);
@@ -32,9 +36,22 @@ class ListSideItem extends HookConsumerWidget {
             isSelected: e.id == section.id,
             alreadyVoted: votedSections.contains(e.id),
             onTap: () async {
-              selectedPretendanceNotifier.clear();
-              animation.forward(from: 0);
-              sectionIdNotifier.setId(e.id);
+              if (selectedPretendance.id == Pretendance.empty().id) {
+                animation.forward(from: 0);
+                sectionIdNotifier.setId(e.id);
+              } else {
+                showDialog(
+                    context: context,
+                    builder: (context) => CustomDialogBox(
+                          title: VoteTextConstants.warning,
+                          descriptions: VoteTextConstants.warningMessage,
+                          onYes: () {
+                            selectedPretendanceNotifier.clear();
+                            animation.forward(from: 0);
+                            sectionIdNotifier.setId(e.id);
+                          },
+                        ));
+              }
             },
           );
         }).toList(),
