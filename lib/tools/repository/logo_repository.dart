1e5fe74f3ba27e0e90875c6961e0aed31eb70ba6
Maxime Roucher <34400034+maximeroucher@users.noreply.github.com>
2023-01-09 12:53:03 +0100

    Fixing error handling + focusscope error + confirmedbooking synchronisation

--- lib/tools/repository/logo_repository.dart
+++ lib/tools/repository/logo_repository.dart
@@ -28,19 +28,12 @@ abstract class LogoRepository extends Repository {
     } else if (response.statusCode == 403) {
       FLog.error(
           text: "GET ${ext + suffix}\n${response.statusCode} ${response.body}");
-      try {
-        String resp = utf8.decode(response.body.runes.toList());
-        final decoded = json.decode(resp);
-        if (decoded["detail"] == expiredTokenDetail) {
-          throw AppException(ErrorType.tokenExpire, decoded["detail"]);
-        } else {
-          throw AppException(ErrorType.notFound, decoded["detail"]);
-        }
-      } catch (e) {
-        FLog.error(
-            text: "GET ${ext + suffix}\nError while decoding response",
-            exception: e);
-        throw AppException(ErrorType.notFound, response.body);
+      String resp = utf8.decode(response.body.runes.toList());
+      final decoded = json.decode(resp);
+      if (decoded["detail"] == expiredTokenDetail) {
+        throw AppException(ErrorType.tokenExpire, decoded["detail"]);
+      } else {
+        throw AppException(ErrorType.notFound, decoded["detail"]);
       }
     } else {
       FLog.error(
