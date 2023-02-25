abb4a2bc139f8ed14ab2628c5ffcd1358ef37ccb
Maxime Roucher <maximeroucher@gmail.com>
2023-02-25 16:07:37 +0100

    Updating order list repository and provider to match new endpoint

--- lib/amap/repositories/order_list_repository.dart
+++ lib/amap/repositories/order_list_repository.dart
@@ -4,26 +4,24 @@ import 'package:myecl/tools/repository/repository.dart';
 class OrderListRepository extends Repository {
   @override
   // ignore: overridden_fields
-  final ext = "amap/deliveries/";
+  final ext = "amap/orders";
 
-  Future<Order> createOrder(
-      String deliveryId, Order order, String userId) async {
+  Future<Order> createOrder(Order order) async {
     return Order.fromJson(
-        await create(order.toJson(), suffix: "$deliveryId/orders"));
+        await create(order.toJson()));
   }
 
-  Future<bool> updateOrder(
-      String deliveryId, Order order, String userId) async {
-    return await update(order.toJson(), deliveryId, suffix: "/orders/${order.id}");
+  Future<bool> updateOrder(Order order) async {
+    return await update(order.toJson(), "/${order.id}");
   }
 
-  Future<bool> deleteOrder(String deliveryId, String orderId) async {
-    return await delete(deliveryId, suffix: "/orders/$orderId");
+  Future<bool> deleteOrder(String orderId) async {
+    return await delete("/$orderId");
   }
 
-  Future<List<Order>> getOrder(String deliveryId, String orderId) async {
+  Future<List<Order>> getOrder(String orderId) async {
     return List<Order>.from(
-        (await getList(suffix: "$deliveryId/orders/$orderId"))
+        (await getList(suffix: "/$orderId"))
             .map((x) => Order.fromJson(x)));
   }
 
