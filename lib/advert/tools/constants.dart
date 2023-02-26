ba7f3dcb872e38100b22a4f4331f022e1a1b68e7
Shaihh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-02-26 17:25:06 +0100

    Admin page complete

--- lib/advert/tools/constants.dart
+++ lib/advert/tools/constants.dart
@@ -2,5 +2,6 @@ class AdvertTextConstants {
   static const String advert = "Annonce";
   static const String strassAdvert = "AdminiSTRation";
   static const String assoAdvert = "Association";
-
+  static const String deleteAdvert = "Supprimer l'annonce ?";
+  static const String deleting = "Suppression";
 }
