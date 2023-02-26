4d908fac5a6840ca967183e4c98202662cfb6b4b
Shaihh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-02-26 17:25:06 +0100

    Admin page complete

---
+++ lib/advert/ui/pages/admin_page/admin_advert_card.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/advert/class/advert.dart';
+import 'package:myecl/advert/ui/pages/main_page/advert_card.dart';
+import 'package:myecl/tools/ui/shrink_button.dart';
+
+class AdminAdvertCard extends HookConsumerWidget {
+  final VoidCallback onTap, onEdit;
+  final Future Function() onDelete;
+  final Advert advert;
+
+const AdminAdvertCard(
+      {super.key,
+      required this.advert,
+      required this.onTap,
+      required this.onEdit,
+      required this.onDelete});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Stack(
+      children: [
+        AdvertCard(onTap: onTap, advert: advert),
+        Positioned(
+          right: 40,
+          child: SizedBox(
+            height: 270,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: onEdit,
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            blurRadius: 10,
+                            offset: const Offset(2, 3))
+                      ],
+                    ),
+                    child: const HeroIcon(HeroIcons.pencil, color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                ShrinkButton(
+                  onTap: onDelete,
+                  waitChild: Container(
+                    width: 40,
+                    height: 40,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(1),
+                            blurRadius: 10,
+                            offset: const Offset(2, 3))
+                      ],
+                    ),
+                    child: const Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.white,
+                    )),
+                  ),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.red.withOpacity(0.2),
+                            blurRadius: 10,
+                            offset: const Offset(2, 3))
+                      ],
+                    ),
+                    child: const HeroIcon(HeroIcons.trash, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/advert/ui/pages/admin_page/admin_page.dart
+++ lib/advert/ui/pages/admin_page/admin_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/advert/class/announcer.dart';
+import 'package:myecl/advert/class/advert.dart';
 import 'package:myecl/advert/providers/advert_list_provider.dart';
 import 'package:myecl/advert/providers/advert_page_provider.dart';
 import 'package:myecl/advert/providers/advert_provider.dart';
 import 'package:myecl/advert/providers/announcer_provider.dart';
+import 'package:myecl/advert/tools/constants.dart';
+import 'package:myecl/advert/ui/pages/admin_page/admin_advert_card.dart';
 import 'package:myecl/advert/ui/pages/admin_page/announcer_bar.dart';
-import 'package:myecl/advert/ui/pages/main_page/advert_card.dart';
+import 'package:myecl/tools/ui/dialog.dart';
 import 'package:myecl/tools/ui/refresher.dart';
 
 class AdminPage extends HookConsumerWidget {
@@ -25,44 +28,99 @@ class AdminPage extends HookConsumerWidget {
         SizedBox(
           height: MediaQuery.of(context).size.height - 165.4,
           child: IntrinsicHeight(
-            
             child: Refresher(
-                onRefresh: () async {
-                  await advertListNotifier.loadAdverts();
+              onRefresh: () async {
+                await advertListNotifier.loadAdverts();
+              },
+              child: advertList.when(
+                data: (data) {
+                  return Column(
+                    children: [
+                      GestureDetector(
+                              onTap: () {
+                                advertNotifier.setAdvert(Advert.empty());
+                                pageNotifier.setAdvertPage(
+                                    AdvertPage.addEditAdvert);
+                              },
+                              child: Container(
+                                  margin: const EdgeInsets.only(
+                                      bottom: 10, top: 20, left: 30, right: 30),
+                                  width: double.infinity,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight,
+                                        colors: [
+                                          Colors.white,
+                                          Colors.grey.shade100,
+                                        ],
+                                      ),
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.2),
+                                          spreadRadius: 5,
+                                          blurRadius: 10,
+                                          offset: const Offset(3, 3),
+                                        )
+                                      ]),
+                                  child: Center(
+                                      child: HeroIcon(
+                                    HeroIcons.plus,
+                                    size: 40,
+                                    color: Colors.grey.shade500,
+                                  ))),
+                            ),
+                      ...data
+                          .map((advert) => selected
+                                      .where(
+                                          (e) => advert.groups.contains(e.name))
+                                      .isNotEmpty ||
+                                  selected.isEmpty
+                              ? AdminAdvertCard(
+                                  onTap: () {
+                                    advertNotifier.setAdvert(advert);
+                                    pageNotifier.setAdvertPage(
+                                        AdvertPage.detailFromMainPage);
+                                  },
+                                  onEdit: () {
+                                    pageNotifier.setAdvertPage(
+                                        AdvertPage.addEditAdvert);
+                                  },
+                                  onDelete: () async {
+                                    await showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return CustomDialogBox(
+                                            title: AdvertTextConstants.deleting,
+                                            descriptions: AdvertTextConstants
+                                                .deleteAdvert,
+                                            onYes: () {
+                                              advertListNotifier
+                                                  .deleteAdvert(advert);
+                                            },
+                                          );
+                                        });
+                                  },
+                                  advert: advert)
+                              : Container())
+                          .toList(),
+                    ],
+                  );
                 },
-                child: advertList.when(
-                  data: (data) {
-                    return Column(
-                      children: [
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        ...data
-                            .map((advert) => selected.where((e)=>advert.groups.contains(e.name)).isNotEmpty || selected.isEmpty ? AdvertCard(
-                                onTap: () {
-                                  print(selected);
-                                  advertNotifier.setAdvert(advert);
-                                  pageNotifier
-                                      .setAdvertPage(AdvertPage.detailFromMainPage);
-                                },
-                                advert: advert):Container()
-                                )
-                            .toList(),
-                      ],
-                    );
-                  },
-                  loading: () {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                  error: (error, stackTrace) {
-                    return Center(
-                      child: Text(error.toString()),
-                    );
-                  },
-                ),
+                loading: () {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+                error: (error, stackTrace) {
+                  return Center(
+                    child: Text(error.toString()),
+                  );
+                },
               ),
+            ),
           ),
         ),
       ],
