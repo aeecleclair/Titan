c28ba3d457ec5bd2c1d5f7182f430558da373203
Shaihh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-03-01 10:33:28 +0100

    ui

--- lib/advert/tools/constants.dart
+++ lib/advert/tools/constants.dart
@@ -1,4 +1,6 @@
 class AdvertTextConstants {
+  static const String add = "Ajouter";
+  static const String edit = "Modifier";
   static const String advert = "Annonce";
   static const String strassAdvert = "AdminiSTRation";
   static const String assoAdvert = "Association";
@@ -6,4 +8,8 @@ class AdvertTextConstants {
   static const String deleting = "Suppression";
   static const String title = "Titre";
   static const String content = "Contenu";
+  static const String tags = "Tags";
+  static const String noValue = "Veuillez entrer une valeur";
+  static const String positiveNumber = "Veuillez entrer un nombre positif";
+  static const String invalidNumber = "Veuillez entrer un nombre";
 }
