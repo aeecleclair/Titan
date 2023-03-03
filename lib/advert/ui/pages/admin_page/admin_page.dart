26c565ac075a665d3515629c010c11a76fd9d632
Shaihh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-03-03 16:44:22 +0100

    clean ui

--- lib/advert/ui/pages/admin_page/admin_page.dart
+++ lib/advert/ui/pages/admin_page/admin_page.dart
@@ -24,113 +24,102 @@ class AdminPage extends HookConsumerWidget {
     final selected = ref.watch(announcerProvider);
     return Column(
       children: [
-        const AnnouncerBar(),
+    
         SizedBox(
-          height: MediaQuery.of(context).size.height - 165.4,
-          child: IntrinsicHeight(
-            child: Refresher(
-              onRefresh: () async {
-                await advertListNotifier.loadAdverts();
+          height: MediaQuery.of(context).size.height-117.5,
+          child: Refresher(
+            onRefresh: () async {
+              await advertListNotifier.loadAdverts();
+            },
+            child: advertList.when(
+              data: (data) {
+                return Column(
+                  children: [
+                    const AnnouncerBar(),
+                    GestureDetector(
+                      onTap: () {
+                        advertNotifier.setAdvert(Advert.empty());
+                        pageNotifier.setAdvertPage(AdvertPage.addEditAdvert);
+                      },
+                      child: Container(
+                          margin: const EdgeInsets.only(
+                              bottom: 10, top: 20, left: 30, right: 30),
+                          width: double.infinity,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Colors.white,
+                                  Colors.grey.shade100,
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  spreadRadius: 5,
+                                  blurRadius: 10,
+                                  offset: const Offset(3, 3),
+                                )
+                              ]),
+                          child: Center(
+                              child: HeroIcon(
+                            HeroIcons.plus,
+                            size: 40,
+                            color: Colors.grey.shade500,
+                          ))),
+                    ),
+                    ...data
+                        .map((advert) => selected
+                                    .where(
+                                        (e) => advert.announcer.contains(e.name))
+                                    .isNotEmpty ||
+                                selected.isEmpty
+                            ? AdminAdvertCard(
+                                onTap: () {
+                                  advertNotifier.setAdvert(advert);
+                                  pageNotifier.setAdvertPage(
+                                      AdvertPage.detailFromAdminPage);
+                                },
+                                onEdit: () {
+                                  advertNotifier.setAdvert(advert);
+                                  pageNotifier.setAdvertPage(
+                                      AdvertPage.addEditAdvert);
+                                },
+                                onDelete: () async {
+                                  await showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return CustomDialogBox(
+                                          title: AdvertTextConstants.deleting,
+                                          descriptions: AdvertTextConstants
+                                              .deleteAdvert,
+                                          onYes: () {
+                                            advertListNotifier
+                                                .deleteAdvert(advert);
+                                          },
+                                        );
+                                      });
+                                },
+                                advert: advert)
+                            : Container())
+                        .toList(),
+                  ],
+                );
               },
-              child: advertList.when(
-                data: (data) {
-                  return Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          advertNotifier.setAdvert(Advert.empty());
-                          pageNotifier.setAdvertPage(AdvertPage.addEditAdvert);
-                        },
-                        child: Container(
-                            margin: const EdgeInsets.only(
-                                bottom: 10, top: 20, left: 30, right: 30),
-                            width: double.infinity,
-                            height: 100,
-                            decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: [
-                                    Colors.white,
-                                    Colors.grey.shade100,
-                                  ],
-                                ),
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.2),
-                                    spreadRadius: 5,
-                                    blurRadius: 10,
-                                    offset: const Offset(3, 3),
-                                  )
-                                ]),
-                            child: Center(
-                                child: HeroIcon(
-                              HeroIcons.plus,
-                              size: 40,
-                              color: Colors.grey.shade500,
-                            ))),
-                      ),
-                      ...data
-                          .map((advert) => selected
-                                      .where(
-                                          (e) => advert.announcer.contains(e.name))
-                                      .isNotEmpty ||
-                                  selected.isEmpty
-                              ? AdminAdvertCard(
-                                  onTap: () {
-                                    advertNotifier.setAdvert(advert);
-                                    pageNotifier.setAdvertPage(
-                                        AdvertPage.detailFromAdminPage);
-                                  },
-                                  onEdit: () {
-                                    advertNotifier.setAdvert(advert);
-                                    pageNotifier.setAdvertPage(
-                                        AdvertPage.addEditAdvert);
-                                  },
-                                  onDelete: () async {
-                                    await showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return CustomDialogBox(
-                                            title: AdvertTextConstants.deleting,
-                                            descriptions: AdvertTextConstants
-                                                .deleteAdvert,
-                                            onYes: () {
-                                              advertListNotifier
-                                                  .deleteAdvert(advert);
-                                            },
-                                          );
-                                        });
-                                  },
-                                  advert: advert)
-                              : Container())
-                          .toList(),
-                    ],
-                  );
-                },
-                loading: () {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
-                error: (error, stackTrace) {
-                  return Center(
-                    child: Text(error.toString()),
-                  );
-                },
-              ),
+              loading: () {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+              error: (error, stackTrace) {
+                return Center(
+                  child: Text(error.toString()),
+                );
+              },
             ),
-          ),
-        ),
-        Expanded(
-          // A flexible child that will grow to fit the viewport but
-          // still be at least as big as necessary to fit its contents.
-          child: Container(
-            color: const Color(0xffee0000), // Red
-            height: 120.0,
-            alignment: Alignment.center,
-            child: const Text('Flexible Content'),
           ),
         ),
       ],
