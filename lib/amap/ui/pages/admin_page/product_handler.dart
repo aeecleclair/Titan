e47f965abd42c054dbf15c333c82fec1b75eb40f
Maxime Roucher <maximeroucher@gmail.com>
2023-03-10 10:29:38 +0100

    Adding amap list product navigation button

--- lib/amap/ui/pages/admin_page/product_handler.dart
+++ lib/amap/ui/pages/admin_page/product_handler.dart
@@ -59,6 +59,7 @@ class ProductHandler extends HookConsumerWidget {
                   pageNotifier.setAmapPage(AmapPage.addEditProduct);
                 },
                 child: Container(
+                    height: 185,
                     margin: const EdgeInsets.only(left: 5.0, bottom: 10),
                     padding: const EdgeInsets.all(12.0),
                     child: Container(
