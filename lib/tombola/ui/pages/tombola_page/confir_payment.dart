de7b57f17614d28d21bf628e2b9cf738af505a61
Arcturyus@gmail.com <romainpas17@gmail.com>
2023-03-11 16:49:34 +0100

    tombola page ui v1

---
+++ lib/tombola/ui/pages/tombola_page/confir_payment.dart
@@ -0,0 +1,169 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tombola/class/raffle.dart';
+import 'package:myecl/tombola/class/type_ticket.dart';
+
+class ConfirmPaymentDialog extends HookConsumerWidget {
+  final TypeTicket type_ticket;
+  final Raffle raffle;
+  const ConfirmPaymentDialog(
+      {Key? key, required this.type_ticket, required this.raffle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Dialog(
+        backgroundColor: Colors.transparent,
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            width: 300,
+            height: 450,
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(191, 255, 255, 255),
+                    blurRadius: 15,
+                    blurStyle: BlurStyle.outer,
+                  ),
+                ],
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color.fromARGB(237, 0, 12, 41),
+                      Color.fromARGB(237, 0, 1, 6)
+                    ])),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 35, vertical: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white.withOpacity(0.3),
+                                  blurRadius: 8,
+                                  offset: const Offset(1, 2),
+                                ),
+                              ],
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(15))),
+                          child: Center(
+                            child: Image.asset("assets/images/logo.png",
+                                height: 100),
+                          ),
+                        ),
+                        Text(
+                          "${type_ticket.price} €",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "${type_ticket.nbTicket} tickets",
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 30),
+                  AutoSizeText(
+                    raffle.name,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Spacer(),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 7, horizontal: 12),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(87, 255, 255, 255),
+                                  blurRadius: 8,
+                                  offset: const Offset(1, 2),
+                                ),
+                              ],
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Color.fromARGB(255, 40, 245, 211),
+                                    Color.fromARGB(255, 11, 94, 188),
+                                    Color.fromARGB(255, 1, 10, 133)
+                                  ],
+                                  stops: [
+                                    0,
+                                    0.7,
+                                    1
+                                  ]),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: Text("Annuler",
+                              maxLines: 1,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))),
+                      Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 7, horizontal: 12),
+                              margin: EdgeInsets.only(left:5),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(87, 255, 255, 255),
+                                  blurRadius: 8,
+                                  offset: const Offset(1, 2),
+                                ),
+                              ],
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Color.fromARGB(255, 245, 183, 40),
+                                    Color.fromARGB(255, 255, 34, 34),
+                                    Color.fromARGB(255, 140, 13, 1)
+                                    
+                                  ],
+                                  stops: [
+                                    0,
+                                    0.7,
+                                    1
+                                  ]),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: Text("Confirmer la prise du billet",
+                              maxLines: 2,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold))),
+                    ],
+                  )
+                ])));
+  }
+}
--- lib/tombola/ui/pages/tombola_page/prize_card.dart
+++ lib/tombola/ui/pages/tombola_page/prize_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/tombola/class/lot.dart';
 
-class PrizeCard extends HookConsumerWidget{ 
+class PrizeCard extends HookConsumerWidget {
   const PrizeCard({
     Key? key,
     required this.prize,
@@ -11,12 +11,10 @@ class PrizeCard extends HookConsumerWidget{
   final Lot prize;
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final color = Colors.cyan;
-    return 
-      Container(
-        width:100,
-        height: 100,
+    return Container(
+        width: 150,
         margin: const EdgeInsets.only(left: 10),
         padding: const EdgeInsets.all(5),
         decoration: BoxDecoration(
@@ -25,7 +23,7 @@ class PrizeCard extends HookConsumerWidget{
                 color: color,
                 spreadRadius: 0,
                 blurRadius: 8,
-                blurStyle:BlurStyle.outer,
+                blurStyle: BlurStyle.outer,
               ),
             ],
             gradient: LinearGradient(
@@ -33,15 +31,53 @@ class PrizeCard extends HookConsumerWidget{
               end: Alignment.bottomRight,
               colors: [
                 color.withOpacity(0.1),
-                color.withOpacity(0.2),
+                color.withOpacity(0.3),
               ],
               stops: const [0, 0.8],
             ),
             borderRadius: const BorderRadius.all(Radius.circular(20))),
-      );
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: color, width: 2),
+                    ),
+                  ),
+                  child: Center(
+                      child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text(
+                            prize.name,
+                            style: TextStyle(
+                                color: color,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          )))),
+              const SizedBox(height: 5,),
+              Center(
+                  child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        "${prize.quantity} lots gagnables",
+                        style: TextStyle(
+                            color: color,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ))),
+              const Spacer(),
+              Text(
+                textAlign: TextAlign.center,
+                prize.description ?? "Pas de description",
+                maxLines: 5,
+                style: TextStyle(
+                    color: color, fontSize: 10, fontWeight: FontWeight.bold),
+              ),
+            ]));
   }
 }
 
-class RowOfPrize{ //Recois des images (ou des wiget LotsCard ? ) de Lots en arguments renvoie la Row des (3?) lots
-
+class RowOfPrize {
+  //Recois des images (ou des wiget LotsCard ? ) de Lots en arguments renvoie la Row des (3?) lots
 }
