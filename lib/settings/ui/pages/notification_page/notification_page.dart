62ee211dcd554acd0d28d8885724f550d1bc82f4
Maxime Roucher <maximeroucher@gmail.com>
2023-03-14 19:32:23 +0100

    Adding notification page in settings

--- lib/settings/ui/pages/notification_page/notification_page.dart
+++ lib/settings/ui/pages/notification_page/notification_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/admin/providers/group_list_provider.dart';
+import 'package:myecl/settings/providers/module_list_provider.dart';
 import 'package:myecl/settings/tools/constants.dart';
 import 'package:myecl/settings/ui/settings.dart';
 import 'package:myecl/tools/constants.dart';
