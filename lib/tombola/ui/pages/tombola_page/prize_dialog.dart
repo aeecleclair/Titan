e05508f42c6c91b429b65e68e9d52ace36645d41
Arcturyus@gmail.com <romainpas17@gmail.com>
2023-03-16 00:18:53 +0100

    few UI possibilities for different widget

--- lib/tombola/ui/pages/tombola_page/prize_dialog.dart
+++ lib/tombola/ui/pages/tombola_page/prize_dialog.dart
@@ -14,72 +14,102 @@ class PrizeDialog extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Dialog(
         backgroundColor: Colors.transparent,
-        child: Container(
+        child: Stack(children: <Widget>[
+          Container(
             width: 350,
             height: 300,
             margin: const EdgeInsets.only(left: 10),
-            padding: const EdgeInsets.all(5),
+            padding: const EdgeInsets.all(25),
             decoration: BoxDecoration(
-                boxShadow: const [
+                boxShadow: [
                   BoxShadow(
-                    color: TombolaColorConstants.darkText,
+                    color: TombolaColorConstants.writtenWhite.withOpacity(0.4),
                     spreadRadius: 0,
                     blurRadius: 8,
                     blurStyle: BlurStyle.outer,
                   ),
                 ],
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.white.withOpacity(0.7),
-                    Colors.white.withOpacity(0.9),
-                  ],
-                  stops: const [0, 0.8],
-                ),
+                gradient: RadialGradient(colors: [
+                  Colors.white,
+                  TombolaColorConstants.gradient1.withOpacity(0.8),
+                ], center: Alignment(1, 1), radius: 0.6),
                 borderRadius: const BorderRadius.all(Radius.circular(20))),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.white, width: 3),
+          ),
+          Container(
+              width: 350,
+              height: 300,
+              margin: const EdgeInsets.only(left: 10),
+              padding: const EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          TombolaColorConstants.writtenWhite.withOpacity(0.4),
+                      spreadRadius: 0,
+                      blurRadius: 8,
+                      blurStyle: BlurStyle.outer,
                     ),
-                  ),
-                  child: Center(
-                      child: FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Text(
-                            prize.name,
-                            style: const TextStyle(
-                                color: TombolaColorConstants.darkText,
-                                fontSize: 60,
-                                fontWeight: FontWeight.bold),
-                          )))),
-              const SizedBox(
-                height: 20,
-              ),
-              Center(
-                  child: FittedBox(
-                      fit: BoxFit.fitWidth,
-                      child: Text(
-                        "${prize.quantity} lots gagnables",
-                        style: const TextStyle(
-                            color: TombolaColorConstants.darkText,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ))),
-              const Spacer(),
-              AutoSizeText(
-                textAlign: TextAlign.justify,
-                prize.description ?? "Pas de description",
-                maxLines: 3,
-                style: const TextStyle(
-                    color: TombolaColorConstants.darkText,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
-              ),
-            ])));
+                  ],
+                  gradient: RadialGradient(colors: [
+                    TombolaColorConstants.gradient2,
+                    TombolaColorConstants.gradient1.withOpacity(0.8),
+                  ], center: Alignment(-0.9, -0.9), radius: 1.4),
+                  borderRadius: const BorderRadius.all(Radius.circular(20))),
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                            color: TombolaColorConstants.writtenWhite,
+                            width: 3),
+                      ),
+                    ),
+                    child: Center(
+                        child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: ShaderMask(
+                                blendMode: BlendMode.srcIn,
+                                shaderCallback: (bounds) => RadialGradient(
+                                      colors: [
+                                        TombolaColorConstants.writtenWhite,
+                                        Color.fromARGB(255, 208, 217, 249)
+                                      ],
+                                      radius: 2.0,
+                                      tileMode: TileMode.mirror,
+                                      center: Alignment.center,
+                                    ).createShader(bounds),
+                                child: Text(
+                                  prize.name,
+                                  style: const TextStyle(
+                                      fontSize: 60,
+                                      fontWeight: FontWeight.bold),
+                                ))))),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          "${prize.quantity} lots gagnables",
+                          style: const TextStyle(
+                              color: TombolaColorConstants.writtenWhite,
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold),
+                        ))),
+                const Spacer(),
+                AutoSizeText(
+                  textAlign: TextAlign.justify,
+                  prize.description ?? "Pas de description",
+                  maxLines: 3,
+                  style: const TextStyle(
+                      color: TombolaColorConstants.writtenWhite,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              ]))
+        ]));
   }
 }
