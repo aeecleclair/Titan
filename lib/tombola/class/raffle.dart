a67e536709dadbb3f5c1a41123f8ba2bf37d3655
Maxime Roucher <maximeroucher@gmail.com>
2023-03-27 21:01:13 +0200

    Adapting to new backend schemas

--- lib/tombola/class/raffle.dart
+++ lib/tombola/class/raffle.dart
@@ -1,93 +1,62 @@
+import 'package:myecl/tombola/class/raffle_status_type.dart';
+import 'package:myecl/tombola/tools/functions.dart';
+
 class Raffle {
   Raffle({
     required this.name,
-    required this.startDate,
-    required this.endDate,
     required this.groupId,
+    required this.raffleStatusType,
     required this.id,
     this.description,
   });
   late final String name;
-  late final DateTime startDate;
-  late final DateTime endDate;
   late final String groupId;
+  late final RaffleStatusType raffleStatusType;
   late final String id;
   late final String? description;
 
   Raffle.fromJson(Map<String, dynamic> json) {
     name = json['name'];
-    startDate = DateTime.parse(json['start_date']);
-    endDate = DateTime.parse(json['end_date']);
     groupId = json['group_id'];
+    raffleStatusType = stringToRaffleStatusType(json['raffles_status_type']);
     id = json['id'];
     description = json['description'];
   }
 
   Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['name'] = name;
-    _data['start_date'] = startDate;
-    _data['end_date'] = endDate;
-    _data['group_id'] = groupId;
-    _data['id'] = id;
-    _data['description'] = description;
-    return _data;
-  }
-
-  Raffle.copyWith({
-    String? name,
-    DateTime? startDate,
-    DateTime? endDate,
-    String? groupId,
-    String? id,
-  }) {
-    this.name = name ?? this.name;
-    this.startDate = startDate ?? this.startDate;
-    this.endDate = endDate ?? this.endDate;
-    this.groupId = groupId ?? this.groupId;
-    this.id = id ?? this.id;
-  }
-
-  Raffle.empty() {
-    name = '';
-    startDate = DateTime.now();
-    endDate = DateTime.now();
-    groupId = '';
-    id = '';
-  }
-
-  @override
-  String toString() {
-    return 'Raffle{name: $name, startDate: $startDate, endDate: $endDate, groupId: $groupId, id: $id}';
+    final data = <String, dynamic>{};
+    data['name'] = name;
+    data['group_id'] = groupId;
+    data['raffles_status_type'] = raffleStatusTypeToString(raffleStatusType);
+    data['id'] = id;
+    data['description'] = description;
+    return data;
   }
 
   Raffle copyWith({
     String? name,
-    DateTime? startDate,
-    DateTime? endDate,
     String? groupId,
+    RaffleStatusType? raffleStatusType,
     String? id,
     String? description,
   }) =>
       Raffle(
           name: name ?? this.name,
-          startDate: startDate ?? this.startDate,
-          endDate: endDate ?? this.endDate,
           groupId: groupId ?? this.groupId,
+          raffleStatusType: raffleStatusType ?? this.raffleStatusType,
           id: id ?? this.id,
           description: description);
 
   Raffle.empty() {
     name = '';
-    startDate = DateTime.now();
-    endDate = DateTime.now();
     groupId = '';
+    raffleStatusType = RaffleStatusType.creation;
     id = '';
     description = '';
   }
 
   @override
   String toString() {
-    return 'Raffle{name: $name, startDate: $startDate, endDate: $endDate, groupId: $groupId, id: $id, description: $description}';
+    return 'Raffle(name: $name, groupId: $groupId, raffleStatusType: $raffleStatusType, id: $id, description: $description)';
   }
 }
--- lib/tombola/class/tickets.dart
+++ lib/tombola/class/tickets.dart
@@ -2,27 +2,30 @@ class Ticket {
   Ticket({
     required this.typeId,
     required this.userId,
+    required this.groupId,
     required this.winningLot,
     required this.id,
     required this.raffleId,
-    required this.price,
+    required this.unitPrice,
     required this.nbTicket,
   });
   late final String typeId;
   late final String userId;
+  late final String groupId;
   late final String winningLot;
   late final String id;
   late final String raffleId;
-  late final int price;
+  late final int unitPrice;
   late final int nbTicket;
 
   Ticket.fromJson(Map<String, dynamic> json) {
     typeId = json['type_id'];
     userId = json['user_id'];
+    groupId = json['group_id'];
     winningLot = json['winning_lot'];
     id = json['id'];
     raffleId = json['raffle_id'];
-    price = json['price'];
+    unitPrice = json['unit_rice'];
     nbTicket = json['nb_ticket'];
   }
 
@@ -30,10 +33,11 @@ class Ticket {
     final data = <String, dynamic>{};
     data['type_id'] = typeId;
     data['user_id'] = userId;
+    data['group_id'] = groupId;
     data['winning_lot'] = winningLot;
     data['id'] = id;
     data['raffle_id'] = raffleId;
-    data['price'] = price;
+    data['unitPrice'] = unitPrice;
     data['nb_ticket'] = nbTicket;
     return data;
   }
@@ -41,33 +45,36 @@ class Ticket {
   Ticket copyWith({
     String? typeId,
     String? userId,
+    String? groupId,
     String? winningLot,
     String? id,
     String? raffleId,
-    int? price,
+    int? unitPrice,
     int? nbTicket,
   }) =>
       Ticket(
           typeId: typeId ?? this.typeId,
           userId: userId ?? this.userId,
+          groupId: groupId ?? this.groupId,
           winningLot: winningLot ?? this.winningLot,
           id: id ?? this.id,
           raffleId: raffleId ?? this.raffleId,
-          price: price ?? this.price,
+          unitPrice: unitPrice ?? this.unitPrice,
           nbTicket: nbTicket ?? this.nbTicket);
 
   Ticket.empty() {
     typeId = '';
     userId = '';
+    groupId = '';
     winningLot = '';
     id = '';
     raffleId = '';
-    price = 0;
+    unitPrice = 0;
     nbTicket = 0;
   }
 
   @override
   String toString() {
-    return 'Ticket(typeId: $typeId, userId: $userId, winningLot: $winningLot, id: $id, raffleId: $raffleId, price: $price, nbTicket: $nbTicket)';
+    return 'Ticket(typeId: $typeId, userId: $userId, groupId: $groupId, winningLot: $winningLot, id: $id, raffleId: $raffleId, unitPrice: $unitPrice, nbTicket: $nbTicket)';
   }
 }
