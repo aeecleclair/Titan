70d965f613a1e1d29caf7a796fa538ae7bfb9362
Maxime Roucher <maximeroucher@gmail.com>
2023-03-28 21:46:13 +0200

    Adding display for winning tickets when raffle is closed

---
+++ lib/tombola/ui/pages/admin_page/winning_ticket_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tombola/class/tickets.dart';
+import 'package:myecl/tombola/tools/constants.dart';
+
+class WinningTicketUI extends HookConsumerWidget {
+  final Ticket ticket;
+  final VoidCallback onEdit;
+  final Future Function() onDelete;
+  const WinningTicketUI({
+    Key? key,
+    required this.ticket,
+    required this.onEdit,
+    required this.onDelete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Stack(children: [
+      Container(
+        width: 130,
+        height: 125,
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 17),
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: TombolaColorConstants.ticketback.withOpacity(0.3),
+                blurRadius: 8,
+                offset: const Offset(2, 3),
+              ),
+            ],
+            color: TombolaColorConstants.ticketback,
+            borderRadius: const BorderRadius.all(Radius.circular(30))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "${ticket.winningLot!.name} €",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              ticket.user.nickname ?? ticket.user.firstname,
+              style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              ticket.user.nickname != null
+                  ? "${ticket.user.firstname} ${ticket.user.name}"
+                  : ticket.user.name,
+              style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      )
+    ]);
+  }
+}
