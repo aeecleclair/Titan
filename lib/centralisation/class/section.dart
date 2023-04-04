400640857a917a1aa5e833a37b413e57fd99f36b
Nawfal <nawfalselmani@hotmail.fr>
2023-04-04 19:49:52 +0200

    class_section_centralisation

---
+++ lib/centralisation/class/section.dart
@@ -0,0 +1,51 @@
+﻿class Module {
+  Module({
+    required this.name,
+    required this.module_list,
+    this.expanded,
+  });
+  late final String name;
+  late final List module_list;
+  late final bool? expanded;
+
+  Module.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    module_list = json['module_list'];
+    expanded = json['expanded'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['name'] = name;
+    data['module_list'] = module_list;
+    if (expanded == null){
+      data['expanded'] = true;
+    } else {
+      data['expanded'] = expanded ;
+    };
+    return data;
+  }
+
+  Module copyWith({
+    String? name,
+    List? module_list,
+    bool? expanded,
+
+  })  =>
+      Module(
+        name: name ?? this.name,
+        module_list: module_list ?? this.module_list,
+        expanded: expanded,
+      );
+
+  Module.empty() {
+    name ='';
+    module_list=[];
+    expanded=true;
+  }
+
+  @override
+  String toString() {
+    return 'Section{name: $name, module_list: $module_list, expanded: $expanded}';
+  }
+}
