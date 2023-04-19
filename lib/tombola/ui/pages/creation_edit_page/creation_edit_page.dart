f9f1b8f5b39d57ed5b4d6e03e68c8f172e61a513
Arcturyus@gmail.com <romainpas17@gmail.com>
2023-04-19 16:13:47 +0200

    Rename PackTicket and new page rename

---
+++ lib/tombola/ui/pages/creation_edit_page/creation_edit_page.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class CreationEditPage extends HookConsumerWidget{
+  const CreationEditPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
