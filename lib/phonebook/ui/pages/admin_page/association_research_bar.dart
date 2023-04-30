84d48c795930a44a41f880986562c04b238749d9
Thonyk <timr.web@free.fr>
2023-04-30 16:00:04 +0200

    Fix errors due to previous merge

--- lib/phonebook/ui/pages/admin_page/association_research_bar.dart
+++ lib/phonebook/ui/pages/admin_page/association_research_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/phonebook/providers/association_list_provider.dart';
 import 'package:myecl/phonebook/providers/research_filter_provider.dart';
 import 'package:myecl/phonebook/tools/constants.dart';
 
