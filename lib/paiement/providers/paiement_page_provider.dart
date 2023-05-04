ad169acd69a1e22684618b089eee99ca1efc6da3
Maxime Roucher <maximeroucher@gmail.com>
2023-05-04 14:24:52 +0200

    Initializing module

---
+++ lib/paiement/providers/paiement_page_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum PaiementPage {
+  main,
+}
+
+class PaiementPageProvider extends StateNotifier<PaiementPage> {
+  PaiementPageProvider() : super(PaiementPage.main);
+
+  void setPaiementPage(PaiementPage i) {
+    state = i;
+  }
+}
+
+final paiementPageProvider =
+    StateNotifierProvider<PaiementPageProvider, PaiementPage>((ref) {
+  return PaiementPageProvider();
+});
