fa779e71535eed55acd87a5c78e73c8827d43896
Maxime Roucher <maximeroucher@gmail.com>
2023-06-13 22:07:03 +0200

    Cleanup
    Signed-off-by: Maxime Roucher <maximeroucher@gmail.com>

--- lib/loan/ui/pages/admin_page/loaners_items.dart
+++ lib/loan/ui/pages/admin_page/loaners_items.dart
@@ -5,7 +5,6 @@ import 'package:myecl/loan/class/loan.dart';
 import 'package:myecl/loan/providers/item_list_provider.dart';
 import 'package:myecl/loan/providers/item_provider.dart';
 import 'package:myecl/loan/providers/loan_page_provider.dart';
-import 'package:myecl/loan/providers/loan_provider.dart';
 import 'package:myecl/loan/providers/loaner_provider.dart';
 import 'package:myecl/loan/providers/loaners_items_provider.dart';
 import 'package:myecl/loan/tools/constants.dart';
@@ -21,7 +20,6 @@ class LoanersItems extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final loaner = ref.watch(loanerProvider);
-    final loanNotifier = ref.watch(loanProvider.notifier);
     final loanersitemsNotifier = ref.watch(loanersItemsProvider.notifier);
     final loanersItems = ref.watch(loanersItemsProvider);
     final itemListNotifier = ref.watch(itemListProvider.notifier);
