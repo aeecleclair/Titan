62e99866522a7e94e242e003a635c0892f8a13e1
Arcturyus <romainpas17@gmail.com>
2023-07-23 10:14:59 +0200

    router

--- lib/elocaps/router.dart
+++ lib/elocaps/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:myecl/drawer/class/module.dart';
+import 'package:myecl/elocaps/ui/pages/game_page/game_page.dart';
+import 'package:myecl/elocaps/ui/pages/history_page/history_page.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/main_page.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
@@ -8,6 +10,10 @@ import 'package:qlevar_router/qlevar_router.dart';
 class ElocapsRouter {
   final ProviderRef ref;
   static const String root = '/elocaps';
+  static const String history = '/history';
+  static const String game = '/game';
+  static const String admin = '/admin';
+
   static final Module module = Module(
       name: "Elocaps",
       icon: HeroIcons.rss, // Mettre un icon venant d'un svg [plus tard]
@@ -19,5 +25,7 @@ class ElocapsRouter {
         path: ElocapsRouter.root,
         builder: () => const EloCapsMainPage(),
         middleware: [AuthenticatedMiddleware(ref)],
+        children: [QRoute(builder: () => const HistoryPage() , path: history),
+        QRoute(builder: () => const GamePage() , path: game),]
       );
 }
