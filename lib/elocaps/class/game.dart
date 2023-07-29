8fda1cf367fd072e64ae7930a1bf65a28b66ca14
Arcturyus <romainpas17@gmail.com>
2023-07-29 18:32:15 +0200

    repo probably with problems

--- lib/elocaps/class/game.dart
+++ lib/elocaps/class/game.dart
@@ -1,17 +1,21 @@
 import 'package:myecl/elocaps/class/caps_mode.dart';
+import 'package:myecl/elocaps/class/game_player.dart';
 import 'package:myecl/elocaps/tools/functions.dart';
 import 'package:myecl/tools/functions.dart';
 
 class Game {
-  Game({required this.id, required this.timestamp, required this.mode});
+  Game({required this.id, required this.timestamp, required this.mode,required this.gamePlayers});
   late final String id;
   late final DateTime timestamp;
   late final CapsMode mode;
+  late final List<GamePlayer> gamePlayers;
 
   Game.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     timestamp = DateTime.parse(json['timestamp']);
     mode = stringToCapsMode(json['mode']);
+    gamePlayers = json['game_players'].map((e) => GamePlayer.fromJson(e)).toList();
+
   }
 
   Map<String, dynamic> toJson() {
@@ -19,18 +23,22 @@ class Game {
     data['id'] = id;
     data['timestamp'] = processDateToAPI(timestamp);
     data['mode'] = capsModeToString(mode);
+    data['game_players'] = gamePlayers.map((e) => e.toJson()).toList();
+
     return data;
   }
 
-  Game copyWith({DateTime? timestamp, CapsMode? mode, String? id}) 
+  Game copyWith({DateTime? timestamp, CapsMode? mode, String? id, List<GamePlayer>? gamePlayers}) 
   => Game(timestamp: timestamp ?? this.timestamp,
       mode: mode ?? this.mode,
-      id: id ?? this.id);
+      id: id ?? this.id,
+      gamePlayers: gamePlayers ?? this.gamePlayers);
 
   Game.empty() {
     id = '';
     timestamp = DateTime.now();
     mode = CapsMode.single;
+    gamePlayers = [];
   }
 
   @override
