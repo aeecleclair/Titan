1e98679ddc6234adbac322d286ec5b12bb46f2e3
Maxime Roucher <maximeroucher@gmail.com>
2023-07-29 16:09:43 +0200

    Using platform dispatcher instead of window
    Signed-off-by: Maxime Roucher <maximeroucher@gmail.com>

--- lib/drawer/providers/is_web_format_provider.dart
+++ lib/drawer/providers/is_web_format_provider.dart
@@ -1,8 +1,8 @@
-import 'dart:ui';
-
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final isWebFormatProvider = Provider((ref) {
+  final window = WidgetsBinding.instance.platformDispatcher.views.first;
   var pixelRatio = window.devicePixelRatio;
   var logicalScreenSize = window.physicalSize / pixelRatio;
   var logicalWidth = logicalScreenSize.width;
