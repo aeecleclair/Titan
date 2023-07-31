94c0aedf51b8755f3772e9ddc677191b0b43e3ab
Maxime Roucher <maximero@theodo.fr>
2023-07-31 23:48:39 +0200

    Updating Message class

--- lib/service/class/message.dart
+++ lib/service/class/message.dart
@@ -1,14 +1,16 @@
+import 'package:myecl/service/class/action.dart';
+
 class Message {
   late final String title;
   late final String content;
-  late final String actionId;
+  late final Action? action;
   late final String context;
   late final bool isVisible;
 
   Message({
     required this.title,
     required this.content,
-    required this.actionId,
+    required this.action,
     required this.context,
     required this.isVisible,
   });
@@ -16,23 +18,13 @@ class Message {
   Message.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     content = json['content'];
-    actionId = json['action_id'];
+    action = json['action'];
     context = json['context'];
     isVisible = json['is_visible'];
   }
 
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['title'] = title;
-    data['content'] = content;
-    data['actionId'] = actionId;
-    data['context'] = context;
-    data['is_visible'] = isVisible;
-    return data;
-  }
-
   @override
   String toString() {
-    return 'Message{title: $title, content: $content, actionId: $actionId, context: $context, isVisible: $isVisible}';
+    return 'Message{title: $title, content: $content, action: $action, context: $context, isVisible: $isVisible}';
   }
 }
