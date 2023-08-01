38b4dbcc66609e6d8a7da236e13b2cd6aa04b600
Maxime Roucher <maximeroucher@gmail.com>
2023-08-01 23:30:20 +0200

    Moving ignoring pointer to drawer

--- lib/drawer/ui/drawer_template.dart
+++ lib/drawer/ui/drawer_template.dart
@@ -88,7 +88,10 @@ class DrawerTemplate extends HookConsumerWidget {
                           borderRadius: BorderRadius.circular(cornerval),
                           child: Stack(
                             children: [
-                              child,
+                              IgnorePointer(
+                                ignoring: controller.isCompleted,
+                                child: child,
+                              ),
                               MouseRegion(
                                 cursor: SystemMouseCursors.click,
                                 onEnter: (event) {
