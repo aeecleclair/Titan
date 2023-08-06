f910d3b5d34858283905c956c39a1bcc3d975831
Maxime Roucher <maximeroucher@gmail.com>
2023-08-06 18:10:45 +0200

    Adding assets

--- lib/login/ui/sign_in.dart
+++ lib/login/ui/sign_in.dart
@@ -28,119 +28,114 @@ class SignIn extends HookConsumerWidget {
           controller.reverse();
         }
       },
-      child: AutofillGroup(
-        child: Form(
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("MyECL",
-                        style: GoogleFonts.elMessiri(
-                            textStyle: const TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white))),
-                  ),
-                ),
-                Expanded(
-                    flex: 5,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            children: [
-                              const Expanded(
-                                child: Image(
-                                  image: AssetImage('assets/images/logo.png'),
-                                ),
-                              ),
-                              SignInUpBar(
-                                isLoading: ref.watch(loadingrovider).when(
-                                    data: (data) => data,
-                                    error: (e, s) => false,
-                                    loading: () => false),
-                                label: LoginTextConstants.signIn,
-                                onPressed: () async {
-                                  await authNotifier.getTokenFromRequest();
-                                  ref.watch(authTokenProvider).when(
-                                      data: (token) {
-                                        QR.to(pathForwarding.path);
-                                      },
-                                      error: (e, s) {
-                                        displayToast(context, TypeMsg.error,
-                                            LoginTextConstants.loginFailed);
-                                      },
-                                      loading: () {});
-                                },
-                                color: ColorConstants.background2,
-                                icon: const HeroIcon(
-                                  HeroIcons.arrowRight,
-                                  color: ColorConstants.background2,
-                                  size: 35.0,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Spacer(
-                          flex: 1,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                                height: 40,
-                                alignment: Alignment.centerLeft,
-                                child: InkWell(
-                                  splashColor:
-                                      const Color.fromRGBO(255, 255, 255, 1),
-                                  onTap: () {
-                                    QR.to(LoginRouter.createAccount);
-                                    controller?.forward();
-                                  },
-                                  child: const Text(
-                                    LoginTextConstants.createAccount,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w800,
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                )),
-                            Container(
-                                height: 40,
-                                alignment: Alignment.centerLeft,
-                                child: InkWell(
-                                  splashColor:
-                                      const Color.fromRGBO(255, 255, 255, 1),
-                                  onTap: () {
-                                    QR.to(LoginRouter.forgotPassword);
-                                    controller?.forward();
-                                  },
-                                  child: const Text(
-                                    LoginTextConstants.forgotPassword,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w800,
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        )
-                      ],
-                    ))
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("MyECL",
+                    style: GoogleFonts.elMessiri(
+                        textStyle: const TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white))),
+              ),
             ),
-          ),
+            Expanded(
+                flex: 5,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: [
+                          const Expanded(
+                            child: Image(
+                              image: AssetImage('assets/images/logo.png'),
+                            ),
+                          ),
+                          SignInUpBar(
+                            isLoading: ref.watch(loadingrovider).when(
+                                data: (data) => data,
+                                error: (e, s) => false,
+                                loading: () => false),
+                            label: LoginTextConstants.signIn,
+                            onPressed: () async {
+                              await authNotifier.getTokenFromRequest();
+                              ref.watch(authTokenProvider).when(
+                                  data: (token) {
+                                    QR.to(pathForwarding.path);
+                                  },
+                                  error: (e, s) {
+                                    displayToast(context, TypeMsg.error,
+                                        LoginTextConstants.loginFailed);
+                                  },
+                                  loading: () {});
+                            },
+                            color: ColorConstants.background2,
+                            icon: const HeroIcon(
+                              HeroIcons.arrowRight,
+                              color: ColorConstants.background2,
+                              size: 35.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            height: 40,
+                            alignment: Alignment.centerLeft,
+                            child: InkWell(
+                              splashColor:
+                                  const Color.fromRGBO(255, 255, 255, 1),
+                              onTap: () {
+                                QR.to(LoginRouter.createAccount);
+                                controller?.forward();
+                              },
+                              child: const Text(
+                                LoginTextConstants.createAccount,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            )),
+                        Container(
+                            height: 40,
+                            alignment: Alignment.centerLeft,
+                            child: InkWell(
+                              splashColor:
+                                  const Color.fromRGBO(255, 255, 255, 1),
+                              onTap: () {
+                                QR.to(LoginRouter.forgotPassword);
+                                controller?.forward();
+                              },
+                              child: const Text(
+                                LoginTextConstants.forgotPassword,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            )),
+                      ],
+                    )
+                  ],
+                ))
+          ],
         ),
       ),
     );
