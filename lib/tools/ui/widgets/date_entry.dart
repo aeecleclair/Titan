5758b687a78c5cfbafd3eab3295eaf1cc12e5821
Maxime Roucher <maximeroucher@gmail.com>
2023-08-09 22:10:56 +0200

    Cleaning date entry up

--- lib/tools/ui/widgets/date_entry.dart
+++ lib/tools/ui/widgets/date_entry.dart
@@ -4,31 +4,21 @@ import 'package:myecl/tools/ui/widgets/text_entry.dart';
 
 class DateEntry extends StatelessWidget {
   final VoidCallback onTap;
-  final String label, suffix, prefix;
-  final bool canBeEmpty;
+  final String label;
   final bool enabled;
   final TextEditingController controller;
   final Color color, enabledColor, errorColor;
   final Widget? suffixIcon;
-  final GlobalKey<FormState>? formKey;
-  final Function(String)? onChanged;
-  final String? Function(String)? validator;
 
   const DateEntry(
       {super.key,
       required this.label,
       required this.controller,
       required this.onTap,
-      this.onChanged,
-      this.validator,
-      this.formKey,
-      this.prefix = '',
-      this.suffix = '',
       this.enabled = true,
-      this.canBeEmpty = false,
       this.color = Colors.black,
       this.enabledColor = Colors.black,
-      this.errorColor = ColorConstants.error,
+      this.errorColor = Colors.red,
       this.suffixIcon});
   @override
   Widget build(BuildContext context) {
@@ -36,21 +26,15 @@ class DateEntry extends StatelessWidget {
       onTap: onTap,
       child: SizedBox(
         child: AbsorbPointer(
-          child: Form(
-            key: formKey,
-            child: TextEntry(
-              label: label,
-              controller: controller,
-              noValueError: TextConstants.noDateError,
-              suffix: suffix,
-              prefix: prefix,
-              enabled: enabled,
-              canBeEmpty: canBeEmpty,
-              color: color,
-              enabledColor: enabledColor,
-              errorColor: errorColor,
-              suffixIcon: suffixIcon,
-            ),
+          child: TextEntry(
+            label: label,
+            controller: controller,
+            noValueError: TextConstants.noDateError,
+            enabled: enabled,
+            color: color,
+            enabledColor: enabledColor,
+            errorColor: errorColor,
+            suffixIcon: suffixIcon,
           ),
         ),
       ),
