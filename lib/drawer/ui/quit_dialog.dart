4c311fc3257f4019eab488ef71dc3e50edea5584
Maxime Roucher <maximeroucher@gmail.com>
2023-08-12 14:58:43 +0200

    Display quit popup on top of the drawer

---
+++ lib/drawer/ui/quit_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/auth/providers/openid_provider.dart';
+import 'package:myecl/drawer/providers/display_quit_popup.dart';
+import 'package:myecl/drawer/tools/constants.dart';
+import 'package:myecl/login/router.dart';
+import 'package:myecl/tools/functions.dart';
+import 'package:myecl/tools/ui/dialog.dart';
+import 'package:qlevar_router/qlevar_router.dart';
+
+class QuitDialog extends HookConsumerWidget {
+  const QuitDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final auth = ref.watch(authTokenProvider.notifier);
+    final isCachingNotifier = ref.watch(isCachingProvider.notifier);
+    final displayQuitNotifier = ref.watch(displayQuitProvider.notifier);
+    return GestureDetector(
+        onTap: () {
+          displayQuitNotifier.setDisplay(false);
+        },
+        child: Container(
+            color: Colors.black54,
+            child: CustomDialogBox(
+                descriptions: DrawerTextConstants.logingOut,
+                title: DrawerTextConstants.logOut,
+                onYes: () {
+                  auth.deleteToken();
+                  isCachingNotifier.set(false);
+                  displayToast(
+                      context, TypeMsg.msg, DrawerTextConstants.logOut);
+                  QR.to(LoginRouter.root);
+                })));
+  }
+}
