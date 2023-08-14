8bf0dd650c4ab962ebae03205792a3e5e0e27b24
Maxime Roucher <maximeroucher@gmail.com>
2023-08-14 21:12:26 +0200

    Creating centralisation template

--- lib/centralisation/ui/centralisation.dart
+++ lib/centralisation/ui/centralisation.dart
@@ -1,55 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/centralisation/providers/centralisation_page_provider.dart';
-import 'package:myecl/drawer/providers/swipe_provider.dart';
-import 'package:myecl/centralisation/ui/page_switcher.dart';
-import 'package:myecl/centralisation/ui/top_bar.dart';
+import 'package:myecl/centralisation/router.dart';
+import 'package:myecl/centralisation/tools/constants.dart';
+import 'package:myecl/tools/ui/top_bar.dart';
 
-class CentralisationHomePage extends HookConsumerWidget {
-  final SwipeControllerNotifier controllerNotifier;
-  final AnimationController controller;
-
-  const CentralisationHomePage({
-    Key? key,
-    required this.controllerNotifier,
-    required this.controller,
-  }) : super(key: key);
+class CentralisationTemplate extends StatelessWidget {
+  final Widget child;
+  const CentralisationTemplate({Key? key, required this.child})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final page = ref.watch(centralisationPageProvider);
-    final pageNotifier = ref.watch(centralisationPageProvider.notifier);
-
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: WillPopScope(
-        onWillPop: () async {
-          switch (page) {
-            case CentralisationPage.main:
-              if (!controller.isCompleted) {
-                controllerNotifier.toggle();
-                break;
-              } else {
-                return true;
-              }
-          }
-          return false;
-        },
-        child: Container(
-          color: Colors.white,
-          child: SafeArea(
-            child: IgnorePointer(
-              ignoring: controller.isCompleted,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  TopBar(
-                    controllerNotifier: controllerNotifier,
-                  ),
-                  const PageSwitcher(),
-                ],
-              ),
-            ),
+      body: Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const TopBar(
+                  title: CentralisationTextConstants.centralisation,
+                  root: CentralisationRouter.root),
+              Expanded(child: child)
+            ],
           ),
         ),
       ),
