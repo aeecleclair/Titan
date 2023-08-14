33d6276c8c6ae729ce4180d1e47013fab9a6ef6a
Maxime Roucher <maximeroucher@gmail.com>
2023-08-14 21:10:45 +0200

    Cleaning constants

--- lib/centralisation/tools/constants.dart
+++ lib/centralisation/tools/constants.dart
@@ -1,20 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:myecl/tools/constants.dart';
-
 class CentralisationTextConstants {
   static const String centralisation = "Centralisation";
-}
-
-
-class TombolaColorConstants extends ColorConstants {
-  static const Color darkText = Color.fromARGB(255, 50, 9, 1);
-  static const Color writtenDark = Color.fromARGB(255, 10, 9, 30);
-  static const Color writtenWhite = Color(0xfffafaeb);
-  static const Color gradient1 = Color(0xff1aa6b7);
-  static const Color gradient2 = Color(0xff002d40);
-  static const Color textDark = Color.fromARGB(255, 0, 23, 32);
-  static const Color redGradient1 = Color(0xFFfb6d10);
-  static const Color redGradient2 = Color.fromARGB(255, 155, 51, 10);
-  static const Color redGradient3 = Color.fromARGB(255, 255, 34, 34);
-  static const Color ticketback = Color(0xff000031);
 }
