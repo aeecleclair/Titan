c09529c83481d6b84b52586ddd95f42f715145d0
Maxime Roucher <maximeroucher@gmail.com>
2023-08-14 18:38:21 +0200

    Fixing import

--- lib/centralisation/ui/page_switcher.dart
+++ lib/centralisation/ui/page_switcher.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/centralisation/providers/centralisation_page_provider.dart';
-import 'package:myecl/centralisation/ui/pages/Main.dart';
+import 'package:myecl/centralisation/ui/pages/main_page.dart';
 
 class PageSwitcher extends ConsumerWidget {
   const PageSwitcher({Key? key}) : super(key: key);
