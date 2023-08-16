33dac19794164b424ae0661cc492051a68f64ec5
Shaîhh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-08-16 21:37:57 +0200

    new notation

--- lib/advert/providers/advert_poster_provider.dart
+++ lib/advert/providers/advert_poster_provider.dart
@@ -7,18 +7,18 @@ import 'package:myecl/advert/repositories/advert_poster_repository.dart';
 import 'package:myecl/tools/providers/single_notifier.dart';
 
 class AdvertPosterNotifier extends SingleNotifier<Image> {
-  final _advertPosterRepository = AdvertPosterRepository();
+  final advertPosterRepository = AdvertPosterRepository();
   AdvertPosterNotifier({required String token})
       : super(const AsyncValue.loading()) {
-    _advertPosterRepository.setToken(token);
+    advertPosterRepository.setToken(token);
   }
 
   Future<Image> getAdvertPoster(String id) async {
-    return await _advertPosterRepository.getAdvertPoster(id);
+    return await advertPosterRepository.getAdvertPoster(id);
   }
 
   Future<Image> updateAdvertPoster(String id, Uint8List bytes) async {
-    return await _advertPosterRepository.addAdvertPoster(bytes, id);
+    return await advertPosterRepository.addAdvertPoster(bytes, id);
   }
 }
 
