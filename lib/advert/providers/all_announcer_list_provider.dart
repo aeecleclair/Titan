7ad85ee2dfc7eb0c3526901f287cb03392f6c73e
Shaîhh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-08-16 21:28:39 +0200

    naming

--- lib/advert/providers/all_announcer_list_provider.dart
+++ lib/advert/providers/all_announcer_list_provider.dart
@@ -4,5 +4,5 @@ import 'package:myecl/advert/providers/announcer_list_provider.dart';
 
 final allAnnouncerList = Provider<List<Announcer>>((ref) {
   final deliveryProvider = ref.watch(announcerListProvider);
-  return deliveryProvider.maybeWhen(data: (loans) => loans, orElse: () => []);
+  return deliveryProvider.maybeWhen(data: (announcers) => announcers, orElse: () => []);
 });
