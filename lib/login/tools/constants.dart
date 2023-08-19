6f45baa8b1c8c84942e3d176b984c9545e5ef20d
Petitoto <Petitoto@users.noreply.github.com>
2023-08-19 16:13:03 +0200

    Fix regex dots + allow centraliens-lyon.net

--- lib/login/tools/constants.dart
+++ lib/login/tools/constants.dart
@@ -13,7 +13,7 @@ class LoginTextConstants {
   static const String emailInvalid =
       'Veuillez entrer une adresse mail de centrale.\nSi vous n\'en possédez pas, veuillez contacter Éclair';
   static const String emailRegExp =
-      r'^[\w\-.]*@((etu(-enise)?).)?ec-lyon.fr$';
+      r'^[\w\-.]*@(((etu(-enise)?)\.)?ec-lyon\.fr|centraliens-lyon\.net)$';
   static const String emptyFieldError = 'Ce champ ne peut pas être vide';
   static const String endActivation = 'Finaliser l\'activation';
   static const String endResetPassword = 'Finaliser la \nréinitialisation';
