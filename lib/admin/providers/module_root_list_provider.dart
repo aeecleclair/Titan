4f90763b1e8008b8e3886dcecc5f2b88263437de
Shaîhh <guillaume.tavernier@ecl22.ec-lyon.fr>
2023-08-20 15:29:54 +0200

    only get visible module when logged in

--- lib/admin/providers/module_root_list_provider.dart
+++ lib/admin/providers/module_root_list_provider.dart
@@ -3,6 +3,7 @@ import 'package:myecl/admin/repositories/module_visibility_repository.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
 import 'package:myecl/tools/providers/list_notifier.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
+import 'package:myecl/user/providers/user_provider.dart';
 
 class ModuleListNotifier extends ListNotifier<String> {
   ModuleVisibilityRepository repository = ModuleVisibilityRepository();
@@ -19,9 +20,12 @@ class ModuleListNotifier extends ListNotifier<String> {
 final moduleRootListProvider =
     StateNotifierProvider<ModuleListNotifier, AsyncValue<List<String>>>((ref) {
   final token = ref.watch(tokenProvider);
+  final userProvider = ref.watch(asyncUserProvider);
   ModuleListNotifier notifier = ModuleListNotifier(token: token);
-  tokenExpireWrapperAuth(ref, () async {
-    await notifier.loadMyModuleRoots();
-  });
+  userProvider.maybeWhen(
+      data: (data) => tokenExpireWrapperAuth(ref, () async {
+            await notifier.loadMyModuleRoots();
+          }),
+      orElse: () {});
   return notifier;
 });
