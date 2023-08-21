2214cd2b8830ad0fa8e431b0a699b8ee34f58038
Maxime Roucher <maximero@theodo.fr>
2023-08-21 09:54:46 +0200

    Using API response

--- lib/cinema/repositories/cinema_topic_repository.dart
+++ lib/cinema/repositories/cinema_topic_repository.dart
@@ -7,17 +7,15 @@ class CinemaTopicRepository extends Repository {
   final prefix = "cinema_";
 
   Future<bool> subscribeSession(String topic) async {
-    // return await create({}, suffix: "topics/$prefix$topic/subscribe");
-    return true;
+    return await create({}, suffix: "topics/$prefix$topic/subscribe");
   }
 
   Future<bool> unsubscribeSession(String topic) async {
-    // return await create({}, suffix: "topics/$prefix$topic/unsubscribe");
-    return true;
+    return await create({}, suffix: "topics/$prefix$topic/unsubscribe");
   }
 
   Future<List<String>> getCinemaTopics() async {
-    return List<String>.from((await getList(suffix: "topics")));
-        // .map((x) => x.split(prefix)[1]));
+    return List<String>.from(
+        (await getList(suffix: "topics")).map((x) => x.split(prefix)[1]));
   }
 }
