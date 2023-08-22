f9fc4057e24e9a257174afce0996600ed92a1f47
Maxime Roucher <maximeroucher@gmail.com>
2023-08-22 22:19:18 +0200

    Adding immediate event notification

--- lib/event/notification_service.dart
+++ lib/event/notification_service.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myecl/cinema/providers/session_list_provider.dart';
-import 'package:myecl/cinema/router.dart';
+import 'package:myecl/event/providers/event_list_provider.dart';
+import 'package:myecl/event/providers/user_event_list_provider.dart';
+import 'package:myecl/event/router.dart';
 import 'package:tuple/tuple.dart';
 
-final Map<String, Tuple2<String, StateNotifierProvider>> cinemaProviders = {
-    "session": Tuple2(
-      CinemaRouter.root,
-      sessionListProvider,
+final Map<String, Tuple2<String, StateNotifierProvider>> eventProviders = {
+    "userEvents": Tuple2(
+      EventRouter.root,
+      eventEventListProvider,
+    ),
+    "events": Tuple2(
+      EventRouter.root + EventRouter.admin,
+      eventListProvider,
     )
 };
