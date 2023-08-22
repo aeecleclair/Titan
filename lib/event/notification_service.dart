8138117e3e35e8682f3a35a0bfd02d62cdd58bea
Maxime Roucher <maximeroucher@gmail.com>
2023-08-22 23:11:11 +0200

    Adding event summary refreshing confirmed events

--- lib/event/notification_service.dart
+++ lib/event/notification_service.dart
@@ -3,6 +3,7 @@ import 'package:myecl/event/providers/confirmed_event_list_provider.dart';
 import 'package:myecl/event/providers/event_list_provider.dart';
 import 'package:myecl/event/providers/user_event_list_provider.dart';
 import 'package:myecl/event/router.dart';
+import 'package:myecl/home/router.dart';
 import 'package:tuple/tuple.dart';
 
 final Map<String, Tuple2<String, List<StateNotifierProvider>>> eventProviders =
@@ -10,6 +11,10 @@ final Map<String, Tuple2<String, List<StateNotifierProvider>>> eventProviders =
   "userEvents": Tuple2(
     EventRouter.root,
     [eventEventListProvider, confirmedEventListProvider],
+  ),
+  "confirmedEvents": Tuple2(
+    HomeRouter.root,
+    [confirmedEventListProvider],
   ),
   "events": Tuple2(
     EventRouter.root + EventRouter.admin,
