12509da3113cfefed879ea9071e1a5c74eb268af
Maxime Roucher <maximeroucher@gmail.com>
2023-08-22 22:16:03 +0200

    Adding immediate booking notification

--- lib/booking/notification_service.dart
+++ lib/booking/notification_service.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myecl/cinema/providers/session_list_provider.dart';
-import 'package:myecl/cinema/router.dart';
+import 'package:myecl/booking/providers/booking_list_provider.dart';
+import 'package:myecl/booking/providers/user_booking_list_provider.dart';
+import 'package:myecl/booking/router.dart';
 import 'package:tuple/tuple.dart';
 
-final Map<String, Tuple2<String, StateNotifierProvider>> cinemaProviders = {
-    "session": Tuple2(
-      CinemaRouter.root,
-      sessionListProvider,
-    )
+final Map<String, Tuple2<String, StateNotifierProvider>> loanProviders = {
+  "userBooking": Tuple2(
+    BookingRouter.root,
+    userBookingListProvider,
+  ),
+  "bookings": Tuple2(
+    BookingRouter.root + BookingRouter.admin,
+    bookingListProvider,
+  )
 };
