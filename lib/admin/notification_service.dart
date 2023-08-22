d0157bce11d0c9ea573e8e73f430c2fa2ad0007b
Maxime Roucher <maximeroucher@gmail.com>
2023-08-22 22:10:10 +0200

    Adding immediate admin notification

---
+++ lib/admin/notification_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myecl/admin/providers/group_list_provider.dart';
+import 'package:myecl/admin/router.dart';
+import 'package:myecl/router.dart';
+import 'package:myecl/user/providers/user_provider.dart';
+import 'package:tuple/tuple.dart';
+
+final Map<String, Tuple2<String, StateNotifierProvider>> adminProviders = {
+  "user": Tuple2(
+    AppRouter.root,
+    asyncUserProvider,
+  ),
+  "userGroups": Tuple2(
+    AppRouter.root,
+    userGroupListNotifier,
+  ),
+  "groups": Tuple2(
+    AdminRouter.root,
+    allGroupListProvider,
+  ),
+};
