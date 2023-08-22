a54bea89dfe1cb96788c2d26c49d3f369c929a01
Maxime Roucher <maximeroucher@gmail.com>
2023-08-22 22:25:17 +0200

    Adding immediate loan notification

--- lib/vote/notification_service.dart
+++ lib/vote/notification_service.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myecl/cinema/providers/session_list_provider.dart';
-import 'package:myecl/cinema/router.dart';
+import 'package:myecl/loan/providers/user_loaner_list_provider.dart';
+import 'package:myecl/loan/router.dart';
 import 'package:tuple/tuple.dart';
 
-final Map<String, Tuple2<String, StateNotifierProvider>> cinemaProviders = {
-    "session": Tuple2(
-      CinemaRouter.root,
-      sessionListProvider,
-    )
+final Map<String, Tuple2<String, StateNotifierProvider>> loanProviders = {
+  "userLoans": Tuple2(
+    LoanRouter.root,
+    userLoanerListProvider,
+  ),
+  "delayedLoans": Tuple2(
+    LoanRouter.root,
+    userLoanerListProvider,
+  ),
 };
