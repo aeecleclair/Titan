2706760f27ba80b7a3b192c8516a9f61a549b8f6
Maxime Roucher <maximeroucher@gmail.com>
2023-09-06 21:59:08 +0200

    fix: calendar details opens event details

--- lib/home/router.dart
+++ lib/home/router.dart
@@ -2,6 +2,7 @@ import 'package:either_dart/either.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:myecl/drawer/class/module.dart';
+import 'package:myecl/event/ui/pages/detail_page/detail_page.dart';
 import 'package:myecl/home/ui/home.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
@@ -9,6 +10,7 @@ import 'package:qlevar_router/qlevar_router.dart';
 class HomeRouter {
   final ProviderRef ref;
   static const String root = '/home';
+  static const String detail = '/detail';
   static final Module module = Module(
       name: "Calendrier",
       icon: const Left(HeroIcons.calendarDays),
@@ -20,5 +22,8 @@ class HomeRouter {
         path: HomeRouter.root,
         builder: () => const HomePage(),
         middleware: [AuthenticatedMiddleware(ref)],
+        children: [
+          QRoute(path: detail, builder: () => const DetailPage(isAdmin: false)),
+        ]
       );
 }
