17e6948c1f32c1b72696ca1a68cfb9833ac63c70
Maxime Roucher <maximeroucher@gmail.com>
2023-09-09 14:50:30 +0200

    feat: starting refactoring advert

--- lib/advert/ui/pages/main_page/main_page.dart
+++ lib/advert/ui/pages/main_page/main_page.dart
@@ -11,7 +11,7 @@ import 'package:myecl/advert/ui/pages/advert.dart';
 import 'package:myecl/advert/router.dart';
 import 'package:myecl/advert/ui/components/announcer_bar.dart';
 import 'package:myecl/advert/ui/components/advert_card.dart';
-import 'package:myecl/tools/ui/refresher.dart';
+import 'package:myecl/tools/ui/layouts/refresher.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 import 'package:myecl/advert/tools/constants.dart';
 
