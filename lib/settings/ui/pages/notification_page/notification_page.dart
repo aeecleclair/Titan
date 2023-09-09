dda6d8d39ac14c335e73cc5a627f6c37be90f84b
Maxime Roucher <maximeroucher@gmail.com>
2023-09-09 23:04:40 +0200

    fix: Fixing notification loader

--- lib/settings/ui/pages/notification_page/notification_page.dart
+++ lib/settings/ui/pages/notification_page/notification_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:load_switch/load_switch.dart';
+import 'package:myecl/service/class/topic.dart';
 import 'package:myecl/service/providers/topic_provider.dart';
+import 'package:myecl/service/tools/functions.dart';
 import 'package:myecl/settings/tools/constants.dart';
 import 'package:myecl/settings/ui/settings.dart';
 import 'package:myecl/tools/constants.dart';
-import 'package:myecl/tools/functions.dart';
 import 'package:myecl/tools/ui/widgets/align_left_text.dart';
 import 'package:myecl/tools/ui/builders/async_child.dart';
 import 'package:myecl/tools/ui/layouts/refresher.dart';
@@ -32,7 +34,7 @@ class NotificationPage extends HookConsumerWidget {
               AsyncChild(
                   value: topics,
                   builder: (context, topic) => Column(
-                      children: topic
+                      children: Topic.values
                           .map((e) => Padding(
                                 padding:
                                     const EdgeInsets.symmetric(vertical: 12),
@@ -40,15 +42,66 @@ class NotificationPage extends HookConsumerWidget {
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(capitalize(e.name),
+                                    Text(topicToFrenchString(e),
                                         style: const TextStyle(
                                             fontSize: 20,
                                             fontWeight: FontWeight.w700,
                                             color: ColorConstants.background2)),
-                                    Switch(
-                                        value: true,
-                                        activeColor: ColorConstants.gradient1,
-                                        onChanged: (value) {})
+                                    LoadSwitch(
+                                      value: topic.contains(e),
+                                      future: () =>
+                                          topicsNotifier.toggleSubscription(e),
+                                      height: 30,
+                                      width: 60,
+                                      curveIn: Curves.easeInBack,
+                                      curveOut: Curves.easeOutBack,
+                                      animationDuration:
+                                          const Duration(milliseconds: 500),
+                                      switchDecoration: (value) =>
+                                          BoxDecoration(
+                                        color: value
+                                            ? ColorConstants.gradient1
+                                                .withOpacity(0.3)
+                                            : Colors.grey.shade200,
+                                        borderRadius: BorderRadius.circular(30),
+                                        shape: BoxShape.rectangle,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: value
+                                                ? ColorConstants.gradient1
+                                                    .withOpacity(0.2)
+                                                : Colors.grey.withOpacity(0.2),
+                                            spreadRadius: 1,
+                                            blurRadius: 3,
+                                            offset: const Offset(0, 1),
+                                          ),
+                                        ],
+                                      ),
+                                      spinColor: (value) => value
+                                          ? ColorConstants.gradient1
+                                          : Colors.grey,
+                                      spinStrokeWidth: 2,
+                                      thumbDecoration: (value) => BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(30),
+                                        shape: BoxShape.rectangle,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: value
+                                                ? ColorConstants.gradient1
+                                                    .withOpacity(0.2)
+                                                : Colors.grey.shade200
+                                                    .withOpacity(0.2),
+                                            spreadRadius: 5,
+                                            blurRadius: 7,
+                                            offset: const Offset(0,
+                                                3), // changes position of shadow
+                                          ),
+                                        ],
+                                      ),
+                                      onChange: (v) {},
+                                      onTap: (v) {},
+                                    ),
                                   ],
                                 ),
                               ))
