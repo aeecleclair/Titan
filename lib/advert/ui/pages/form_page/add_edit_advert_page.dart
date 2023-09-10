72fb80fa43f2573a814c6542d0c975f54eefe54f
Maxime Roucher <maximeroucher@gmail.com>
2023-09-10 17:19:17 +0200

    feat: Using addEditButtonLayout on advert

--- lib/advert/ui/pages/form_page/add_edit_advert_page.dart
+++ lib/advert/ui/pages/form_page/add_edit_advert_page.dart
@@ -19,6 +19,7 @@ import 'package:myecl/advert/ui/components/announcer_bar.dart';
 import 'package:myecl/tools/functions.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
 import 'package:myecl/tools/ui/builders/waiting_button.dart';
+import 'package:myecl/tools/ui/layouts/add_edit_button_layout.dart';
 import 'package:myecl/tools/ui/widgets/text_entry.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
@@ -306,23 +307,7 @@ class AdvertAddEditAdvertPage extends HookConsumerWidget {
                                 color: Colors.white,
                                 fontSize: 25,
                                 fontWeight: FontWeight.bold)),
-                        builder: (child) => Container(
-                            width: double.infinity,
-                            padding: const EdgeInsets.only(top: 8, bottom: 12),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 10,
-                                  offset: const Offset(3, 3),
-                                ),
-                              ],
-                            ),
-                            child: child),
+                        builder: (child) => AddEditButtonLayout(child: child),
                       ),
                     ],
                   )),
