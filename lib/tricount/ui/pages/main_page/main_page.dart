75c273bacff7e89d35433ce3d9adf7660e0732b5
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 19:01:31 +0200

    feat: placing group stats on top on group cards

--- lib/tricount/ui/pages/main_page/main_page.dart
+++ lib/tricount/ui/pages/main_page/main_page.dart
@@ -3,7 +3,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/tools/ui/builders/async_child.dart';
 import 'package:myecl/tools/ui/layouts/refresher.dart';
 import 'package:myecl/tricount/providers/sharer_group_list_provider.dart';
-import 'package:myecl/tricount/ui/pages/detail_page/detail_page.dart';
 import 'package:myecl/tricount/ui/pages/main_page/sharer_group_stats.dart';
 import 'package:myecl/tricount/ui/pages/tricount.dart';
 
@@ -19,23 +18,28 @@ class TricountMainPage extends HookConsumerWidget {
           onRefresh: () async {
             sharerGroupListNotifier.loadSharerGroupList();
           },
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              children: [
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  color: Colors.red,
+          child: Stack(
+            children: [
+              Container(
+                height: 400,
+                width: double.infinity,
+                color: Colors.red,
+              ),
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    Container(height: 300),
+                    AsyncChild(
+                        value: sharerGroupList,
+                        builder: (context, sharerGroupList) => SharerGroupStats(
+                              equilibriumTransactions:
+                                  sharerGroupList[0].equilibriumTransactions,
+                            )),
+                  ],
                 ),
-                AsyncChild(
-                    value: sharerGroupList,
-                    builder: (context, sharerGroupList) => SharerGroupStats(
-                          equilibriumTransactions:
-                              sharerGroupList[0].equilibriumTransactions,
-                        )),
-              ],
-            ),
+              ),
+            ],
           )),
     );
   }
