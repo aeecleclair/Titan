07212ba11ae9323642e7bba23ab158d4c5b4dc5b
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 10:43:02 +0200

    feat: adding tricount template

---
+++ lib/tricount/ui/pages/tricount.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/ui/widgets/top_bar.dart';
+import 'package:myecl/tricount/tools/constants.dart';
+import 'package:myecl/tricount/ui/router.dart';
+
+class TricountTemplate extends HookConsumerWidget {
+  final Widget child;
+  const TricountTemplate({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: Column(
+        children: [
+          const TopBar(
+            title: TricountTextConstants.tricount,
+            root: TricountRouter.root,
+          ),
+          Expanded(child: child)
+        ],
+      ),
+    );
+  }
+}
