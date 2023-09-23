f15a52a5880389822e08a3c107af5d2c4e461333
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 10:50:09 +0200

    feat: adding sharer group add edit page

---
+++ lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/tricount/ui/pages/tricount.dart';
+
+class AddEditSharerGroupPage extends StatelessWidget {
+  const AddEditSharerGroupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TricountTemplate(child: Container());
+  }
+}
