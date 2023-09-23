ea5b5a0897cfb5bd9cf0478327a6e465b075bfc1
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 13:34:54 +0200

    feat: cross group stats returns equilibrium transaction list

--- lib/tricount/providers/cross_group_stats_provider.dart
+++ lib/tricount/providers/cross_group_stats_provider.dart
@@ -1,15 +1,17 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tricount/class/equilibrium_transaction.dart';
 import 'package:myecl/tricount/providers/sharer_group_list_provider.dart';
 import 'package:myecl/user/class/list_users.dart';
 import 'package:myecl/user/providers/user_provider.dart';
 
-final crossGroupStatsProvider = Provider<Map<SimpleUser, double>>((ref) {
+final crossGroupStatsProvider = Provider<List<EquilibriumTransaction>>((ref) {
   final sharerGroups = ref.watch(sharerGroupListProvider);
   final me = ref.watch(userProvider);
   final crossGroupStats = <SimpleUser, double>{};
   sharerGroups.whenData((sharerGroups) {
     for (final sharerGroup in sharerGroups) {
-      for (final equilibriumTransaction in sharerGroup.equilibriumTransactions) {
+      for (final equilibriumTransaction
+          in sharerGroup.equilibriumTransactions) {
         if (me.id != equilibriumTransaction.from.id) {
           if (crossGroupStats.containsKey(equilibriumTransaction.from)) {
             crossGroupStats[equilibriumTransaction.from] =
@@ -29,8 +31,14 @@ final crossGroupStatsProvider = Provider<Map<SimpleUser, double>>((ref) {
             crossGroupStats[equilibriumTransaction.to] =
                 -equilibriumTransaction.amount;
           }
+        }
       }
     }
-  }});
-  return crossGroupStats;
+  });
+  return crossGroupStats.entries
+      .map((e) => EquilibriumTransaction(
+          from: e.value > 0 ? me.toSimpleUser() : e.key,
+          to: e.value > 0 ? e.key : me.toSimpleUser(),
+          amount: e.value))
+      .toList();
 });
