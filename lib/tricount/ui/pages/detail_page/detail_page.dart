8ab31322f07980e8cc2a2ccc4dc38f0c62a84e2a
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 10:49:50 +0200

    feat: adding detail page

---
+++ lib/tricount/ui/pages/detail_page/detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/tricount/ui/pages/tricount.dart';
+
+class SharerGroupDetailPage extends StatelessWidget {
+  const SharerGroupDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TricountTemplate(child: Container());
+  }
+}
