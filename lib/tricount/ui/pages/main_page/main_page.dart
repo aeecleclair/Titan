2ccc148bc1364e33fa57e416b9ad4db544e474af
Maxime Roucher <maximeroucher@gmail.com>
2023-09-23 10:49:38 +0200

    feat: adding main page

---
+++ lib/tricount/ui/pages/main_page/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/tricount/ui/pages/tricount.dart';
+
+class TricountMainPage extends StatelessWidget {
+  const TricountMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TricountTemplate(child: Container(),);
+  }
+}
