2a6b843c32f7bb02d791d97402c5e95fa0cdd5e1
Maxime Roucher <maximeroucher@gmail.com>
2023-09-24 21:03:26 +0200

    feat: updating animation

--- lib/tricount/ui/pages/main_page/sharer_group_card.dart
+++ lib/tricount/ui/pages/main_page/sharer_group_card.dart
@@ -15,18 +15,18 @@ class SharerGroupCard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final width = 4.47 * MediaQuery.of(context).size.width / 10;
     final x = depth - offset;
-    final angle = (7.5 * x * x + 37.5 * x) * 3.14 / 180;
-    final translation = (-25 * x * x + 35 * x);
+    final angle = (22.5 * x * x + 22.5 * x) * 3.14 / 180;
+    final translation = ((30 - width) * x * x + (width + 30) * x);
     return Transform(
       alignment: Alignment.center,
       transform: Matrix4.identity()
         ..setEntry(3, 2, 0.001)
         ..rotateY(angle)
-        ..translate(translation, 0, 0)
-        ..scale(1.4 - x.abs() * 0.2, 1.0, 1.0),
+        ..scale(1 - 0.15 * x.abs())
+        ..translate(translation, 0, 0),
       child: Container(
-          margin: EdgeInsets.symmetric(vertical: 12 * x.abs()),
           decoration: BoxDecoration(
               color: Colors.red[(depth + 1) * 100],
               boxShadow: [
@@ -35,43 +35,39 @@ class SharerGroupCard extends HookConsumerWidget {
                   spreadRadius: 5,
                   blurRadius: 7,
                   offset: const Offset(0, 3),
-                )
+                ),
               ],
-              borderRadius: BorderRadius.circular(20)),
-          child: Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()
-                ..setEntry(3, 2, 0.001)
-                ..scale(1 / (1.4 - x.abs() * 0.2), 1.0, 1.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Text(
-                    sharerGroup.name,
-                    style: const TextStyle(
-                        color: Color(0xff09263d),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    "${sharerGroup.sharers.length} participants",
-                    style: const TextStyle(
-                        color: Color(0xff09263d),
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const Spacer(),
-                  Text(
-                    "${sharerGroup.totalAmount.toStringAsFixed(2)}€",
-                    style: const TextStyle(
-                        color: Color(0xff1C4668),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ))),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.red[(depth + 2) * 100]!)),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                sharerGroup.name,
+                style: const TextStyle(
+                    color: Color(0xff09263d),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "${sharerGroup.sharers.length} participants",
+                style: const TextStyle(
+                    color: Color(0xff09263d),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              Text(
+                "${sharerGroup.totalAmount.toStringAsFixed(2)}€",
+                style: const TextStyle(
+                    color: Color(0xff1C4668),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+            ],
+          )),
     );
   }
 }
