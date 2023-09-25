ad35f51863fcaab8cde21312a9b96ba9c54d67e5
Maxime Roucher <maximeroucher@gmail.com>
2023-09-25 18:58:59 +0200

    feat: making sharer cards unscrollable

--- lib/tricount/ui/pages/main_page/main_page.dart
+++ lib/tricount/ui/pages/main_page/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/token_expire_wrapper.dart';
 import 'package:myecl/tools/ui/builders/async_child.dart';
 import 'package:myecl/tools/ui/layouts/refresher.dart';
 import 'package:myecl/tricount/providers/sharer_group_list_provider.dart';
@@ -15,21 +16,42 @@ class TricountMainPage extends HookConsumerWidget {
     final sharerGroupList = ref.watch(sharerGroupListProvider);
     final sharerGroupListNotifier = ref.watch(sharerGroupListProvider.notifier);
     return TricountTemplate(
-      child: Refresher(
-          onRefresh: () async {
-            sharerGroupListNotifier.loadSharerGroupList();
-          },
+      child: RefreshIndicator(
+          onRefresh: () => tokenExpireWrapper(ref, () async {
+                sharerGroupListNotifier.loadSharerGroupList();
+              }),
           child: AsyncChild(
             value: sharerGroupList,
-            builder: (context, sharerGroupList) => SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
+            builder: (context, sharerGroupList) => SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Stack(
                 children: [
-                  SharerGroupHandler(sharerGroups: sharerGroupList),
-                  SharerGroupStats(
-                    equilibriumTransactions:
-                        sharerGroupList[0].equilibriumTransactions,
-                  )
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Column(children: [
+                      SharerGroupHandler(sharerGroups: sharerGroupList),
+                      const SizedBox(height: 150),
+                      Expanded(
+                        child: Container(
+                            width: double.infinity, color: const Color(0xff09263D)),
+                      ),
+                    ]),
+                  ),
+                  SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 300),
+                          SharerGroupStats(
+                            equilibriumTransactions:
+                                sharerGroupList[0].equilibriumTransactions,
+                          ),
+                        ],
+                      )),
                 ],
               ),
             ),
