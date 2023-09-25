9c4941c5ed053bfa0e4428e11a3353806af930b6
Maxime Roucher <maximeroucher@gmail.com>
2023-09-25 22:58:51 +0200

    feat: updating stats UI

--- lib/tricount/ui/pages/main_page/sharer_group_stats.dart
+++ lib/tricount/ui/pages/main_page/sharer_group_stats.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:myecl/tools/ui/widgets/align_left_text.dart';
@@ -11,15 +13,21 @@ class SharerGroupStats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: max(MediaQuery.of(context).size.height - 380,
+          equilibriumTransactions.length * 65 + 100),
       width: double.infinity,
       decoration: BoxDecoration(
+          gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xff1C4668), Color(0xff09263D)]),
           boxShadow: [
             BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 10,
-                offset: const Offset(0, -5))
+              color: const Color(0xff09263D).withOpacity(0.2),
+              blurRadius: 10,
+              offset: const Offset(0, -5),
+            ),
           ],
-          color: Colors.white,
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(30), topRight: Radius.circular(30))),
       child: Column(
