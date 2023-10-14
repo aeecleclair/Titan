8c4ffba081cf8e89728c2f3743185da8404faad1
Maxime Roucher <maximero@theodo.fr>
2023-10-14 17:38:56 +0200

    feat: updating can vote condition

--- lib/vote/providers/can_vote_provider.dart
+++ lib/vote/providers/can_vote_provider.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/user/providers/user_provider.dart';
+import 'package:myecl/vote/providers/voting_group_list_provider.dart';
 
 final canVoteProvider = StateProvider<bool>((ref) {
   final me = ref.watch(userProvider);
-  return me.groups
+  final votingGroupList = ref.watch(votingGroupListProvider);
+  final myGroupIds = me.groups
       .map((e) => e.id)
-      .contains("45649735-866a-49df-b04b-a13c74fd5886");
+      .toList();
+  return votingGroupList.any((e) => myGroupIds.contains(e.id));
 });
