55649d532feaefbc9c96d5ed0e90853037008b2b
Maxime Roucher <maximero@theodo.fr>
2023-10-14 16:20:39 +0200

    feat: adding voter repository

---
+++ lib/vote/repositories/voter_repository.dart
@@ -0,0 +1,24 @@
+import 'package:myecl/tools/repository/repository.dart';
+import 'package:myecl/vote/class/voter.dart';
+
+class VoterRepository extends Repository {
+  @override
+  // ignore: overridden_fields
+  final ext = "campaign/voters";
+
+  Future<bool> deleteVoter(String voterId) async {
+    return await delete("/$voterId");
+  }
+
+  Future<bool> updateVoter(Voter voter) async {
+    return await update(voter.toJson(), "/${voter.id}");
+  }
+
+  Future<Voter> createVoter(Voter voter) async {
+    return Voter.fromJson(await create(voter.toJson()));
+  }
+
+  Future<List<Voter>> getVoters() async {
+    return (await getList()).map((e) => Voter.fromJson(e)).toList();
+  }
+}
