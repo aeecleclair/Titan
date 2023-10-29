fa90d2dec308502ba9fcff0444aac12c293776f1
Maxime Roucher <maximero@theodo.fr>
2023-10-29 18:44:17 +0100

    feat: adding sharer group creation page

--- lib/tricount/tools/constants.dart
+++ lib/tricount/tools/constants.dart
@@ -1,3 +1,6 @@
 class TricountTextConstants {
+  static const String addGroup = 'Ajouter un groupe';
+  static const String editGroup = 'Modifier un groupe';
+  static const String name = 'Nom du groupe';
   static const String tricount = 'Tricount';
 }
