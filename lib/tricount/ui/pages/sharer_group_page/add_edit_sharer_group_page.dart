e9a1a7bbf846f90fd718cfe7ab414c5a5989a96e
Maxime Roucher <maximero@theodo.fr>
2023-10-29 18:44:17 +0100

    feat: adding sharer group creation page

--- lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
+++ lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/ui/widgets/align_left_text.dart';
+import 'package:myecl/tools/ui/widgets/text_entry.dart';
+import 'package:myecl/tricount/class/sharer_group.dart';
+import 'package:myecl/tricount/providers/sharer_group_provider.dart';
+import 'package:myecl/tricount/tools/constants.dart';
 import 'package:myecl/tricount/ui/pages/tricount.dart';
 
-class AddEditSharerGroupPage extends StatelessWidget {
+class AddEditSharerGroupPage extends HookConsumerWidget {
   const AddEditSharerGroupPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return TricountTemplate(child: Container());
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sharerGroup = ref.watch(sharerGroupProvider);
+    final isEdit = sharerGroup.id != SharerGroup.empty().id;
+    final name = useTextEditingController();
+    return TricountTemplate(
+        child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Form(
+                key: key,
+                child: Column(children: [
+                  const SizedBox(height: 30),
+                  AlignLeftText(
+                    isEdit
+                        ? TricountTextConstants.editGroup
+                        : TricountTextConstants.addGroup,
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Column(children: [
+                        const SizedBox(height: 30),
+                        TextEntry(
+                          label: TricountTextConstants.name,
+                          controller: name,
+                        ),
+                      ]))
+                ]))));
   }
 }
