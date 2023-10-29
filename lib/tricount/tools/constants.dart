a38233f6dea263f6d0d61821a3a04c95c5534248
Maxime Roucher <maximero@theodo.fr>
2023-10-29 19:23:42 +0100

    feat: adding user search bar

--- lib/tricount/tools/constants.dart
+++ lib/tricount/tools/constants.dart
@@ -1,6 +1,8 @@
 class TricountTextConstants {
   static const String addGroup = 'Ajouter un groupe';
   static const String editGroup = 'Modifier un groupe';
+  static const String member = 'Membre';
   static const String name = 'Nom du groupe';
   static const String tricount = 'Tricount';
+
 }
--- lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
+++ lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/token_expire_wrapper.dart';
 import 'package:myecl/tools/ui/widgets/align_left_text.dart';
 import 'package:myecl/tools/ui/widgets/text_entry.dart';
 import 'package:myecl/tricount/class/sharer_group.dart';
 import 'package:myecl/tricount/providers/sharer_group_provider.dart';
 import 'package:myecl/tricount/tools/constants.dart';
+import 'package:myecl/tricount/ui/pages/sharer_group_page/search_result.dart';
 import 'package:myecl/tricount/ui/pages/tricount.dart';
+import 'package:myecl/user/providers/user_list_provider.dart';
 
 class AddEditSharerGroupPage extends HookConsumerWidget {
   const AddEditSharerGroupPage({super.key});
@@ -14,31 +17,49 @@ class AddEditSharerGroupPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final sharerGroup = ref.watch(sharerGroupProvider);
+    final usersNotifier = ref.watch(userList.notifier);
     final isEdit = sharerGroup.id != SharerGroup.empty().id;
     final name = useTextEditingController();
+    final queryController = useTextEditingController();
     return TricountTemplate(
         child: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             child: Form(
                 key: key,
-                child: Column(children: [
-                  const SizedBox(height: 30),
-                  AlignLeftText(
-                    isEdit
-                        ? TricountTextConstants.editGroup
-                        : TricountTextConstants.addGroup,
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    color: Colors.grey,
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                      child: Column(children: [
-                        const SizedBox(height: 30),
-                        TextEntry(
-                          label: TricountTextConstants.name,
-                          controller: name,
-                        ),
-                      ]))
-                ]))));
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(children: [
+                    const SizedBox(height: 30),
+                    AlignLeftText(
+                      isEdit
+                          ? TricountTextConstants.editGroup
+                          : TricountTextConstants.addGroup,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(height: 30),
+                    TextEntry(
+                      label: TricountTextConstants.name,
+                      controller: name,
+                    ),
+                    const SizedBox(height: 20),
+                    TextEntry(
+                      label: TricountTextConstants.member,
+                      onChanged: (value) {
+                        tokenExpireWrapper(ref, () async {
+                          if (queryController.text.isNotEmpty) {
+                            await usersNotifier
+                                .filterUsers(queryController.text);
+                          } else {
+                            usersNotifier.clear();
+                          }
+                        });
+                      },
+                      canBeEmpty: true,
+                      controller: queryController,
+                    ),
+                    const SizedBox(height: 20),
+                    SearchResult(queryController: queryController),
+                  ]),
+                ))));
   }
 }
