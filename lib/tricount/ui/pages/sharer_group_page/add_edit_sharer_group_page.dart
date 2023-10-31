06bd07ec2d75e89c788cbb9331dc3b2561be325e
Maxime Roucher <maximero@theodo.fr>
2023-10-31 22:21:25 +0100

    feat: adding add edit button

--- lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
+++ lib/tricount/ui/pages/sharer_group_page/add_edit_sharer_group_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/raffle/tools/constants.dart';
+import 'package:myecl/tools/functions.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
+import 'package:myecl/tools/ui/builders/waiting_button.dart';
+import 'package:myecl/tools/ui/layouts/add_edit_button_layout.dart';
 import 'package:myecl/tools/ui/widgets/align_left_text.dart';
 import 'package:myecl/tools/ui/widgets/text_entry.dart';
 import 'package:myecl/tricount/class/sharer_group.dart';
+import 'package:myecl/tricount/providers/sharer_group_list_provider.dart';
 import 'package:myecl/tricount/providers/sharer_group_provider.dart';
 import 'package:myecl/tricount/tools/constants.dart';
 import 'package:myecl/tricount/ui/pages/sharer_group_page/search_result.dart';
 import 'package:myecl/tricount/ui/pages/sharer_group_page/sharer_group_member_chip_list.dart';
 import 'package:myecl/tricount/ui/pages/tricount.dart';
 import 'package:myecl/user/providers/user_list_provider.dart';
+import 'package:qlevar_router/qlevar_router.dart';
 
 class AddEditSharerGroupPage extends HookConsumerWidget {
   const AddEditSharerGroupPage({super.key});
@@ -18,10 +24,16 @@ class AddEditSharerGroupPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final sharerGroup = ref.watch(sharerGroupProvider);
+    final sharerGroupListNotifier = ref.watch(sharerGroupListProvider.notifier);
     final usersNotifier = ref.watch(userList.notifier);
     final isEdit = sharerGroup.id != SharerGroup.empty().id;
     final name = useTextEditingController();
     final queryController = useTextEditingController();
+
+    void displayToastWithContext(TypeMsg type, String msg) {
+      displayToast(context, type, msg);
+    }
+
     return TricountTemplate(
         child: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
@@ -70,6 +82,52 @@ class AddEditSharerGroupPage extends HookConsumerWidget {
                         ),
                         const SizedBox(height: 20),
                         SearchResult(queryController: queryController),
+                        const SizedBox(height: 50),
+                        WaitingButton(
+                          builder: (child) => AddEditButtonLayout(child: child),
+                          onTap: () async {
+                            await tokenExpireWrapper(ref, () async {
+                              SharerGroup newSharerGroup = SharerGroup(
+                                  equilibriumTransactions: [],
+                                  id: '',
+                                  name: name.text,
+                                  sharers: sharerGroup.sharers,
+                                  totalAmount: 0.0,
+                                  transactions: []);
+                              final value = isEdit
+                                  ? await sharerGroupListNotifier
+                                      .updateSharerGroup(newSharerGroup)
+                                  : await sharerGroupListNotifier
+                                      .addSharerGroup(newSharerGroup);
+                              if (value) {
+                                QR.back();
+                                if (isEdit) {
+                                  displayToastWithContext(TypeMsg.msg,
+                                      RaffleTextConstants.updatedSharerGroup);
+                                } else {
+                                  displayToastWithContext(TypeMsg.msg,
+                                      RaffleTextConstants.addedSharerGroup);
+                                }
+                              } else {
+                                if (isEdit) {
+                                  displayToastWithContext(TypeMsg.error,
+                                      RaffleTextConstants.updatingError);
+                                } else {
+                                  displayToastWithContext(TypeMsg.error,
+                                      RaffleTextConstants.addingError);
+                                }
+                              }
+                            });
+                          },
+                          child: Text(
+                              isEdit
+                                  ? RaffleTextConstants.edit
+                                  : RaffleTextConstants.add,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold)),
+                        )
                       ]))
                 ]))));
   }
