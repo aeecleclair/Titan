34cf3db403a1dcb8fb45c026d8c59afd538b8653
Maxime Roucher <maximero@theodo.fr>
2023-11-02 22:18:28 +0100

    feat: styling member list

---
+++ lib/tricount/ui/pages/detail_page/member_card.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:myecl/user/class/list_users.dart';
+
+class MemberCard extends StatelessWidget {
+  final SimpleUser member;
+  const MemberCard({super.key, required this.member});
+
+  @override
+  Widget build(BuildContext context) {
+    final name = member.nickname != null
+        ? member.nickname!.substring(0, min(member.nickname!.length, 3))
+        : member.firstname.substring(0, min(member.firstname.length, 3));
+    return Container(
+      height: 80,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Color(0xff1C4668),
+            child: Text(
+              name,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(width: 10),
+          AutoSizeText(
+            member.nickname ?? member.firstname,
+            style: const TextStyle(
+                color: Color(0xff1C4668),
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          const Spacer(),
+          HeroIcon(
+            HeroIcons.trash,
+            color: Colors.red.shade800,
+            size: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
