2dc6eeacacef8173f1294910ecab4f21adb016c7
Maxime Roucher <maximero@theodo.fr>
2023-11-02 21:49:40 +0100

    feat: adding detail page layout

--- lib/tricount/ui/pages/detail_page/detail_page.dart
+++ lib/tricount/ui/pages/detail_page/detail_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/tools/ui/layouts/refresher.dart';
+import 'package:myecl/tricount/providers/sharer_group_provider.dart';
 import 'package:myecl/tricount/ui/pages/tricount.dart';
 
-class SharerGroupDetailPage extends StatelessWidget {
+class SharerGroupDetailPage extends HookConsumerWidget {
   const SharerGroupDetailPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return TricountTemplate(child: Container());
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sharerGroup = ref.watch(sharerGroupProvider);
+    return TricountTemplate(child: Refresher(
+      onRefresh:() async {},
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200
+            ),
+            PageView(
+              children: [
+              ]
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
