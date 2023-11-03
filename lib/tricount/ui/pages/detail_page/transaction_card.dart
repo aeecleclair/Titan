1254a1cd2dbfd51a3283db18e2834fb33729bbfd
Maxime Roucher <maximero@theodo.fr>
2023-11-03 20:29:43 +0100

    feat: adding creation card style

--- lib/tricount/ui/pages/detail_page/transaction_card.dart
+++ lib/tricount/ui/pages/detail_page/transaction_card.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:myecl/tricount/class/transaction.dart';
+import 'package:myecl/tricount/tools/functions.dart';
 
 class TransactionCard extends StatelessWidget {
   final Transaction transaction;
@@ -7,6 +9,39 @@ class TransactionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 80,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: const Color(0xff09263D),
+            child: Text(
+              getAvatarName(transaction.payer),
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: AutoSizeText(transaction.title, maxLines: 1, style: const TextStyle(
+                color: Color(0xff09263D),
+                fontSize: 20,
+                fontWeight: FontWeight.bold))
+          ),
+          const SizedBox(width: 10),
+          Text(
+            "${transaction.amount.toStringAsFixed(2)}€",
+            style: const TextStyle(
+                color: Color(0xff09263D),
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
