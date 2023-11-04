e87cbf08b18eb780b3a32bd5474901ad76732926
Maxime Roucher <maximero@theodo.fr>
2023-11-04 10:28:06 +0100

    feat: using balance card

---
+++ lib/tricount/ui/pages/detail_page/balance_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/tricount/class/transaction.dart';
+
+class BalanceCard extends StatelessWidget {
+  final Transaction transaction;
+  final double maxAbsBalance;
+const BalanceCard({super.key, required this.transaction, required this.maxAbsBalance});
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
