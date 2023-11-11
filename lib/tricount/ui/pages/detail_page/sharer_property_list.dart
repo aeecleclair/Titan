bb623eb794dca25be773b72ae13f0129e1784398
Maxime Roucher <maximero@theodo.fr>
2023-11-11 17:26:41 +0100

    feat: removing function call from page view

--- lib/tricount/ui/pages/detail_page/sharer_property_list.dart
+++ lib/tricount/ui/pages/detail_page/sharer_property_list.dart
@@ -1,44 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:heroicons/heroicons.dart';
 
 class SharerPropertyList<T> extends StatelessWidget {
   final List<T> propertyList;
   final String title;
   final Widget Function(T) builder;
-  final void Function()? onTap;
   const SharerPropertyList(
       {super.key,
       required this.propertyList,
       required this.builder,
-      required this.title,
-      this.onTap});
+      required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(children: [
-        Row(
-          children: [
-            SizedBox(
-              height: 30,
-              child: Center(
-                child: Text(
-                  title,
-                  style: const TextStyle(color: Color(0xff09263D), fontSize: 20),
-                ),
-              ),
-            ),
-            const Spacer(),
-            onTap != null ? GestureDetector(
-              onTap: onTap,
-              child: const HeroIcon(
-                HeroIcons.plus,
-                color: Color(0xff09263D),
-                size: 30,
-              ),
-            ): Container()
-          ],
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          height: 30,
+          child: Text(
+            title,
+            style: const TextStyle(color: Color(0xff09263D), fontSize: 20),
+          ),
         ),
         const SizedBox(height: 10),
         ...propertyList.map((e) => builder(e))
