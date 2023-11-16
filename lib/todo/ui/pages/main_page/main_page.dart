b24bb94335bc70b8cd1f8c25fdb6cc947fa192e7
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: adding add button

--- lib/todo/ui/pages/main_page/main_page.dart
+++ lib/todo/ui/pages/main_page/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/todo/providers/todo_list_provider.dart';
 import 'package:myecl/todo/tools/constants.dart';
@@ -26,9 +27,25 @@ class TodoMainPage extends HookConsumerWidget {
               ),
             ),
           ),
+          const SizedBox(height: 10),
+          Container(
+              margin: const EdgeInsets.only(top: 10),
+              width: double.infinity,
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: const HeroIcon(HeroIcons.plus, size: 30)),
           ...todos.when(
-              data: (todos) => todos
-                  .map((todo) => TodoCard(todo: todo)),
+              data: (todos) => todos.map((todo) => TodoCard(todo: todo)),
               loading: () => [const CircularProgressIndicator()],
               error: (error, stack) => [Text(error.toString())])
         ],
