fe43c948a390f8395e01156bc243709ef6f9b513
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: adding asd edit page

---
+++ lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/todo/ui/pages/todo.dart';
+
+class AddEditTodoPage extends StatelessWidget {
+  const AddEditTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TodoTemplate(child: Container());
+  }
+}
