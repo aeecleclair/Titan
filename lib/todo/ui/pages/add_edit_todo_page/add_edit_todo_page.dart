dcf8ce659cbb34d6fb1478fbc56e424d8a90ddb2
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: handling todo creation

--- lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
+++ lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
@@ -3,16 +3,20 @@ import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/todo/class/todo.dart';
+import 'package:myecl/todo/providers/todo_list_provider.dart';
 import 'package:myecl/todo/tools/constants.dart';
 import 'package:myecl/todo/ui/pages/add_edit_todo_page/date_entry.dart';
 import 'package:myecl/todo/ui/pages/add_edit_todo_page/text_entry.dart';
 import 'package:myecl/todo/ui/pages/todo.dart';
+import 'package:myecl/tools/token_expire_wrapper.dart';
+import 'package:qlevar_router/qlevar_router.dart';
 
 class AddEditTodoPage extends HookConsumerWidget {
   const AddEditTodoPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final todoListNotifier = ref.read(todoListProvider.notifier);
     final formKey = GlobalKey<FormState>();
     final name = useTextEditingController();
     final deadline = useTextEditingController();
@@ -52,6 +56,20 @@ class AddEditTodoPage extends HookConsumerWidget {
                       GestureDetector(
                         onTap: () {
                           if (formKey.currentState!.validate()) {
+                            final newTodo = Todo(
+                                id: "",
+                                name: name.text,
+                                deadline: deadline.text.isNotEmpty
+                                    ? DateTime.parse(deadline.text)
+                                    : null,
+                                done: false,
+                                creation: DateTime.now());
+                            tokenExpireWrapper(ref, () async {
+                              final value = await todoListNotifier.addTodo(newTodo);
+                              if (value) {
+                                QR.back();
+                              }
+                            });
                           }
                         },
                         child: Container(
