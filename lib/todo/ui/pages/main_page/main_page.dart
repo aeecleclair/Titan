d548add2489a6698df32c2b3faf5577ff03849ee
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: displaying top bar on main page

--- lib/todo/ui/pages/main_page/main_page.dart
+++ lib/todo/ui/pages/main_page/main_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:myecl/todo/ui/pages/todo.dart';
 
 class TodoMainPage extends StatelessWidget {
   const TodoMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TodoTemplate(child: Container());
   }
 }
