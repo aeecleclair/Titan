cb00f1db5277dff9dc5b1a34f93aa7c5c925598f
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: importing todos

--- lib/todo/ui/pages/main_page/main_page.dart
+++ lib/todo/ui/pages/main_page/main_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/todo/providers/todo_list_provider.dart';
 import 'package:myecl/todo/ui/pages/todo.dart';
 
-class TodoMainPage extends StatelessWidget {
+class TodoMainPage extends HookConsumerWidget {
   const TodoMainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(todoListProvider);
     return TodoTemplate(child: Container());
   }
 }
