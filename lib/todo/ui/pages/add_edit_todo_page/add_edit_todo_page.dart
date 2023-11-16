186fffb558fed72000fe18d98daa0bd3b9ee1ac4
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: adding name field

--- lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
+++ lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/todo/tools/constants.dart';
+import 'package:myecl/todo/ui/pages/add_edit_todo_page/text_entry.dart';
 import 'package:myecl/todo/ui/pages/todo.dart';
 
-class AddEditTodoPage extends StatelessWidget {
+class AddEditTodoPage extends HookConsumerWidget {
   const AddEditTodoPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = GlobalKey<FormState>();
+    final name = useTextEditingController();
     return TodoTemplate(
         child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
+            physics: const BouncingScrollPhysics(),
             child: Padding(
                 padding: const EdgeInsets.only(right: 30, top: 30, left: 30),
-                child: Column(
-                  children: [
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        TodoTextConstants.todoList,
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          TodoTextConstants.todoList,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                  ],
+                      const SizedBox(height: 10),
+                      TextEntry(
+                        controller: name,
+                        isInt: false,
+                        keyboardType: TextInputType.text,
+                        label: "Name",
+                        suffix: "",
+                      )
+                    ],
+                  ),
                 ))));
   }
 }
