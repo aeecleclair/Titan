691c30538d98bfc31d12b504be7bf58312ada5d8
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: adding ui

--- lib/todo/ui/pages/main_page/todo_card.dart
+++ lib/todo/ui/pages/main_page/todo_card.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/todo/class/todo.dart';
+import 'package:myecl/todo/providers/todo_list_provider.dart';
+import 'package:myecl/tools/functions.dart';
 
-class TodoCard extends StatelessWidget {
+class TodoCard extends HookConsumerWidget {
   final Todo todo;
   const TodoCard({super.key, required this.todo});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: ListTile(
+        trailing: Checkbox(
+          value: todo.done,
+          onChanged: (value) {},
+        ),
+        title: Text(todo.name),
+        subtitle: Text(todo.deadline != null ? processDate(todo.deadline!) : ""),
+      ),
+    );
   }
 }
