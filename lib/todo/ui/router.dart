38c0a7bf5a02deb19378b7f1b86b110b07c7b36a
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:07:04 +0100

    feat: adding main page

--- lib/todo/ui/router.dart
+++ lib/todo/ui/router.dart
@@ -2,6 +2,7 @@ import 'package:either_dart/either.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:myecl/drawer/class/module.dart';
+import 'package:myecl/todo/ui/pages/main_page/main_page.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
