107b3363400d50af8055fae2cce510dc559e348a
Maxime Roucher <maximero@theodo.fr>
2023-11-16 17:08:02 +0100

    feat: adding add edit page title

--- lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
+++ lib/todo/ui/pages/add_edit_todo_page/add_edit_todo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myecl/todo/tools/constants.dart';
 import 'package:myecl/todo/ui/pages/todo.dart';
 
 class AddEditTodoPage extends StatelessWidget {
@@ -6,6 +7,25 @@ class AddEditTodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TodoTemplate(child: Container());
+    return TodoTemplate(
+        child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Padding(
+                padding: const EdgeInsets.only(right: 30, top: 30, left: 30),
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        TodoTextConstants.todoList,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ))));
   }
 }
