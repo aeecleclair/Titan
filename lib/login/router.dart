5f17b4dd0599d858455dbae8087f5fdb59c53239
Maxime Roucher <maximero@theodo.fr>
2023-11-17 18:53:08 +0100

    feat: adding deferred loading to login module

--- lib/login/router.dart
+++ lib/login/router.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/drawer/providers/is_web_format_provider.dart';
-import 'package:myecl/login/ui/app_sign_in.dart';
-import 'package:myecl/login/ui/pages/create_account_page/create_account_page.dart';
-import 'package:myecl/login/ui/pages/forget_page/forget_page.dart';
-import 'package:myecl/login/ui/pages/recover_password/recover_password_page.dart';
-import 'package:myecl/login/ui/pages/register_page/register_page.dart';
-import 'package:myecl/login/ui/web/web_sign_in.dart';
+import 'package:myecl/login/ui/app_sign_in.dart' deferred as app_sign_in;
+import 'package:myecl/login/ui/pages/create_account_page/create_account_page.dart'
+    deferred as create_account_page;
+import 'package:myecl/login/ui/pages/forget_page/forget_page.dart'
+    deferred as forget_page;
+import 'package:myecl/login/ui/pages/recover_password/recover_password_page.dart'
+    deferred as recover_password_page;
+import 'package:myecl/login/ui/pages/register_page/register_page.dart'
+    deferred as register_page;
+import 'package:myecl/login/ui/web/web_sign_in.dart' deferred as web_sign_in;
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
+import 'package:myecl/tools/middlewares/deferred_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 class LoginRouter {
@@ -20,34 +25,51 @@ class LoginRouter {
 
   QRoute accountRoute() => QRoute(
           path: createAccount,
-          builder: () => const Register(),
+          builder: () => register_page.Register(),
           pageType: const QMaterialPage(),
+          middleware: [
+            DeferredLoadingMiddleware(register_page.loadLibrary)
+          ],
           children: [
             QRoute(
               path: mailReceived,
               pageType: const QMaterialPage(),
-              builder: () => const CreateAccountPage(),
+              builder: () => create_account_page.CreateAccountPage(),
+              middleware: [
+                DeferredLoadingMiddleware(create_account_page.loadLibrary)
+              ],
             ),
           ]);
 
   QRoute passwordRoute() => QRoute(
           path: forgotPassword,
-          builder: () => const ForgetPassword(),
+          builder: () => forget_page.ForgetPassword(),
           pageType: const QMaterialPage(),
+          middleware: [
+            DeferredLoadingMiddleware(forget_page.loadLibrary)
+          ],
           children: [
             QRoute(
               path: mailReceived,
               pageType: const QMaterialPage(),
-              builder: () => const RecoverPasswordPage(),
+              builder: () => recover_password_page.RecoverPasswordPage(),
+              middleware: [
+                DeferredLoadingMiddleware(recover_password_page.loadLibrary)
+              ],
             ),
           ]);
 
   QRoute route() => QRoute(
         path: LoginRouter.root,
         builder: () => (kIsWeb && ref.watch(isWebFormatProvider))
-            ? const WebSignIn()
-            : const AppSignIn(),
+            ? web_sign_in.WebSignIn()
+            : app_sign_in.AppSignIn(),
         pageType: const QMaterialPage(),
-        middleware: [AuthenticatedMiddleware(ref)],
+        middleware: [
+          AuthenticatedMiddleware(ref),
+          DeferredLoadingMiddleware((kIsWeb && ref.watch(isWebFormatProvider))
+              ? web_sign_in.loadLibrary
+              : app_sign_in.loadLibrary),
+        ],
       );
 }
