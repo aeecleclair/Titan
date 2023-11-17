d595398275bf427939da04ba0dd470f11d634c8b
Maxime Roucher <maximero@theodo.fr>
2023-11-17 18:29:30 +0100

    feat: applying deferred loading to admin module

--- lib/admin/router.dart
+++ lib/admin/router.dart
@@ -1,12 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/admin/providers/is_admin.dart';
-import 'package:myecl/admin/ui/pages/add_association_page/add_association_page.dart';
-import 'package:myecl/admin/ui/pages/add_loaner_page/add_loaner_page.dart';
-import 'package:myecl/admin/ui/pages/edit_module_visibility/edit_module_visibility.dart';
-import 'package:myecl/admin/ui/pages/edit_page/edit_page.dart';
-import 'package:myecl/admin/ui/pages/main_page/main_page.dart';
+import 'package:myecl/admin/ui/pages/add_association_page/add_association_page.dart'
+    deferred as add_association_page;
+import 'package:myecl/admin/ui/pages/add_loaner_page/add_loaner_page.dart'
+    deferred as add_loaner_page;
+import 'package:myecl/admin/ui/pages/edit_module_visibility/edit_module_visibility.dart'
+    deferred as edit_module_visibility;
+import 'package:myecl/admin/ui/pages/edit_page/edit_page.dart'
+    deferred as edit_page;
+import 'package:myecl/admin/ui/pages/main_page/main_page.dart'
+    deferred as main_page;
 import 'package:myecl/tools/middlewares/admin_middleware.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
+import 'package:myecl/tools/middlewares/deferred_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 class AdminRouter {
@@ -20,21 +26,35 @@ class AdminRouter {
 
   QRoute route() => QRoute(
           path: AdminRouter.root,
-          builder: () => const AdminMainPage(),
+          builder: () => main_page.AdminMainPage(),
           middleware: [
             AuthenticatedMiddleware(ref),
-            AdminMiddleware(ref, isAdminProvider)
+            AdminMiddleware(ref, isAdminProvider),
+            DeferredLoadingMiddleware(main_page.loadLibrary)
           ],
           children: [
             QRoute(
                 path: addAssociation,
-                builder: () => const AddAssociationPage()),
-            QRoute(path: addLoaner, builder: () => const AddLoanerPage()),
+                builder: () => add_association_page.AddAssociationPage(),
+                middleware: [
+                  DeferredLoadingMiddleware(add_association_page.loadLibrary)
+                ]),
+            QRoute(
+                path: addLoaner,
+                builder: () => add_loaner_page.AddLoanerPage(),
+                middleware: [
+                  DeferredLoadingMiddleware(add_loaner_page.loadLibrary)
+                ]),
             QRoute(
                 path: editAssociation,
-                builder: () => const EditAssociationPage()),
+                builder: () => edit_page.EditAssociationPage(),
+                middleware: [DeferredLoadingMiddleware(edit_page.loadLibrary)]),
             QRoute(
                 path: editModuleVisibility,
-                builder: () => const EditModulesVisibilityPage()),
+                builder: () =>
+                    edit_module_visibility.EditModulesVisibilityPage(),
+                middleware: [
+                  DeferredLoadingMiddleware(edit_module_visibility.loadLibrary)
+                ]),
           ]);
 }
