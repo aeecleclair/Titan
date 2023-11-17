476d63b5bd84894ba6210b8fe9bd4f7eb005e18f
Maxime Roucher <maximero@theodo.fr>
2023-11-17 18:25:01 +0100

    fix: name

--- lib/tools/middlewares/deferred_middleware.dart
+++ lib/tools/middlewares/deferred_middleware.dart
@@ -1,9 +1,9 @@
 import 'package:qlevar_router/qlevar_router.dart';
 
-class DeferredLoader extends QMiddleware {
+class DeferredLoadingMiddleware extends QMiddleware {
   final Future<dynamic> Function() loader;
 
-  DeferredLoader(this.loader);
+  DeferredLoadingMiddleware(this.loader);
 
   @override
   Future onEnter() async {
