0a1f62a71b0bfae0fa09342b23132a5dfde954be
Maxime Roucher <maximero@theodo.fr>
2023-11-17 18:56:00 +0100

    feat: adding deferred loading to raffle module

--- lib/raffle/router.dart
+++ lib/raffle/router.dart
@@ -3,14 +3,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:myecl/drawer/class/module.dart';
 import 'package:myecl/raffle/providers/is_raffle_admin.dart';
-import 'package:myecl/raffle/ui/pages/admin_module_page/admin_module_page.dart';
-import 'package:myecl/raffle/ui/pages/creation_edit_page/creation_edit_page.dart';
-import 'package:myecl/raffle/ui/pages/main_page/main_page.dart';
-import 'package:myecl/raffle/ui/pages/pack_ticket_page/add_edit_pack_ticket_page.dart';
-import 'package:myecl/raffle/ui/pages/prize_page/add_edit_prize_page.dart';
-import 'package:myecl/raffle/ui/pages/raffle_page/raffle_page.dart';
+import 'package:myecl/raffle/ui/pages/admin_module_page/admin_module_page.dart'
+    deferred as admin_module_page;
+import 'package:myecl/raffle/ui/pages/creation_edit_page/creation_edit_page.dart'
+    deferred as creation_edit_page;
+import 'package:myecl/raffle/ui/pages/main_page/main_page.dart'
+    deferred as main_page;
+import 'package:myecl/raffle/ui/pages/pack_ticket_page/add_edit_pack_ticket_page.dart'
+    deferred as add_edit_pack_ticket_page;
+import 'package:myecl/raffle/ui/pages/prize_page/add_edit_prize_page.dart'
+    deferred as add_edit_prize_page;
+import 'package:myecl/raffle/ui/pages/raffle_page/raffle_page.dart'
+    deferred as raffle_page;
 import 'package:myecl/tools/middlewares/admin_middleware.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
+import 'package:myecl/tools/middlewares/deferred_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 class RaffleRouter {
@@ -29,32 +36,49 @@ class RaffleRouter {
   RaffleRouter(this.ref);
   QRoute route() => QRoute(
         path: RaffleRouter.root,
-        builder: () => const RaffleMainPage(),
-        middleware: [AuthenticatedMiddleware(ref)],
+        builder: () => main_page.RaffleMainPage(),
+        middleware: [
+          AuthenticatedMiddleware(ref),
+          DeferredLoadingMiddleware(main_page.loadLibrary)
+        ],
         children: [
           QRoute(
               path: admin,
-              builder: () => const AdminModulePage(),
+              builder: () => admin_module_page.AdminModulePage(),
               middleware: [
                 AdminMiddleware(ref, isRaffleAdminProvider),
+                DeferredLoadingMiddleware(admin_module_page.loadLibrary)
               ]),
           QRoute(
               path: detail,
-              builder: () => const RaffleInfoPage(),
+              builder: () => raffle_page.RaffleInfoPage(),
               middleware: [
                 AdminMiddleware(ref, isRaffleAdminProvider),
+                DeferredLoadingMiddleware(raffle_page.loadLibrary)
               ],
               children: [
                 QRoute(
                     path: creation,
-                    builder: () => const CreationPage(),
+                    builder: () => creation_edit_page.CreationPage(),
+                    middleware: [
+                      DeferredLoadingMiddleware(creation_edit_page.loadLibrary)
+                    ],
                     children: [
                       QRoute(
                           path: addEditPrize,
-                          builder: () => const AddEditPrizePage()),
+                          builder: () => add_edit_prize_page.AddEditPrizePage(),
+                          middleware: [
+                            DeferredLoadingMiddleware(
+                                add_edit_prize_page.loadLibrary)
+                          ]),
                       QRoute(
                           path: addEditPackTicket,
-                          builder: () => const AddEditPackTicketPage()),
+                          builder: () =>
+                              add_edit_pack_ticket_page.AddEditPackTicketPage(),
+                          middleware: [
+                            DeferredLoadingMiddleware(
+                                add_edit_pack_ticket_page.loadLibrary)
+                          ]),
                     ]),
               ]),
         ],
