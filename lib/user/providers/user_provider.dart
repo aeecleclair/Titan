8bd48d8f3e63d986bdf3e7460f89619f220dd6f0
Maxime Roucher <maximero@theodo.fr>
2023-11-20 22:59:24 +0100

    feat: removing user repository

--- lib/user/providers/user_provider.dart
+++ lib/user/providers/user_provider.dart
@@ -2,52 +2,59 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/adapters/users.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
+import 'package:myecl/generated/client_index.dart';
 import 'package:myecl/generated/openapi.models.swagger.dart';
 import 'package:myecl/tools/providers/single_notifier%20copy.dart';
+import 'package:myecl/tools/repository/repository2.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
-import 'package:myecl/user/repositories/user_repository.dart';
 
 class UserNotifier extends SingleNotifier2<CoreUser> {
+  final Openapi _userRepository;
   UserNotifier({required String token})
-      : _userRepository = UserRepository(token: token),
+      : _userRepository = getRepository(token),
         super(const AsyncLoading());
 
-  final UserRepository _userRepository;
-
   Future<AsyncValue<CoreUser>> loadUser(String userId) async {
-    return await load(() async => _userRepository.getUser(userId));
+    return await load(
+        () async => _userRepository.usersUserIdGet(userId: userId));
   }
 
   Future<AsyncValue<CoreUser>> loadMe() async {
-    return await load(_userRepository.getMe);
+    return await load(_userRepository.usersMeGet);
   }
 
   Future<bool> updateUser(CoreUser user) async {
     return await update(
-        (user) => _userRepository.updateUser(
-            coreUserUpdateAdminAdapter(user), user.id),
+        (user) => _userRepository.usersUserIdPatch(
+            body: coreUserUpdateAdminAdapter(user), userId: user.id),
         user);
   }
 
   Future<bool> updateMe(CoreUser user) async {
     return await update(
-        (user) async => _userRepository.updateMe(coreUserUpdateAdapter(user)),
+        (user) async =>
+            _userRepository.usersMePatch(body: coreUserUpdateAdapter(user)),
         user);
   }
 
   Future<bool> changePassword(
       String oldPassword, String newPassword, CoreUser user) async {
-    return (await _userRepository.changePassword(
-            oldPassword, newPassword, user.email))
+    return (await _userRepository.usersChangePasswordPost(
+            body: ChangePasswordRequest(
+                oldPassword: oldPassword,
+                newPassword: newPassword,
+                email: user.email)))
         .isSuccessful;
   }
 
   Future<bool> deletePersonal() async {
-    return (await _userRepository.deletePersonalData()).isSuccessful;
+    return (await _userRepository.usersMeAskDeletionPost()).isSuccessful;
   }
 
   Future<bool> askMailMigration(String mail) async {
-    return (await _userRepository.askMailMigration(mail)).isSuccessful;
+    return (await _userRepository.usersMigrateMailPost(
+            body: MailMigrationRequest(newEmail: mail)))
+        .isSuccessful;
   }
 }
 
