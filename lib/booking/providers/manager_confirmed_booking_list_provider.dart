f69c83d8c182a45d8698e64bfd19a985906a7465
Julien <120588494+julien4215@users.noreply.github.com>
2023-11-23 14:47:30 +0100

    use new repository architecture

--- lib/booking/providers/manager_confirmed_booking_list_provider.dart
+++ lib/booking/providers/manager_confirmed_booking_list_provider.dart
@@ -1,20 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myecl/auth/providers/openid_provider.dart';
 import 'package:myecl/booking/class/booking.dart';
 import 'package:myecl/booking/repositories/booking_repository.dart';
 import 'package:myecl/tools/providers/list_notifier.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
 
 class ManagerConfirmedBookingListProvider extends ListNotifier<Booking> {
-  final BookingRepository _bookingRepository = BookingRepository();
-  ManagerConfirmedBookingListProvider({required String token})
-      : super(const AsyncValue.loading()) {
-    _bookingRepository.setToken(token);
-  }
+  final BookingRepository bookingRepository;
+  ManagerConfirmedBookingListProvider({required this.bookingRepository})
+      : super(const AsyncValue.loading());
 
   Future<AsyncValue<List<Booking>>> loadConfirmedBookingForManager() async {
     return await loadList(
-        () async => _bookingRepository.getUserManageConfirmedBookingList());
+        () async => bookingRepository.getUserManageConfirmedBookingList());
   }
 
   Future<bool> addBooking(Booking booking) async {
@@ -34,8 +31,9 @@ class ManagerConfirmedBookingListProvider extends ListNotifier<Booking> {
 final managerConfirmedBookingListProvider = StateNotifierProvider<
     ManagerConfirmedBookingListProvider, AsyncValue<List<Booking>>>(
   (ref) {
-    final token = ref.watch(tokenProvider);
-    final provider = ManagerConfirmedBookingListProvider(token: token);
+    final bookingRepository = ref.watch(bookingRepositoryProvider);
+    final provider = ManagerConfirmedBookingListProvider(
+        bookingRepository: bookingRepository);
     tokenExpireWrapperAuth(
       ref,
       () async {
