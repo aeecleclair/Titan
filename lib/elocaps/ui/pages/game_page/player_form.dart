8358450c213b757fa0d1816bded1a9a460e2f8b7
Maxime Roucher <maximeroucher@gmail.com>
2023-12-08 21:34:36 +0100

    fix: using text entry

--- lib/elocaps/ui/pages/game_page/player_form.dart
+++ lib/elocaps/ui/pages/game_page/player_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/tools/token_expire_wrapper.dart';
 import 'package:myecl/elocaps/ui/pages/game_page/search.dart';
+import 'package:myecl/tools/ui/widgets/text_entry.dart';
 import 'package:myecl/user/class/list_users.dart';
 import 'package:myecl/user/providers/user_list_provider.dart';
 
@@ -27,7 +28,8 @@ class PlayerForm extends HookConsumerWidget {
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          TextFormField(
+          TextEntry(
+            label: "Joueur ${index + 1}",
             onChanged: (value) {
               isFocused.value = List.generate(4, (index) => false)..[index] = true;
               tokenExpireWrapper(ref, () async {
@@ -38,18 +40,7 @@ class PlayerForm extends HookConsumerWidget {
                 }
               });
             },
-            cursorColor: Colors.black,
             controller: queryController,
-            decoration: InputDecoration(
-              labelText: "Joueur $num",
-              floatingLabelStyle: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-              focusedBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 2.0),
-              ),
-            ),
           ),
           const SizedBox(
             height: 10,
