e5a1ec57cecefb6c347a67aa8ae306f5d9474b1a
Maxime Roucher <maximeroucher@gmail.com>
2023-12-08 17:24:52 +0100

    feat: adding redirection

--- lib/elocaps/ui/pages/main_page/main_page.dart
+++ lib/elocaps/ui/pages/main_page/main_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/elocaps/providers/player_list_provider.dart';
+import 'package:myecl/elocaps/router.dart';
 import 'package:myecl/elocaps/ui/elocaps.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/leader_board.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/podium.dart';
 import 'package:myecl/tools/ui/builders/async_child.dart';
 import 'package:myecl/tools/ui/layouts/add_edit_button_layout.dart';
+import 'package:qlevar_router/qlevar_router.dart';
 
 class EloCapsMainPage extends HookConsumerWidget {
   const EloCapsMainPage({Key? key}) : super(key: key);
@@ -26,17 +28,22 @@ class EloCapsMainPage extends HookConsumerWidget {
             ]),
           ),
         ),
-        const Positioned(
+        Positioned(
           bottom: 20,
           left: 30,
           right: 30,
-          child: AddEditButtonLayout(
-            child: Text(
-              "Jouer",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
+          child: GestureDetector(
+            onTap: () {
+              QR.to(ElocapsRouter.root + ElocapsRouter.game);
+            },
+            child: const AddEditButtonLayout(
+              child: Text(
+                "Jouer",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
           ),
         ),
