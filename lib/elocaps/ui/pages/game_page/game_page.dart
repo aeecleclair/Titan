d928e1d3ab1354283e6929de8a3e3a3719e62301
Maxime Roucher <maximeroucher@gmail.com>
2023-12-08 23:07:49 +0100

    feat: updating score inputs

--- lib/elocaps/ui/pages/game_page/game_page.dart
+++ lib/elocaps/ui/pages/game_page/game_page.dart
@@ -13,6 +13,7 @@ import 'package:myecl/elocaps/ui/pages/game_page/player_form.dart';
 import 'package:myecl/tools/ui/layouts/horizontal_list_view.dart';
 import 'package:myecl/tools/ui/layouts/item_chip.dart';
 import 'package:myecl/tools/ui/widgets/align_left_text.dart';
+import 'package:myecl/tools/ui/widgets/text_entry.dart';
 import 'package:myecl/user/class/list_users.dart';
 import 'package:myecl/user/providers/user_provider.dart';
 import 'package:qlevar_router/qlevar_router.dart';
@@ -51,6 +52,10 @@ class GamePage extends HookConsumerWidget {
           queryController: useTextEditingController(text: ""),
           user: useState(SimpleUser.empty())),
     ];
+    final scores = [
+      useTextEditingController(text: ""),
+      useTextEditingController(text: ""),
+    ];
     final players = useState(<SimpleUser>[]);
 
     return ElocapsTemplate(
@@ -78,60 +83,6 @@ class GamePage extends HookConsumerWidget {
                     ));
               },
               height: 40),
-          // if (!isGameCreated.value) ...[
-          //   const SizedBox(height: 20),
-          //   GestureDetector(
-          //       onTap: () {
-          //         showDialog(
-          //           context: context,
-          //           builder: (BuildContext context) {
-          //             return ModeDialog(
-          //               players: playersForm,
-          //             );
-          //           },
-          //         );
-          //       },
-          //       child: AnimatedBuilder(
-          //           animation: animation,
-          //           builder: (context, child) {
-          //             return Container(
-          //                 decoration: BoxDecoration(
-          //                     borderRadius: BorderRadius.circular(5),
-          //                     boxShadow: [
-          //                       BoxShadow(
-          //                         color: Colors.black.withOpacity(0.5),
-          //                         blurRadius: 7,
-          //                       )
-          //                     ],
-          //                     gradient: RadialGradient(
-          //                       colors: const [
-          //                         Color.fromARGB(255, 201, 34, 21),
-          //                         Color.fromARGB(255, 248, 187, 55),
-          //                         Color.fromARGB(255, 201, 34, 21),
-          //                         Color.fromARGB(255, 176, 16, 4),
-          //                       ],
-          //                       stops: [
-          //                         0.1 + 0.6 * animation.value,
-          //                         0.25 + 0.6 * animation.value,
-          //                         0.35 + 0.65 * animation.value,
-          //                         1,
-          //                       ],
-          //                       radius: 2,
-          //                       center: Alignment(
-          //                           0.65 +
-          //                               math.cos(3 * animation.value +
-          //                                   math.pi / 2),
-          //                           0.2 - math.sin(animation.value)),
-          //                     )),
-          //                 padding: const EdgeInsets.all(10),
-          //                 child: Text(
-          //                     "Mode actuel: ${modeChosen.toString().split('.').last}",
-          //                     style: const TextStyle(
-          //                         fontSize: 25,
-          //                         fontWeight: FontWeight.w700,
-          //                         color:
-          //                             Color.fromARGB(255, 248, 248, 248))));
-          //           })),
           const SizedBox(height: 30),
           Form(
             key: key,
@@ -164,65 +115,69 @@ class GamePage extends HookConsumerWidget {
             ),
           ),
           const SizedBox(height: 30),
-          GestureDetector(
-            onTap: () {
-              players.value = playersForm
-                  .map<SimpleUser>((e) => e.user.value)
-                  .where((user) => user.id != "")
-                  .toList();
-
-              if (modeChosen == CapsMode.cd && players.value.length == 4) {
-                isGameCreated.value = true;
-              } else if (modeChosen != CapsMode.cd &&
-                  players.value.length == 2) {
-                isGameCreated.value = true;
-              }
-            },
-            child: const MyButton(text: "Lancer la partie"),
-          ),
-          // ],
+          if (!isGameCreated.value)
+            GestureDetector(
+              onTap: () {
+                players.value = playersForm
+                    .map<SimpleUser>((e) => e.user.value)
+                    .where((user) => user.id != "")
+                    .toList();
+                if (modeChosen == CapsMode.cd && players.value.length == 4) {
+                  isGameCreated.value = true;
+                } else if (modeChosen != CapsMode.cd &&
+                    players.value.length == 2) {
+                  isGameCreated.value = true;
+                }
+              },
+              child: const MyButton(text: "Lancer la partie"),
+            ),
+          const SizedBox(height: 20),
           if (isGameCreated.value) ...[
+            const AlignLeftText("Scores finaux",
+                padding: EdgeInsets.symmetric(horizontal: 30)),
             const SizedBox(height: 20),
-            ShaderMask(
-                blendMode: BlendMode.srcIn,
-                shaderCallback: (bounds) => const LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 12, 0, 0),
-                        Color.fromARGB(255, 63, 2, 2),
-                        Color.fromARGB(255, 251, 118, 70),
-                      ],
-                      tileMode: TileMode.mirror,
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ).createShader(bounds),
-                child: const Text(
-                  "Scores finaux",
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                )),
-            const SizedBox(height: 20),
-            Form(
-                key: key,
-                child: Column(
-                    children: playersForm
-                        .map((e) => e.queryController.text != ""
-                            ? Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text("${e.queryController.text} : ",
-                                      style: const TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w700,
-                                          color:
-                                              Color.fromARGB(255, 7, 8, 14))),
-                                  Container(
-                                      child: const Text(
-                                          "ici on met le score //bug textformField à voir"))
-                                ],
-                              )
-                            : Container())
-                        .where((element) => element != Container())
-                        .toList())),
-            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Form(
+                  key: key,
+                  child: Row(
+                      children: modeChosen == CapsMode.cd
+                          ? [
+                              Expanded(
+                                child: TextEntry(
+                                  label: "Score équipe 1",
+                                  controller: scores[0],
+                                  isInt: true,
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: TextEntry(
+                                  label: "Score équipe 2",
+                                  controller: scores[1],
+                                  isInt: true,
+                                ),
+                              ),
+                            ]
+                          : [
+                              Expanded(
+                                child: TextEntry(
+                                  label: "Score joueur 1",
+                                  controller: scores[0],
+                                  isInt: true,
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: TextEntry(
+                                  label: "Score joueur 2",
+                                  controller: scores[1],
+                                  isInt: true,
+                                ),
+                              ),
+                            ])),
+            ),
+            const SizedBox(height: 30),
             GestureDetector(
                 onTap: () {
                   gameNotifier.createGame(Game(
