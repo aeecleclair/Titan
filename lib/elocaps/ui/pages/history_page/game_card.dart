4229315cb681d08dfdbe16bba0042e15826c28ee
Maxime Roucher <maximeroucher@gmail.com>
2023-12-09 23:24:08 +0100

    feat: updating history page

--- lib/elocaps/ui/pages/history_page/game_card.dart
+++ lib/elocaps/ui/pages/history_page/game_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/elocaps/class/game.dart';
-import 'package:myecl/elocaps/tools/functions.dart';
+import 'package:myecl/tools/functions.dart';
 
 class GameCard extends HookConsumerWidget {
   const GameCard({Key? key, required this.game}) : super(key: key);
@@ -10,12 +10,11 @@ class GameCard extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
+    final firstTeam = game.gamePlayers.where((element) => element.team == 1);
+    final secondTeam = game.gamePlayers.where((element) => element.team == 2);
     return Container(
-      width: MediaQuery.of(context).size.width * 0.4,
-      padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
-      margin: const EdgeInsets.all(5),
-      height: 80,
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
       decoration: const BoxDecoration(
         gradient: LinearGradient(colors: [
           Color.fromARGB(255, 115, 3, 3),
@@ -23,14 +22,72 @@ class GameCard extends HookConsumerWidget {
         ], begin: Alignment.bottomRight, end: Alignment.topLeft),
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
-      child: Center(
-          child: Text(
-        "${capsModeToString(game.mode)}\n${game.timestamp.toString()}\n${game.gamePlayers.map((e) => e.user.nickname).join(', ')}",
-        style: const TextStyle(
-          fontWeight: FontWeight.w700,
-          color: Color.fromARGB(255, 255, 252, 251),
-        ),
-      )),
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Text(
+              "${game.mode.name} - ${processDatePrint(game.timestamp.toString().split(" ")[0])}",
+              style: const TextStyle(color: Colors.white)),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    firstTeam
+                        .map((e) => e.user.nickname ?? e.user.firstname)
+                        .join(" - "),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    secondTeam
+                        .map((e) => e.user.nickname ?? e.user.firstname)
+                        .join(" - "),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    firstTeam
+                        .fold<int>(
+                            0,
+                            (previousValue, element) =>
+                                previousValue + element.quarters)
+                        .toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    secondTeam
+                        .fold<int>(
+                            0,
+                            (previousValue, element) =>
+                                previousValue + element.quarters)
+                        .toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
