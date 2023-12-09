e9fdf0af5a6488b41c270df6636e60e5429cd697
Maxime Roucher <maximeroucher@gmail.com>
2023-12-09 23:04:24 +0100

    feat: history cleanup

--- lib/elocaps/ui/pages/history_page/history_page.dart
+++ lib/elocaps/ui/pages/history_page/history_page.dart
@@ -3,6 +3,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/elocaps/providers/player_histo_provider.dart';
 import 'package:myecl/elocaps/ui/elocaps.dart';
 import 'package:myecl/elocaps/ui/pages/history_page/game_card.dart';
+import 'package:myecl/tools/ui/builders/async_child.dart';
+import 'package:myecl/tools/ui/layouts/refresher.dart';
+import 'package:myecl/tools/ui/widgets/align_left_text.dart';
 import 'package:myecl/user/providers/user_provider.dart';
 
 class HistoryPage extends HookConsumerWidget {
@@ -11,26 +14,25 @@ class HistoryPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
-    final myHisto = ref.watch(playerHistoProvider);
+    final history = ref.watch(playerHistoProvider);
+    final historyNotifier = ref.read(playerHistoProvider.notifier);
 
     return ElocapsTemplate(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
+        child: Refresher(
+      onRefresh: () async {
+        historyNotifier.loadHisto(user.id);
+      },
       child: Column(
         children: [
           const SizedBox(height: 20),
-          Text(
-              "Bonjour bienvenue dans ton historique de partie ${user.nickname}"),
+          const AlignLeftText("Historique",
+              padding: EdgeInsets.symmetric(horizontal: 30)),
           const SizedBox(height: 20),
-          ...myHisto.when(
-            data: (data) => data.reversed.map((e) => GameCard(game: e)),
-            error: (error, stackTrace) {
-              return [Text("erreur : ${error.toString()}")];
-            },
-            loading: () {
-              return [const CircularProgressIndicator()];
-            },
-          ),
+          AsyncChild(
+              value: history,
+              builder: (context, games) => Column(
+                  children:
+                      games.reversed.map((e) => GameCard(game: e)).toList())),
         ],
       ),
     ));
