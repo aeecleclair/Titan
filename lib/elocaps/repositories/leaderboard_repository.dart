68d4b536dd4185341de7d032a370eebf29fdb5c7
Maxime Roucher <maximeroucher@gmail.com>
2023-12-11 20:59:05 +0100

    feat: loading player

--- lib/elocaps/repositories/leaderboard_repository.dart
+++ lib/elocaps/repositories/leaderboard_repository.dart
@@ -6,7 +6,8 @@ class LeaderBoardRepository extends Repository {
   // ignore: overridden_fields
   final ext = "elocaps/leaderboard";
 
-   Future<List<Player>> getLeaderBoard(String mode) async {
-    return List<Player>.from((await getList(suffix:"?mode=$mode")).map((x) => Player.fromJson(x)));
+  Future<List<Player>> getLeaderBoard(String mode) async {
+    return List<Player>.from(
+        (await getList(suffix: "?mode=$mode")).map((x) => Player.fromJson(x)));
   }
 }
