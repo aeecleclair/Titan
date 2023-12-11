f65399241bdbb2af7b901d14918ea29a9b234fea
Maxime Roucher <maximeroucher@gmail.com>
2023-12-11 22:55:12 +0100

    feat: adding leaderboard for every mode

--- lib/elocaps/ui/pages/main_page/main_page.dart
+++ lib/elocaps/ui/pages/main_page/main_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/elocaps/class/caps_mode.dart';
+import 'package:myecl/elocaps/providers/leader_board_player_map_notifier.dart';
+import 'package:myecl/elocaps/providers/mode_chosen_provider.dart';
 import 'package:myecl/elocaps/providers/player_list_provider.dart';
 import 'package:myecl/elocaps/router.dart';
 import 'package:myecl/elocaps/ui/button.dart';
 import 'package:myecl/elocaps/ui/elocaps.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/leader_board.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/podium.dart';
-import 'package:myecl/tools/ui/builders/async_child.dart';
+import 'package:myecl/tools/ui/builders/auto_loader_child.dart';
+import 'package:myecl/tools/ui/layouts/horizontal_list_view.dart';
+import 'package:myecl/tools/ui/layouts/item_chip.dart';
+import 'package:myecl/tools/ui/widgets/align_left_text.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 class EloCapsMainPage extends HookConsumerWidget {
@@ -14,19 +20,52 @@ class EloCapsMainPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final leaderBoardPlayers = ref.watch(playerListProvider);
+    final leaderBoardPlayerNotifier = ref.watch(playerListProvider.notifier);
+    final leaderBoardPlayerList = ref.watch(leaderBoardPlayerListProvider);
+    final leaderBoardPlayerListNotifier =
+        ref.read(leaderBoardPlayerListProvider.notifier);
+    final modeChosen = ref.watch(modeChosenProvider);
+    final modeChosenNotifier = ref.watch(modeChosenProvider.notifier);
 
     return ElocapsTemplate(
         child: Stack(
       children: [
-        AsyncChild(
-          value: leaderBoardPlayers,
-          builder: (context, players) => const SingleChildScrollView(
-            child: Column(children: [
-              Podium(),
-              LeaderBoard(),
-            ]),
-          ),
+        Column(
+          children: [
+            const SizedBox(height: 20),
+            const AlignLeftText("Mode de jeu",
+                padding: EdgeInsets.symmetric(horizontal: 30)),
+            const SizedBox(height: 20),
+            HorizontalListView.builder(
+                items: CapsMode.values,
+                itemBuilder: (context, item, i) {
+                  final selected = item == modeChosen;
+                  return ItemChip(
+                      selected: selected,
+                      onTap: () {
+                        modeChosenNotifier.setMode(item);
+                      },
+                      child: Text(
+                        item.name,
+                        style: TextStyle(
+                            color: selected ? Colors.white : Colors.black),
+                      ));
+                },
+                height: 40),
+            const SizedBox(height: 20),
+            AutoLoaderChild(
+              value: leaderBoardPlayerList,
+              mapKey: modeChosen,
+              notifier: leaderBoardPlayerListNotifier,
+              listLoader: leaderBoardPlayerNotifier.loadRanking,
+              dataBuilder: (context, players) => SingleChildScrollView(
+                child: Column(children: [
+                  Podium(players: players),
+                  LeaderBoard(players: players),
+                ]),
+              ),
+            ),
+          ],
         ),
         Positioned(
           bottom: 20,
@@ -34,6 +73,7 @@ class EloCapsMainPage extends HookConsumerWidget {
           right: 0,
           child: GestureDetector(
             onTap: () {
+              modeChosenNotifier.setMode(CapsMode.values.first);
               QR.to(ElocapsRouter.root + ElocapsRouter.game);
             },
             child: const MyButton(
--- lib/elocaps/ui/pages/main_page/podium.dart
+++ lib/elocaps/ui/pages/main_page/podium.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/elocaps/providers/player_list_provider.dart';
+import 'package:myecl/elocaps/class/player.dart';
 import 'package:myecl/elocaps/ui/pages/main_page/podium_card.dart';
-import 'package:myecl/tools/ui/builders/async_child.dart';
 import 'package:myecl/user/providers/user_provider.dart';
 
 class Podium extends HookConsumerWidget {
-const Podium({super.key});
+  final List<Player> players;
+  const Podium({super.key, required this.players});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref){
-    final leaderBoardPlayers = ref.watch(playerListProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
     final me = ref.watch(userProvider);
-    return AsyncChild(
-      value: leaderBoardPlayers,
-      builder: (context, players) => Container(
-                height: 200,
+    return Container(
+      height: 200,
       padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -23,24 +20,24 @@ const Podium({super.key});
         children: [
           if (players.length > 1)
             PodiumCard(
-                player: players[1],
-                index: 1,
-                isMe: players[1].user.id == me.id,
-              ),
+              player: players[1],
+              index: 1,
+              isMe: players[1].user.id == me.id,
+            ),
           if (players.isNotEmpty)
             PodiumCard(
-                player: players[0],
-                index: 0,
-                isMe: players[0].user.id == me.id,
-              ),
+              player: players[0],
+              index: 0,
+              isMe: players[0].user.id == me.id,
+            ),
           if (players.length > 2)
-             PodiumCard(
-                player: players[2],
-                index: 2,
-                isMe: players[2].user.id == me.id,
-              ),
+            PodiumCard(
+              player: players[2],
+              index: 2,
+              isMe: players[2].user.id == me.id,
+            ),
         ],
       ),
-    ));
+    );
   }
 }
