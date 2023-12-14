20bd17780d4e00b46297e9b18a6adc1123b03de9
Maxime Roucher <maximeroucher@gmail.com>
2023-12-14 13:35:11 +0100

    feat: centering podium card

--- lib/elocaps/ui/pages/main_page/podium_card.dart
+++ lib/elocaps/ui/pages/main_page/podium_card.dart
@@ -15,14 +15,28 @@ class PodiumCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color color = ElocapsColorConstant.podium_color[index];
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        isMe
-            ? CircleAvatar(
-                radius: 27,
-                backgroundColor: Colors.black,
-                child: CircleAvatar(
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          isMe
+              ? CircleAvatar(
+                  radius: 27,
+                  backgroundColor: Colors.black,
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white,
+                    child: ClipOval(
+                      child: Image.network(
+                        "https://www.gravatar.com/avatar/${player.user.id}?d=identicon",
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                )
+              : CircleAvatar(
                   radius: 25,
                   backgroundColor: Colors.white,
                   child: ClipOval(
@@ -34,63 +48,51 @@ class PodiumCard extends StatelessWidget {
                     ),
                   ),
                 ),
-              )
-            : CircleAvatar(
-                radius: 25,
-                backgroundColor: Colors.white,
-                child: ClipOval(
-                  child: Image.network(
-                    "https://www.gravatar.com/avatar/${player.user.id}?d=identicon",
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
+          const SizedBox(height: 7),
+          Text(player.user.nickname ?? player.user.firstname,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(height: 3),
+          Container(
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: [
+                  color.withOpacity(0.5),
+                  color,
+                ],
+                center: Alignment.topLeft,
+                radius: 1.5,
+              ),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: color.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3),
                 ),
-              ),
-        const SizedBox(height: 7),
-        Text(player.user.nickname ?? player.user.firstname,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                fontWeight: FontWeight.bold)),
-        const SizedBox(height: 3),
-        Container(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-              colors: [
-                color.withOpacity(0.5),
-                color,
               ],
-              center: Alignment.topLeft,
-              radius: 1.5,
             ),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(15),
-              topRight: Radius.circular(15),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: color.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 5,
-                offset: const Offset(0, 3),
+            height: 90.0 - 15 * index - (isMe ? 5 : 0),
+            width: 60,
+            padding: const EdgeInsets.only(top: 7),
+            child: Text(
+              player.elo.toString(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 17,
+                color: Colors.white,
               ),
-            ],
-          ),
-          height: 90.0 - 15 * index - (isMe ? 5 : 0),
-          width: 60,
-          padding: const EdgeInsets.only(top: 7),
-          child: Text(
-            player.elo.toString(),
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 17,
-              color: Colors.white,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
