d79a223c547911ab04dfe7f2c21ee9b516008939
Maxime Roucher <maximeroucher@gmail.com>
2024-01-18 18:39:49 +0100

    feat: updating balance class

--- lib/tricount/class/balance.dart
+++ lib/tricount/class/balance.dart
@@ -1,61 +1,40 @@
 class Balance {
   late final String userId;
-  late final String reimbursementId;
   late final double amount;
-  late final double totalExpense;
-  late final double totalPayment;
 
   Balance(
       {required this.userId,
-      required this.reimbursementId,
-      required this.amount,
-      required this.totalExpense,
-      required this.totalPayment});
+      required this.amount});
 
   Balance.fromJson(Map<String, dynamic> json) {
     userId = json['user_id'];
-    reimbursementId = json['reimbursement_id'];
     amount = json['amount'];
-    totalExpense = json['total_expense'];
-    totalPayment = json['total_payment'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['user_id'] = userId;
-    data['reimbursement_id'] = reimbursementId;
     data['amount'] = amount;
-    data['total_expense'] = totalExpense;
-    data['total_payment'] = totalPayment;
     return data;
   }
 
   @override
   String toString() {
-    return 'Balance{userId: $userId, reimbursementId: $reimbursementId, amount: $amount, totalExpense: $totalExpense, totalPayment: $totalPayment}';
+    return 'Balance{userId: $userId, amount: $amount}';
   }
 
   Balance copyWith({
     String? userId,
-    String? reimbursementId,
     double? amount,
-    double? totalExpense,
-    double? totalPayment,
   }) {
     return Balance(
       userId: userId ?? this.userId,
-      reimbursementId: reimbursementId ?? this.reimbursementId,
       amount: amount ?? this.amount,
-      totalExpense: totalExpense ?? this.totalExpense,
-      totalPayment: totalPayment ?? this.totalPayment,
     );
   }
 
   Balance.empty()
       : this(
             userId: '',
-            reimbursementId: '',
-            amount: 0.0,
-            totalExpense: 0.0,
-            totalPayment: 0.0);
+            amount: 0.0);
 }
