d05fd9cf0960744984ff31ba75ac20c8b66e6d05
Shaîhh <guillaume.tavernier@etu.ec-lyon.fr>
2024-02-03 21:00:52 +0100

    fix: add radius to scrollbar to make it consistent on Android devices

--- lib/drawer/ui/list_module.dart
+++ lib/drawer/ui/list_module.dart
@@ -14,6 +14,7 @@ class ListModule extends HookConsumerWidget {
     return Scrollbar(
       controller: scrollController,
       interactive: true,
+      radius: const Radius.circular(8),
       thumbVisibility: true,
       child: SingleChildScrollView(
         controller: scrollController,
