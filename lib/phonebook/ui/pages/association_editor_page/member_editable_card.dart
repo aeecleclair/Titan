2938c5351329f3c49ae6b0bfd6c2bd87ba5e1307
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 15:57:00 +0100

    Updating association editing page

--- lib/phonebook/ui/pages/association_editor_page/member_editable_card.dart
+++ lib/phonebook/ui/pages/association_editor_page/member_editable_card.dart
@@ -36,116 +36,128 @@ class MemberEditableCard extends HookConsumerWidget {
       displayToast(context, type, msg);
     }
 
-
     return Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(),
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(20)),
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: const Offset(2, 3),
+                  ),
+                ],
+              ),
+              child: profilePicture.when(
+                data: (picture) {
+                  return CircleAvatar(
+                    radius: 20,
+                    backgroundImage: picture.isEmpty
+                        ? const AssetImage('assets/images/logo_alpha.png')
+                        : Image.memory(picture).image,
+                  );
+                },
+                loading: () {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+                error: (e, s) {
+                  return const Center(
+                    child: Text("Error"),
+                  );
+                },
+              ),
             ),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(2, 3),
-                      ),
-                    ],
-                  ),
-                  child: profilePicture.when(
-                    data: (picture) {
-                      return CircleAvatar(
-                        radius: 20,
-                        backgroundImage: picture.isEmpty
-                            ? const AssetImage('assets/images/logo_alpha.png')
-                            : Image.memory(picture).image,
-                      );
-                    },
-                    loading: () {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    },
-                    error: (e, s) {
-                      return const Center(
-                        child: Text(
-                            PhonebookTextConstants.errorLoadAssociationPicture),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: 400,
-                  child: Column(
-                    children: [
-                      Text(
-                        "${member.member.name} ${member.member.firstname} (${member.member.nickname})",
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        member.member.email,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                Text(member.memberships.firstWhere((element) => element.association.id == association.id).apparentName,
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 400,
+              child: Column(
+                children: [
+                  Text(
+                    "${member.member.name} ${member.member.firstname} (${member.member.nickname})",
                     style: const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                    )),
-                const Spacer(),
-                EditionButton(onEdition: () async {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return MembershipDialog(
-                            apparentNameController: controller,
-                            title: PhonebookTextConstants.editMembership,
-                            defaultText: member.memberships.firstWhere((element) => element.association.id == association.id).apparentName,
-                            onConfirm: () async {
-                              final result = await associationNotifier.updateMember(
-                                  association,
-                                  member.toMember(),
-                                  memberRoleTags,
-                                  controller.text);
-                              if (result) {
-                                await associationMembersNotifier.loadMembers(association.id);
-                                displayToastWithContext(TypeMsg.msg, PhonebookTextConstants.updatedMember);
-                              } else {
-                                displayToastWithContext(TypeMsg.error, PhonebookTextConstants.updatingError);
-                              }
-                            });
-                      });
-                }),
-                const SizedBox(width: 10),
-                DeleteButton(onDelete: 
-                  () async {
-                    final result = await associationNotifier.deleteMember(association, member);
-                    await associationMembersNotifier.loadMembers(association.id);
-                    if (result) {
-                      displayToastWithContext(TypeMsg.msg, PhonebookTextConstants.deletedMember);
-                    } else {
-                      displayToastWithContext(TypeMsg.error, PhonebookTextConstants.deletingError);
-                    }
-                  },)
-              ],
-            ));
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    member.member.email,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            Text(
+                member.memberships
+                    .firstWhere(
+                        (element) => element.association.id == association.id)
+                    .apparentName,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )),
+            const Spacer(),
+            EditionButton(onEdition: () async {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return MembershipDialog(
+                        apparentNameController: controller,
+                        title: PhonebookTextConstants.editMembership,
+                        defaultText: member.memberships
+                            .firstWhere((element) =>
+                                element.association.id == association.id)
+                            .apparentName,
+                        onConfirm: () async {
+                          final result = await associationNotifier.updateMember(
+                              association,
+                              member.toMember(),
+                              memberRoleTags,
+                              controller.text);
+                          if (result) {
+                            await associationMembersNotifier
+                                .loadMembers(association.id);
+                            displayToastWithContext(TypeMsg.msg,
+                                PhonebookTextConstants.updatedMember);
+                          } else {
+                            displayToastWithContext(TypeMsg.error,
+                                PhonebookTextConstants.updatingError);
+                          }
+                        });
+                  });
+            }),
+            const SizedBox(width: 10),
+            DeleteButton(
+              onDelete: () async {
+                final result =
+                    await associationNotifier.deleteMember(association, member);
+                await associationMembersNotifier.loadMembers(association.id);
+                if (result) {
+                  displayToastWithContext(
+                      TypeMsg.msg, PhonebookTextConstants.deletedMember);
+                } else {
+                  displayToastWithContext(
+                      TypeMsg.error, PhonebookTextConstants.deletingError);
+                }
+              },
+            )
+          ],
+        ));
   }
 }
