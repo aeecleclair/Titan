1be5fc02fb4ac7ae153b756492cff7347a919e78
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 15:57:04 +0100

    feat: using common top bar

--- lib/phonebook/ui/phonebook.dart
+++ lib/phonebook/ui/phonebook.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/drawer/providers/animation_provider.dart';
-import 'package:myecl/drawer/providers/swipe_provider.dart';
-import 'package:myecl/phonebook/ui/top_bar.dart';
+import 'package:myecl/phonebook/router.dart';
+import 'package:myecl/phonebook/tools/constants.dart';
+import 'package:myecl/tools/ui/widgets/top_bar.dart';
 
 class PhonebookTemplate extends HookConsumerWidget {
   final Widget child;
@@ -10,27 +10,15 @@ class PhonebookTemplate extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final animationNotifier = ref.watch(animationProvider.notifier);
-    final controller =
-        ref.watch(swipeControllerProvider(animationNotifier.animation!));
-    final controllerNotifier = ref
-        .watch(swipeControllerProvider(animationNotifier.animation!).notifier);
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: SafeArea(
-          child: IgnorePointer(
-            ignoring: controller.isCompleted,
-            child: Column(
-              children: [
-                TopBar(
-                  controllerNotifier: controllerNotifier,
-                ),
-                Expanded(child: child),
-              ],
-            ),
+      body: Column(
+        children: [
+          const TopBar(
+            title: PhonebookTextConstants.phonebook,
+            root: PhonebookRouter.root,
           ),
-        ),
+          Expanded(child: child),
+        ],
       ),
     );
   }
