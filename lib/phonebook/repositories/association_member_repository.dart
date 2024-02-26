d211d6b268afea184f83f216d4af8c92760a73ca
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 15:57:01 +0100

    Cleanup
    Signed-off-by: Maxime Roucher <maximeroucher@gmail.com>

--- lib/phonebook/repositories/association_member_repository.dart
+++ lib/phonebook/repositories/association_member_repository.dart
@@ -1,5 +1,4 @@
 import 'package:myecl/phonebook/class/complete_member.dart';
-import 'package:myecl/phonebook/tools/fake_class.dart';
 import 'package:myecl/tools/repository/repository.dart';
 
 class AssociationMemberRepository extends Repository {
@@ -7,9 +6,10 @@ class AssociationMemberRepository extends Repository {
   // ignore: overridden_fields
   final ext = "phonebook/associations/";
 
-  Future<List<CompleteMember>> getAssociationMemberList(String associationId) async {
-    // return fakeMembersList.where((element) => element.memberships.map((e) => e.association.id).contains(associationId)).toList();
+  Future<List<CompleteMember>> getAssociationMemberList(
+      String associationId) async {
     return List<CompleteMember>.from(
-       (await getList(suffix: "$associationId/members")).map((x) => CompleteMember.fromJSON(x)));
+        (await getList(suffix: "$associationId/members"))
+            .map((x) => CompleteMember.fromJSON(x)));
   }
 }
