af812c9dad558c755b8bbd6b154a5690830a597b
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 15:57:04 +0100

    feat: adapting cards UI

--- lib/phonebook/ui/pages/main_page/association_card.dart
+++ lib/phonebook/ui/pages/main_page/association_card.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/phonebook/class/association.dart';
 import 'package:myecl/phonebook/providers/association_picture_provider.dart';
 import 'package:myecl/phonebook/providers/associations_pictures_provider.dart';
 import 'package:myecl/phonebook/providers/complete_member_provider.dart';
-import 'package:myecl/phonebook/tools/constants.dart';
-import 'package:myecl/tools/token_expire_wrapper.dart';
+import 'package:myecl/tools/ui/builders/auto_loader_child.dart';
+import 'package:myecl/tools/ui/layouts/card_layout.dart';
 
 class AssociationCard extends HookConsumerWidget {
   const AssociationCard({
@@ -30,87 +29,29 @@ class AssociationCard extends HookConsumerWidget {
     final member = ref.watch(completeMemberProvider);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: GestureDetector(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: GestureDetector(
           onTap: onClicked,
-          child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 5),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        blurRadius: 5,
-                        spreadRadius: 2)
-                  ]),
+          child: CardLayout(
+              margin: EdgeInsets.zero,
               child: Row(children: [
-                associationPictures.when(
-                  data: (pictures) {
-                    if (pictures[association] != null) {
-                      return pictures[association]!.when(
-                        data: (picture) {
-                          if (picture.isNotEmpty) {
-                            return Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: picture.first.image,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            );
-                          } else {
-                            Future.delayed(
-                                const Duration(milliseconds: 1),
-                                () => associationPicturesNotifier.setTData(
-                                    association, const AsyncLoading()));
-                            tokenExpireWrapper(ref, () async {
-                              associationPictureNotifier
-                                  .getAssociationPicture(association.id)
-                                  .then((value) {
-                                associationPicturesNotifier.setTData(
-                                    association, AsyncData([value]));
-                              });
-                            });
-                            return const HeroIcon(
-                              HeroIcons.userCircle,
-                              size: 40,
-                            );
-                          }
-                        },
-                        loading: () {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        },
-                        error: (e, s) {
-                          return const Center(
-                            child: Text(PhonebookTextConstants
-                                .errorLoadAssociationPicture),
-                          );
-                        },
-                      );
-                    }
-                    return const HeroIcon(
-                      HeroIcons.userCircle,
-                      size: 40,
-                    );
-                  },
-                  loading: () {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                  error: (e, s) {
-                    return const Center(
-                      child: Text(
-                          PhonebookTextConstants.errorLoadAssociationPicture),
-                    );
-                  },
+                AutoLoaderChild(
+                  value: associationPictures,
+                  notifier: associationPicturesNotifier,
+                  mapKey: association,
+                  loader: (association) => associationPictureNotifier
+                      .getAssociationPicture(association.id),
+                  dataBuilder: (context, value) => Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: value.first.image,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
                 ),
                 const SizedBox(width: 10),
                 Text(
@@ -133,7 +74,7 @@ class AssociationCard extends HookConsumerWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ]))),
-    );
+              ])),
+        ));
   }
 }
