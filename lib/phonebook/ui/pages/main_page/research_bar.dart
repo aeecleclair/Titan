a4315b4d0b37ac05f10054d2af02778ee40426ec
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 15:57:00 +0100

    Cleanup

--- lib/phonebook/ui/pages/main_page/research_bar.dart
+++ lib/phonebook/ui/pages/main_page/research_bar.dart
@@ -1,26 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/phonebook/providers/association_list_provider.dart';
-import 'package:myecl/phonebook/providers/filtered_association_list_provider.dart';
 import 'package:myecl/phonebook/providers/research_filter_provider.dart';
 import 'package:myecl/phonebook/tools/constants.dart';
-import 'package:myecl/tools/token_expire_wrapper.dart';
 
 class ResearchBar extends HookConsumerWidget {
   const ResearchBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final associationListNotifier =
-        ref.watch(associationListProvider.notifier);
-    final filteredAssociationListNotifier =
-        ref.watch(filteredAssociationListProvider.notifier);
     final focusNode = useFocusNode();
     final editingController = useTextEditingController();
     final filterNotifier = ref.watch(filterProvider.notifier);
-    final associationsNotifier = ref.watch(associationListProvider.notifier);
-    
+
     return Container(
         decoration: BoxDecoration(
           border: Border.all(),
@@ -44,17 +36,17 @@ class ResearchBar extends HookConsumerWidget {
           controller: editingController,
           cursorColor: PhonebookColorConstants.textDark,
           decoration: const InputDecoration(
-              labelText: PhonebookTextConstants.associationPureSearch,
-              labelStyle: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: PhonebookColorConstants.textDark),
-              suffixIcon: Icon(
-                Icons.search,
-                color: PhonebookColorConstants.textDark,
-                size: 30,
-              ),
-              ),
+            labelText: PhonebookTextConstants.associationPureSearch,
+            labelStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: PhonebookColorConstants.textDark),
+            suffixIcon: Icon(
+              Icons.search,
+              color: PhonebookColorConstants.textDark,
+              size: 30,
+            ),
+          ),
         ));
   }
 }
