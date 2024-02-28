bb9f3cde1138e1bed59326f4869f1096e3af58f2
Shaîhh <guillaume.tavernier@etu.ec-lyon.fr>
2024-02-28 10:33:52 +0100

    fix: member card on mobile

--- lib/phonebook/ui/pages/association_page/member_card.dart
+++ lib/phonebook/ui/pages/association_page/member_card.dart
@@ -29,103 +29,95 @@ class MemberCard extends HookConsumerWidget {
             memberships.mandateYear == association.mandateYear);
 
     return GestureDetector(
-        onTap: () {
-          memberNotifier.setCompleteMember(member);
-          QR.to(PhonebookRouter.root + PhonebookRouter.memberDetail);
-        },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: CardLayout(
-              margin: EdgeInsets.zero,
-              child: Row(
-                children: [
-                  const SizedBox(width: 20),
-                  if (!kIsWeb) ...[
-                    if (member.member.nickname != null)
-                      CopiabledText(
+      onTap: () {
+        memberNotifier.setCompleteMember(member);
+        QR.to(PhonebookRouter.root + PhonebookRouter.memberDetail);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+        child: CardLayout(
+          margin: EdgeInsets.zero,
+          child: Row(
+            children: [
+              if (!kIsWeb) ...[
+                if (member.member.nickname != null) ...[
+                  CopiabledText(
+                    member.member.nickname!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  CopiabledText(
+                    "(${member.member.name} ${member.member.firstname})",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Color.fromARGB(255, 115, 115, 115),
+                    ),
+                  ),
+                ] else
+                  CopiabledText(
+                    "${member.member.name} ${member.member.firstname}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                const Spacer(),
+                CopiabledText(
+                  assoMembership == null
+                      ? PhonebookTextConstants.noMemberRole
+                      : assoMembership.apparentName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                )
+              ] else ...[
+                if (member.member.nickname != null)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
                         member.member.nickname!,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 16,
                         ),
-                        flex: 1,
                       ),
-                    CopiabledText(
-                      "${member.member.name} ${member.member.firstname}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: member.member.nickname != null
-                            ? const Color.fromARGB(255, 115, 115, 115)
-                            : Colors.black,
-                      ),
-                      flex: 1,
-                    ),
-                    CopiabledText(member.member.email,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                        flex: 1),
-                    if (member.member.phone != null)
-                      CopiabledText(member.member.phone!,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                          flex: 1),
-                    CopiabledText(
-                        assoMembership == null
-                            ? PhonebookTextConstants.noMemberRole
-                            : assoMembership.apparentName,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                        flex: 1)
-                  ] else ...[
-                    if (member.member.nickname != null)
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            member.member.nickname!,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            "${member.member.name} ${member.member.firstname}",
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 115, 115, 115),
-                            ),
-                          ),
-                        ],
-                      )
-                    else
                       Text(
                         "${member.member.name} ${member.member.firstname}",
                         style: const TextStyle(
-                          fontWeight: FontWeight.bold,
                           fontSize: 16,
+                          color: Color.fromARGB(255, 115, 115, 115),
                         ),
                       ),
-                    const Spacer(flex: 1),
-                    Text(
-                      assoMembership == null
-                          ? PhonebookTextConstants.noMemberRole
-                          : assoMembership.apparentName,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                    ],
+                  )
+                else
+                  Text(
+                    "${member.member.name} ${member.member.firstname}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
-                  ],
-                ],
-              )),
-        ));
+                  ),
+                const Spacer(flex: 1),
+                Text(
+                  assoMembership == null
+                      ? PhonebookTextConstants.noMemberRole
+                      : assoMembership.apparentName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
