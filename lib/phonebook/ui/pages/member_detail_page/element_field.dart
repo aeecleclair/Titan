5a8cc0174d95124d96dc477444ac691fda74869e
Shaîhh <guillaume.tavernier@etu.ec-lyon.fr>
2024-02-29 15:50:43 +0100

    lint and remove unused code

--- lib/phonebook/ui/pages/member_detail_page/element_field.dart
+++ lib/phonebook/ui/pages/member_detail_page/element_field.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:myecl/phonebook/tools/constants.dart';
@@ -16,52 +15,28 @@ class ElementField extends StatelessWidget {
     }
 
     return Container(
-        margin: const EdgeInsets.symmetric(vertical: 5.0),
-        child: (kIsWeb)
-            ? Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Center(
-                          child: Text(
-                        label,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ))),
-                  Expanded(
-                      flex: 4,
-                      child: Center(
-                          child: SelectableText(
-                        value,
-                        maxLines: 1,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                        onTap: () {
-                          Clipboard.setData(ClipboardData(text: value));
-                          displayToastWithContext(
-                              TypeMsg.msg, PhonebookTextConstants.copied);
-                        },
-                      ))),
-                ],
-              )
-            : Column(children: [
-                Center(
-                    child: Text(
-                  label,
-                  style: const TextStyle(fontSize: 16),
-                )),
-                Center(
-                    child: SelectableText(
-                  value,
-                  maxLines: 1,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16),
-                  onTap: () {
-                    Clipboard.setData(ClipboardData(text: value));
-                    displayToastWithContext(
-                        TypeMsg.msg, PhonebookTextConstants.copied);
-                  },
-                ))
-              ]));
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Column(
+        children: [
+          Center(
+              child: Text(
+            label,
+            style: const TextStyle(fontSize: 16),
+          )),
+          Center(
+            child: SelectableText(
+              value,
+              maxLines: 1,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: value));
+                displayToastWithContext(
+                    TypeMsg.msg, PhonebookTextConstants.copied);
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
