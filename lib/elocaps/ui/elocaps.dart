5195cfdff144fb03c61d924b0c83a9c02c247ad6
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-03-16 05:11:24 +0100

    Fix history badge

--- lib/elocaps/ui/elocaps.dart
+++ lib/elocaps/ui/elocaps.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:myecl/elocaps/providers/player_histo_provider.dart';
 import 'package:myecl/elocaps/router.dart';
 import 'package:myecl/elocaps/tools/constants.dart';
 import 'package:myecl/tools/ui/widgets/top_bar.dart';
-import 'package:qlevar_router/qlevar_router.dart';
 
 class ElocapsTemplate extends HookConsumerWidget {
   final Widget child;
@@ -13,39 +10,16 @@ class ElocapsTemplate extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final history = ref.watch(playerHistoProvider);
-    final displayBadge = history.maybeWhen(
-        data: (games) => games
-            .any((element) => !element.isConfirmed && !element.isCancelled),
-        orElse: () => false);
     return Scaffold(
       body: Container(
         color: Colors.white,
         child: SafeArea(
           child: Column(
             children: [
-              TopBar(
-                  title: ElocapsTextConstant.elocaps,
-                  root: ElocapsRouter.root,
-                  rightIcon: QR.currentPath == ElocapsRouter.root
-                      ? GestureDetector(
-                          onTap: () {
-                            QR.to(ElocapsRouter.root + ElocapsRouter.history);
-                          },
-                          child: Center(
-                              child: (displayBadge)
-                                  ? Badge(
-                                      isLabelVisible: displayBadge,
-                                      smallSize: 10,
-                                      child: const HeroIcon(
-                                        HeroIcons.clipboardDocumentList,
-                                        size: 30,
-                                      ))
-                                  : const HeroIcon(
-                                      HeroIcons.clipboardDocumentList,
-                                      size: 30,
-                                    )))
-                      : null),
+              const TopBar(
+                title: ElocapsTextConstant.elocaps,
+                root: ElocapsRouter.root,
+              ),
               Expanded(child: child),
             ],
           ),
