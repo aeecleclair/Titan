af0c095ec1725af4bfdf89fa6a973a6a8d8c4ee8
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-03-19 09:40:29 +0100

    WIP rebuilding on search change

--- lib/elocaps/ui/pages/game_page/search.dart
+++ lib/elocaps/ui/pages/game_page/search.dart
@@ -7,8 +7,11 @@ import 'package:myecl/tools/ui/builders/async_child.dart';
 class SearchResult extends HookConsumerWidget {
   final TextEditingController queryController;
   final int index;
-  const SearchResult(
-      {super.key, required this.index, required this.queryController});
+  const SearchResult({
+    super.key,
+    required this.index,
+    required this.queryController,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -17,37 +20,42 @@ class SearchResult extends HookConsumerWidget {
     final playersNotifier = ref.watch(playersProvider.notifier);
     final players = ref.watch(playersProvider);
     return AsyncChild(
-        value: users,
-        builder: (context, u) => Column(
-            children: u
-                .where((user) =>
-                    !players.values.map((e) => e.id).contains(user.id))
-                .map((user) => GestureDetector(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width: 20,
-                            ),
-                            Expanded(
-                              child: Text(
-                                user.getName(),
-                                style: const TextStyle(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ]),
-                    ),
-                    onTap: () {
-                      playersNotifier.setPlayer(index, user);
-                      queryController.text = user.getName();
-                      usersNotifier.clear();
-                    }))
-                .toList()));
+      value: users,
+      builder: (context, u) => Column(
+        children: u
+            .where((user) => !players.values.map((e) => e.id).contains(user.id))
+            .map(
+              (user) => GestureDetector(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: Text(
+                          user.getName(),
+                          style: const TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  playersNotifier.setPlayer(index, user);
+                  queryController.text = user.getName();
+                  usersNotifier.clear();
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
