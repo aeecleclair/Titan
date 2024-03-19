bd53481e44374251937d3d0b229fc8c8602d2498
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-03-19 14:03:53 +0100

    Fixing game page rebuiling on text entry change

--- lib/elocaps/providers/players_provider.dart
+++ lib/elocaps/providers/players_provider.dart
@@ -6,8 +6,9 @@ class PlayersNotifier extends StateNotifier<Map<int, SimpleUser>> {
   PlayersNotifier() : super(<int, SimpleUser>{});
 
   void setPlayer(int playerIndex, SimpleUser user) {
-    state[playerIndex] = user;
-    print(playerIndex);
+    final copy = Map<int, SimpleUser>.from(state);
+    copy[playerIndex] = user;
+    state = Map<int, SimpleUser>.from(copy);
   }
 
   void reset() {
