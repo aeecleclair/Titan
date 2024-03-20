fddcf33458628a1dc634d1670cbcebfe7166543e
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-03-20 10:19:12 +0100

    Refresher wip

--- lib/elocaps/ui/pages/main_page/main_page.dart
+++ lib/elocaps/ui/pages/main_page/main_page.dart
@@ -29,26 +29,29 @@ class EloCapsMainPage extends HookConsumerWidget {
     final modeChosen = ref.watch(modeChosenProvider);
     final me = ref.watch(userProvider);
     final leaderBoardPlayerList = ref.watch(
-        leaderBoardPlayerListProvider.select((value) => value[modeChosen]));
+      leaderBoardPlayerListProvider.select((value) => value[modeChosen]),
+    );
     final leaderBoardPlayerListNotifier =
         ref.read(leaderBoardPlayerListProvider.notifier);
     final modeChosenNotifier = ref.watch(modeChosenProvider.notifier);
     final history = ref.watch(playerHistoProvider);
 
     final displayBadge = history.maybeWhen(
-        data: ((games) => games
-            .where((e) => !e.isConfirmed && !e.isCancelled)
-            .any((element) => !element.gamePlayers
-                .where((user) => user.playerId == me.id)
-                .first
-                .hasConfirmed)),
-        orElse: () => false);
+      data: ((games) =>
+          games.where((e) => !e.isConfirmed && !e.isCancelled).any(
+                (element) => !element.gamePlayers
+                    .where((user) => user.playerId == me.id)
+                    .first
+                    .hasConfirmed,
+              )),
+      orElse: () => false,
+    );
     return ElocapsTemplate(
-        child: Stack(
-      children: [
-        Refresher(
+      child: Stack(
+        children: [
+          Refresher(
             onRefresh: () async {
-              leaderBoardPlayerNotifier.loadRanking;
+              await leaderBoardPlayerNotifier.loadRanking(modeChosen);
             },
             child: Column(
               children: [
@@ -58,23 +61,24 @@ class EloCapsMainPage extends HookConsumerWidget {
                   children: [
                     Expanded(
                       child: HorizontalListView.builder(
-                          items: CapsMode.values,
-                          itemBuilder: (context, item, i) {
-                            final selected = item == modeChosen;
-                            return ItemChip(
-                                selected: selected,
-                                onTap: () {
-                                  modeChosenNotifier.setMode(item);
-                                },
-                                child: Text(
-                                  capsModeToString(item),
-                                  style: TextStyle(
-                                      color: selected
-                                          ? Colors.white
-                                          : Colors.black),
-                                ));
-                          },
-                          height: 40),
+                        items: CapsMode.values,
+                        itemBuilder: (context, item, i) {
+                          final selected = item == modeChosen;
+                          return ItemChip(
+                            selected: selected,
+                            onTap: () {
+                              modeChosenNotifier.setMode(item);
+                            },
+                            child: Text(
+                              capsModeToString(item),
+                              style: TextStyle(
+                                color: selected ? Colors.white : Colors.black,
+                              ),
+                            ),
+                          );
+                        },
+                        height: 40,
+                      ),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -108,29 +112,33 @@ class EloCapsMainPage extends HookConsumerWidget {
                   notifier: leaderBoardPlayerListNotifier,
                   listLoader: leaderBoardPlayerNotifier.loadRanking,
                   dataBuilder: (context, players) => SingleChildScrollView(
-                    child: Column(children: [
-                      Podium(players: players),
-                      LeaderBoard(players: players),
-                    ]),
+                    child: Column(
+                      children: [
+                        Podium(players: players),
+                        LeaderBoard(players: players),
+                      ],
+                    ),
                   ),
                 ),
               ],
-            )),
-        Positioned(
-          bottom: 10,
-          left: 0,
-          right: 0,
-          child: GestureDetector(
-            onTap: () {
-              modeChosenNotifier.setMode(CapsMode.values.first);
-              QR.to(ElocapsRouter.root + ElocapsRouter.game);
-            },
-            child: const MyButton(
-              text: ElocapsTextConstant.play,
             ),
           ),
-        ),
-      ],
-    ));
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () {
+                modeChosenNotifier.setMode(CapsMode.values.first);
+                QR.to(ElocapsRouter.root + ElocapsRouter.game);
+              },
+              child: const MyButton(
+                text: ElocapsTextConstant.play,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
