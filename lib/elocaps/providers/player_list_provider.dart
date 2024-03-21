08ca5f945360e262dae7166ccc3261be6d1dbaf5
Julien <120588494+julien4215@users.noreply.github.com>
2024-03-21 11:24:00 +0100

    fix refresher main page for leaderboard

--- lib/elocaps/providers/player_list_provider.dart
+++ lib/elocaps/providers/player_list_provider.dart
@@ -2,9 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
 import 'package:myecl/elocaps/class/caps_mode.dart';
 import 'package:myecl/elocaps/class/player.dart';
+import 'package:myecl/elocaps/providers/mode_chosen_provider.dart';
 import 'package:myecl/elocaps/repositories/leaderboard_repository.dart';
 import 'package:myecl/elocaps/tools/functions.dart';
 import 'package:myecl/tools/providers/list_notifier.dart';
+import 'package:myecl/tools/token_expire_wrapper.dart';
 
 class PlayerListNotifier extends ListNotifier<Player> {
   final LeaderBoardRepository _leaderboardrepository = LeaderBoardRepository();
@@ -24,6 +26,11 @@ class PlayerListNotifier extends ListNotifier<Player> {
 final playerListProvider =
     StateNotifierProvider<PlayerListNotifier, AsyncValue<List<Player>>>((ref) {
   final token = ref.watch(tokenProvider);
-  PlayerListNotifier notifier = PlayerListNotifier(token: token);
+  final modeChosen = ref.watch(modeChosenProvider);
+  final notifier = PlayerListNotifier(token: token);
+  tokenExpireWrapperAuth(ref, () async {
+    await notifier.loadRanking(modeChosen);
+  });
+
   return notifier;
 });
