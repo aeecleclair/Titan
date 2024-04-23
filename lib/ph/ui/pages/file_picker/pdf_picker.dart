2ff13e0b973ca00c91d7b6547e02b3de8dff68b9
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-04-23 15:05:54 +0200

    Adding bla.pdf instead of "Choose pdf" when pdf has been choosen

--- lib/ph/ui/pages/file_picker/pdf_picker.dart
+++ lib/ph/ui/pages/file_picker/pdf_picker.dart
@@ -7,7 +7,6 @@ import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/ph/providers/file_picker_result_provider.dart';
 import 'package:myecl/ph/providers/ph_send_pdf_provider.dart';
-import 'package:myecl/ph/tools/constants.dart';
 import 'package:myecl/ph/ui/button.dart';
 import 'package:myecl/tools/functions.dart';
 
@@ -20,32 +19,37 @@ class PdfPicker extends HookConsumerWidget {
     final phSendPdfNotifier = ref.watch(phSendPdfProvider.notifier);
     final resultNotifier = ref.watch(filePickerResultProvider.notifier);
     final result = ref.watch(filePickerResultProvider);
-    void displayToastWithContext(TypeMsg type, String msg) {
-      displayToast(context, type, msg);
-    }
-
     return SizedBox(
       height: 40,
       child: GestureDetector(
-        onTap: () async {
-          final selectedFile = await FilePicker.platform.pickFiles(
-            allowMultiple: false,
-            type: FileType.custom,
-            allowedExtensions: ['pdf'],
-          );
-          resultNotifier.setFilePickerResult(selectedFile);
-          if (selectedFile != null) {
-            final Uint8List bytes;
-            if (selectedFile.files.single.bytes != null) {
-              bytes = selectedFile.files.single.bytes!;
-            } else {
-              bytes = await File(selectedFile.files.first.path!).readAsBytes();
+          onTap: () async {
+            final selectedFile = await FilePicker.platform.pickFiles(
+              allowMultiple: false,
+              type: FileType.custom,
+              allowedExtensions: ['pdf'],
+            );
+            resultNotifier.setFilePickerResult(selectedFile);
+            if (selectedFile != null) {
+              final Uint8List bytes;
+              if (selectedFile.files.single.bytes != null) {
+                bytes = selectedFile.files.single.bytes!;
+              } else {
+                bytes =
+                    await File(selectedFile.files.first.path!).readAsBytes();
+              }
+              if (bytes.length < 10000000) {
+                phSendPdfNotifier.set(bytes);
+              } else {
+                displayToast(context, TypeMsg.error, "Fichier trop volumineux");
+              }
             }
           },
           child: MyButton(
-            text: (result.value != null)
-                ? result.value!.files.single.name
-                : "Ajouter un fichier PDF",
+            text: isEdit
+                ? "Modifier le fichier PDF"
+                : (result != null)
+                    ? result.files.single.name
+                    : "Ajouter un fichier PDF",
           )),
     );
   }
