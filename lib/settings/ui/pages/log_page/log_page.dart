85b5b62d5f60d87357895a4711857cbc1ead2029
Maxime Roucher <maximeroucher@gmail.com>
2024-05-08 19:32:15 +0200

    refacto: log page

--- lib/settings/ui/pages/log_page/log_page.dart
+++ lib/settings/ui/pages/log_page/log_page.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/settings/providers/logs_provider.dart';
-import 'package:myecl/settings/tools/constants.dart';
-import 'package:myecl/settings/ui/pages/log_page/log_card.dart';
+import 'package:myecl/settings/ui/pages/log_page/log_tab.dart';
 import 'package:myecl/settings/ui/settings.dart';
-import 'package:myecl/tools/ui/builders/async_child.dart';
-import 'package:myecl/tools/ui/widgets/dialog.dart';
 import 'package:myecl/tools/ui/layouts/refresher.dart';
 
 class LogPage extends HookConsumerWidget {
@@ -14,81 +10,15 @@ class LogPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final logs = ref.watch(logsProvider);
     final logsNotifier = ref.watch(logsProvider.notifier);
     return SettingsTemplate(
       child: Refresher(
         onRefresh: () async {
           await logsNotifier.getLogs();
         },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    SettingsTextConstants.logs,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: ((context) => CustomDialogBox(
-                              title: SettingsTextConstants.deleting,
-                              descriptions: SettingsTextConstants.deleteLogs,
-                              onYes: (() async {
-                                logsNotifier.deleteLogs();
-                              }),
-                            )),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 12,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            blurRadius: 10,
-                            offset: const Offset(0, 5),
-                          ),
-                        ],
-                      ),
-                      child: const Row(
-                        children: [
-                          HeroIcon(
-                            HeroIcons.trash,
-                            color: Colors.white,
-                            size: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              AsyncChild(
-                value: logs,
-                builder: (context, data) => Column(
-                  children: data.map((e) => LogCard(log: e)).toList(),
-                ),
-              ),
-              const SizedBox(height: 20),
-            ],
-          ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: LogTab(),
         ),
       ),
     );
