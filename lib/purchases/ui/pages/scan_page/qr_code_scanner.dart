1339b496df038e50285870b59a416b070422c77a
Maxime Roucher <maximeroucher@gmail.com>
2024-09-06 15:54:46 +0200

    feat: adding overlay border

--- lib/purchases/ui/pages/scan_page/qr_code_scanner.dart
+++ lib/purchases/ui/pages/scan_page/qr_code_scanner.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:myecl/purchases/class/product.dart';
+import 'package:myecl/purchases/class/ticket_information.dart';
 
 class QRCodeScannerScreen extends StatefulWidget {
   const QRCodeScannerScreen({
     super.key,
     required this.product,
     required this.onScan,
+    required this.scanner,
   });
 
   final Product product;
   final Function onScan;
+  final AsyncValue<TicketInformation> scanner;
 
   @override
   _QRCodeScannerScreenState createState() => _QRCodeScannerScreenState();
@@ -25,6 +29,25 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
   Widget build(BuildContext context) {
     return MobileScanner(
       controller: controller,
+      overlayBuilder: (context, constraints) {
+        return Center(
+          child: Container(
+            width: constraints.maxWidth * 0.8,
+            height: constraints.maxWidth * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color:  widget.scanner.when(
+                  data: (data) => Colors.green,
+                  loading: () => Colors.white,
+                  error: (error, stackTrace) => Colors.red,
+                ),
+                width: 2,
+              ),
+            ),
+          ),
+        );
+      },
       onDetect: (BarcodeCapture capture) async {
         setState(() {
           qrCode = capture.barcodes.first.rawValue;
@@ -34,3 +57,5 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
     );
   }
 }
+
+
--- lib/purchases/ui/pages/scan_page/scan_dialog.dart
+++ lib/purchases/ui/pages/scan_page/scan_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/purchases/class/product.dart';
@@ -7,8 +8,10 @@ import 'package:myecl/purchases/providers/tag_provider.dart';
 import 'package:myecl/purchases/providers/ticket_list_provider.dart';
 import 'package:myecl/purchases/tools/constants.dart';
 import 'package:myecl/purchases/ui/pages/scan_page/qr_code_scanner.dart';
+import 'package:myecl/tools/constants.dart';
 import 'package:myecl/tools/functions.dart';
 import 'package:myecl/tools/ui/layouts/add_edit_button_layout.dart';
+import 'package:myecl/tools/ui/layouts/card_button.dart';
 import 'package:myecl/tools/ui/layouts/card_layout.dart';
 
 class ScanDialog extends HookConsumerWidget {
@@ -20,7 +23,9 @@ class ScanDialog extends HookConsumerWidget {
     final scannerNotifier = ref.watch(scannerProvider.notifier);
     final scanner = ref.watch(scannerProvider);
     final tag = ref.watch(tagProvider);
+    final tagNotifier = ref.read(tagProvider.notifier);
     final ticketListNotifier = ref.read(ticketListProvider.notifier);
+    final shouldSetTag = useState(true);
 
     void displayToastWithContext(TypeMsg type, String msg) {
       displayToast(context, type, msg);
@@ -28,161 +33,227 @@ class ScanDialog extends HookConsumerWidget {
 
     return CardLayout(
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          Text(
-            product.nameFR,
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(20),
-            height: 300,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: QRCodeScannerScreen(
-                product: product,
-                onScan: (secret) async {
-                  await scannerNotifier.scanTicket(product.id, secret);
-                  scanner.when(
-                    data: (data) {
-                      scannerNotifier.setScanner(
-                        data.copyWith(
-                          secret: secret,
-                        ),
-                      );
-                    },
-                    error: (error, stack) {
-                      displayToastWithContext(
-                        TypeMsg.error,
-                        error.toString(),
-                      );
-                    },
-                    loading: () {},
-                  );
-                },
+      child: shouldSetTag.value
+          ? Column(children: [
+              const SizedBox(height: 20),
+              Text(
+                "Ajouter un tag pour les scans",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ),
-          scanner.when(
-            data: (data) {
-              final user = data.user;
-              return Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(50),
+              const SizedBox(height: 20),
+              TextField(
+                onChanged: (value) async {
+                  tagNotifier.setTag(value);
+                },
+                cursorColor: PurchasesColorConstants.textDark,
+                decoration: const InputDecoration(
+                  isDense: true,
+                  label: Text(
+                    PurchasesTextConstants.tag,
+                    style: TextStyle(
+                      color: PurchasesColorConstants.textDark,
                     ),
-                    child: const HeroIcon(
-                      HeroIcons.check,
-                      size: 30,
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: ColorConstants.gradient1),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  shouldSetTag.value = false;
+                },
+                child: const AddEditButtonLayout(
+                  child: Text(
+                    "Scanner",
+                    style: TextStyle(
                       color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
                   ),
-                  Text(
-                    user.getName(),
-                    style: const TextStyle(fontSize: 20, color: Colors.black),
-                  ),
+                ),
+              ),
+            ])
+          : SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
                   const SizedBox(height: 10),
                   Text(
-                    "${data.ticket.scanLeft.toString()} / ${product.ticketMaxUse} ${PurchasesTextConstants.leftScan}",
-                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                    product.nameFR,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  const SizedBox(height: 30),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: scannerNotifier.reset,
-                          child: const Flexible(
-                            flex: 2,
-                            child: AddEditButtonLayout(
-                              color: Colors.red,
-                              child: Text(
-                                "Annuler",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ),
-                          ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        tag.isNotEmpty ? "Tag : $tag" : "Aucun tag",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () async {
-                            final value = await ticketListNotifier
-                                .consumeTicket(data, tag);
-                            if (value) {
-                              displayToastWithContext(
-                                  TypeMsg.msg, "Scan validé");
-                              scannerNotifier.reset();
-                            } else {
-                              displayToastWithContext(TypeMsg.error,
-                                  "Erreur lors de la validation");
-                            }
+                      ),
+                      const SizedBox(width: 10),
+                      CardButton(
+                        color: Colors.black,
+                        child: GestureDetector(
+                          onTap: () {
+                            shouldSetTag.value = true;
                           },
-                          child: const Flexible(
-                            flex: 2,
-                            child: AddEditButtonLayout(
-                              color: Colors.green,
-                              child: Text(
-                                "Valider",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ),
+                          child: const HeroIcon(
+                            HeroIcons.pencil,
+                            size: 20,
+                            color: Colors.white,
                           ),
                         ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(20),
+                    height: 300,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: QRCodeScannerScreen(
+                        product: product,
+                        scanner: scanner,
+                        onScan: (secret) async {
+                          await scannerNotifier.scanTicket(product.id, secret);
+                          scanner.when(
+                            data: (data) {
+                              scannerNotifier.setScanner(
+                                data.copyWith(
+                                  secret: secret,
+                                ),
+                              );
+                            },
+                            error: (error, stack) {
+                              displayToastWithContext(
+                                TypeMsg.error,
+                                error.toString(),
+                              );
+                              Future.delayed(
+                                const Duration(seconds: 2),
+                                () {
+                                  scannerNotifier.reset();
+                                },
+                              );
+                            },
+                            loading: () {},
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  scanner.when(
+                    data: (data) {
+                      final user = data.user;
+                      return Column(
+                        children: [
+                          Text(
+                            user.getName(),
+                            style: const TextStyle(
+                                fontSize: 20, color: Colors.black),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            "${data.ticket.scanLeft.toString()} / ${product.ticketMaxUse} ${PurchasesTextConstants.leftScan}",
+                            style: const TextStyle(
+                                fontSize: 16, color: Colors.black),
+                          ),
+                          const SizedBox(height: 30),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: scannerNotifier.reset,
+                                  child: const Flexible(
+                                    flex: 2,
+                                    child: AddEditButtonLayout(
+                                      color: Colors.red,
+                                      child: Text(
+                                        "Annuler",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const Spacer(),
+                                GestureDetector(
+                                  onTap: () async {
+                                    final value = await ticketListNotifier
+                                        .consumeTicket(data, tag);
+                                    if (value) {
+                                      displayToastWithContext(
+                                        TypeMsg.msg,
+                                        "Scan validé",
+                                      );
+                                      scannerNotifier.reset();
+                                    } else {
+                                      displayToastWithContext(
+                                        TypeMsg.error,
+                                        "Erreur lors de la validation",
+                                      );
+                                    }
+                                  },
+                                  child: const Flexible(
+                                    flex: 2,
+                                    child: AddEditButtonLayout(
+                                      color: Colors.green,
+                                      child: Text(
+                                        "Valider",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                    loading: () => const Text(
+                      PurchasesTextConstants.loading,
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    error: (error, stack) => Column(
+                      children: [
+                        const Text(
+                          "Erreur",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          error.toString(),
+                          style: const TextStyle(
+                              fontSize: 16, color: Colors.black),
+                        ),
+                        const SizedBox(height: 10),
                       ],
                     ),
                   ),
                 ],
-              );
-            },
-            loading: () => const Text(
-              PurchasesTextConstants.loading,
-              style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
             ),
-            error: (error, stack) => Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.red[800],
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const HeroIcon(
-                    HeroIcons.check,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                ),
-                const Text(
-                  "Erreur",
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  error.toString(),
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
