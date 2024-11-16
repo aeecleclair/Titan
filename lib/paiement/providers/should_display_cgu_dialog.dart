7da4111166778bdf9ac3b17432af8701b3f268ee
Maxime Roucher <maximeroucher@gmail.com>
2024-11-16 18:42:13 +0100

    feat: adding cgu dialog

---
+++ lib/paiement/providers/should_display_cgu_dialog.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myecl/paiement/providers/cgu_provider.dart';
+
+class ShouldDisplayCguDialog extends StateNotifier<bool> {
+  final bool defaultValue;
+  ShouldDisplayCguDialog(this.defaultValue) : super(defaultValue);
+
+  void update(s) {
+    state = s;
+  }
+}
+
+final shouldDisplayCguDialogProvider =
+    StateNotifierProvider.autoDispose<ShouldDisplayCguDialog, bool>((ref) {
+  final cgu = ref.watch(cguProvider);
+  return cgu.maybeWhen(
+    orElse: () => ShouldDisplayCguDialog(false),
+    data: (value) => ShouldDisplayCguDialog(
+        value.acceptedCguVersion != value.latestCguVersion),
+  );
+});
