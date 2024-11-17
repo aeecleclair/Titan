876b9717f27e7fbc77ddc8bebeea88e40cf783a6
Maxime Roucher <maximeroucher@gmail.com>
2024-11-17 05:35:02 +0100

    feat: adding devices handling

---
+++ lib/paiement/ui/pages/devices_page/add_device_button.dart
@@ -0,0 +1,60 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:myecl/tools/ui/builders/waiting_button.dart';
+
+class AddDeviceButton extends StatelessWidget {
+  final Future Function() onTap;
+  const AddDeviceButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(25)),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+            color: Colors.grey.shade200.withOpacity(0.5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                WaitingButton(
+                  onTap: onTap,
+                  builder: (child) => Container(
+                    height: MediaQuery.of(context).size.height - 250,
+                    child: child,
+                  ),
+                  child: const Column(
+                    children: [
+                      Spacer(flex: 3),
+                      Center(
+                        child: HeroIcon(
+                          HeroIcons.plus,
+                          color: Color.fromARGB(255, 2, 2, 2),
+                          size: 65,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        "Ajouter cet appareil",
+                        style: TextStyle(
+                          color: Color(0xff204550),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
