09b84569d4a3e478f6815e9a30cdd1c289998971
Maxime Roucher <maximeroucher@gmail.com>
2024-11-17 15:19:35 +0100

    feat: improving qr code display

--- lib/paiement/ui/pages/pay_page/confirm_button.dart
+++ lib/paiement/ui/pages/pay_page/confirm_button.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth_android/local_auth_android.dart';
 import 'package:local_auth_darwin/local_auth_darwin.dart';
+import 'package:myecl/event/tools/functions.dart';
 import 'package:myecl/paiement/providers/key_service_provider.dart';
+import 'package:myecl/paiement/providers/pay_amount_provider.dart';
 import 'package:myecl/paiement/router.dart';
+import 'package:myecl/paiement/ui/pages/pay_page/info_card.dart';
+import 'package:myecl/paiement/ui/pages/pay_page/qr_code.dart';
 import 'package:myecl/tools/functions.dart';
+import 'package:myecl/tools/ui/layouts/add_edit_button_layout.dart';
+import 'package:myecl/tools/ui/layouts/card_button.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 class ConfirmButton extends ConsumerWidget {
@@ -15,33 +22,119 @@ class ConfirmButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final keyService = ref.watch(keyServiceProvider);
+    final payAmount = ref.watch(payAmountProvider);
     final LocalAuthentication auth = LocalAuthentication();
 
     void displayToastWithContext(TypeMsg type, String msg) {
       displayToast(context, type, msg);
     }
 
+    final enabled = payAmount.isNotEmpty &&
+        double.parse(payAmount.replaceAll(',', '.')) > 0;
+
+    final formatter = NumberFormat("#,##0.00", "fr_FR");
+
+    void displayQRModal() {
+      showModalBottomSheet(
+          context: context,
+          enableDrag: false,
+          scrollControlDisabledMaxHeightRatio:
+              (1 - 100 / MediaQuery.of(context).size.height),
+          backgroundColor: Colors.transparent,
+          builder: (context) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              margin: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      InfoCard(
+                        icons: HeroIcons.currencyEuro,
+                        title: "Montant",
+                        value:
+                            '${formatter.format(double.parse(payAmount.replaceAll(',', '.')))} €',
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      InfoCard(
+                        icons: HeroIcons.clock,
+                        title: "Valide jusqu'à",
+                        value: processDateOnlyHour(
+                            DateTime.now().add(const Duration(minutes: 5))),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: QrCode(),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: GestureDetector(
+                      child: AddEditButtonLayout(
+                        color: Colors.grey.shade200.withOpacity(0.5),
+                        child: const Text(
+                          'Fermer',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          });
+    }
+
     return GestureDetector(
       onTap: () async {
-        final bool didAuthenticate = await auth.authenticate(
-          localizedReason: 'Veuillez vous authentifier pour payer',
-          authMessages: [
-            const AndroidAuthMessages(
-              signInTitle: 'L\'authentification est requise pour payer',
-              cancelButton: 'Non merci',
-            ),
-            const IOSAuthMessages(
-              cancelButton: 'Non merci',
-            ),
-          ],
-        );
-        if (!didAuthenticate) {
+        if (!enabled) {
           displayToastWithContext(
             TypeMsg.error,
-            'L\'authentification a échoué',
+            'Veuillez entrer un montant valide',
           );
           return;
         }
+        // final bool didAuthenticate = await auth.authenticate(
+        //   localizedReason: 'Veuillez vous authentifier pour payer',
+        //   authMessages: [
+        //     const AndroidAuthMessages(
+        //       signInTitle: 'L\'authentification est requise pour payer',
+        //       cancelButton: 'Non merci',
+        //     ),
+        //     const IOSAuthMessages(
+        //       cancelButton: 'Non merci',
+        //     ),
+        //   ],
+        // );
+        // if (!didAuthenticate) {
+        //   displayToastWithContext(
+        //     TypeMsg.error,
+        //     'L\'authentification a échoué',
+        //   );
+        //   return;
+        // }
         if ((await keyService.getKeyId()) == null) {
           displayToastWithContext(
             TypeMsg.error,
@@ -49,13 +142,13 @@ class ConfirmButton extends ConsumerWidget {
           );
           return;
         }
-        QR.to(PaymentRouter.root + PaymentRouter.pay + PaymentRouter.qr);
+        displayQRModal();
       },
       child: Container(
         height: 75,
         width: 75,
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: enabled ? Colors.white : Colors.grey.shade200.withOpacity(0.5),
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.1),
@@ -65,9 +158,9 @@ class ConfirmButton extends ConsumerWidget {
           ],
           borderRadius: BorderRadius.circular(25),
         ),
-        child: const HeroIcon(
+        child: HeroIcon(
           HeroIcons.qrCode,
-          color: Color(0xff017f80),
+          color: enabled ? const Color(0xff017f80) : const Color.fromARGB(134, 1, 128, 128),
           size: 65,
         ),
       ),
