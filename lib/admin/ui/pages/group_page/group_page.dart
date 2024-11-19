ebe42660b44f9cad90a169a88bee14b6a8bf3e0b
Thonyk <timr.web@free.fr>
2024-11-19 04:56:58 +0100

    Feat: add school UI

--- lib/admin/ui/pages/group_page/group_page.dart
+++ lib/admin/ui/pages/group_page/group_page.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/admin/providers/group_id_provider.dart';
 import 'package:myecl/admin/providers/group_list_provider.dart';
 import 'package:myecl/admin/router.dart';
 import 'package:myecl/admin/ui/admin.dart';
+import 'package:myecl/admin/ui/pages/group_page/group_ui.dart';
 import 'package:myecl/admin/ui/pages/main_page/card_ui.dart';
 import 'package:myecl/loan/providers/loaner_list_provider.dart';
+import 'package:myecl/admin/tools/constants.dart';
 import 'package:myecl/tools/constants.dart';
 import 'package:myecl/tools/ui/builders/async_child.dart';
+import 'package:myecl/tools/ui/widgets/dialog.dart';
+import 'package:myecl/tools/functions.dart';
 import 'package:myecl/tools/ui/layouts/refresher.dart';
+import 'package:myecl/tools/token_expire_wrapper.dart';
 import 'package:myecl/user/providers/user_list_provider.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
-class AdminMainPage extends HookConsumerWidget {
-  const AdminMainPage({super.key});
+class GroupsPage extends HookConsumerWidget {
+  const GroupsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final groups = ref.watch(allGroupListProvider);
     final groupsNotifier = ref.watch(allGroupListProvider.notifier);
+    final groupIdNotifier = ref.watch(groupIdProvider.notifier);
+    final loans = ref.watch(loanerListProvider);
     final loanListNotifier = ref.watch(loanerListProvider.notifier);
     ref.watch(userList);
+    void displayToastWithContext(TypeMsg type, String msg) {
+      displayToast(context, type, msg);
+    }
+
+    final List<String> loanersId = loans.maybeWhen(
+      data: (value) => value.map((e) => e.groupManagerId).toList(),
+      orElse: () => [],
+    );
 
     return AdminTemplate(
       child: Refresher(
@@ -43,14 +59,16 @@ class AdminMainPage extends HookConsumerWidget {
                       GestureDetector(
                         onTap: () {
                           QR.to(
-                            AdminRouter.root + AdminRouter.editModuleVisibility,
+                            AdminRouter.root +
+                                AdminRouter.groups +
+                                AdminRouter.addGroup,
                           );
                         },
                         child: CardUi(
                           children: [
                             const Spacer(),
                             HeroIcon(
-                              HeroIcons.eye,
+                              HeroIcons.plus,
                               color: Colors.grey.shade700,
                               size: 40,
                             ),
@@ -60,23 +78,11 @@ class AdminMainPage extends HookConsumerWidget {
                       ),
                       GestureDetector(
                         onTap: () {
-                          QR.to(AdminRouter.root + AdminRouter.groups);
-                        },
-                        child: CardUi(
-                          children: [
-                            const Spacer(),
-                            HeroIcon(
-                              HeroIcons.users,
-                              color: Colors.grey.shade700,
-                              size: 40,
-                            ),
-                            const Spacer(),
-                          ],
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          QR.to(AdminRouter.root + AdminRouter.schools);
+                          QR.to(
+                            AdminRouter.root +
+                                AdminRouter.addLoaner +
+                                AdminRouter.addLoaner,
+                          );
                         },
                         child: CardUi(
                           children: [
@@ -85,7 +91,7 @@ class AdminMainPage extends HookConsumerWidget {
                               clipBehavior: Clip.none,
                               children: [
                                 HeroIcon(
-                                  HeroIcons.academicCap,
+                                  HeroIcons.buildingLibrary,
                                   color: Colors.grey.shade700,
                                   size: 40,
                                 ),
@@ -104,6 +110,50 @@ class AdminMainPage extends HookConsumerWidget {
                           ],
                         ),
                       ),
+                      ...g.map(
+                        (group) => GroupUi(
+                          group: group,
+                          isLoaner: loanersId.contains(group.id),
+                          onEdit: () {
+                            groupIdNotifier.setId(group.id);
+                            QR.to(
+                              AdminRouter.root +
+                                  AdminRouter.groups +
+                                  AdminRouter.editGroup,
+                            );
+                          },
+                          onDelete: () async {
+                            await showDialog(
+                              context: context,
+                              builder: (context) {
+                                return CustomDialogBox(
+                                  title: AdminTextConstants.deleting,
+                                  descriptions:
+                                      AdminTextConstants.deleteAssociation,
+                                  onYes: () async {
+                                    tokenExpireWrapper(ref, () async {
+                                      final value = await groupsNotifier
+                                          .deleteGroup(group);
+                                      if (value) {
+                                        displayToastWithContext(
+                                          TypeMsg.msg,
+                                          AdminTextConstants.deletedAssociation,
+                                        );
+                                      } else {
+                                        displayToastWithContext(
+                                          TypeMsg.error,
+                                          AdminTextConstants.deletingError,
+                                        );
+                                      }
+                                    });
+                                  },
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
                     ],
                   ),
                 ],
---
+++ lib/admin/ui/pages/school_page/school_ui.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/admin/class/school.dart';
+import 'package:myecl/admin/ui/pages/school_page/school_button.dart';
+import 'package:myecl/admin/ui/pages/main_page/card_ui.dart';
+import 'package:myecl/tools/constants.dart';
+import 'package:myecl/tools/ui/builders/waiting_button.dart';
+
+class SchoolUi extends HookConsumerWidget {
+  final School school;
+  final void Function() onEdit;
+  final Future Function() onDelete;
+  const SchoolUi({
+    super.key,
+    required this.school,
+    required this.onEdit,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return CardUi(
+      children: [
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            school.name,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Row(
+          children: [
+            GestureDetector(
+              onTap: onEdit,
+              child: SchoolButton(
+                gradient1: Colors.grey.shade800,
+                gradient2: Colors.grey.shade900,
+                child: const HeroIcon(
+                  HeroIcons.eye,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            if (school.id != "dce19aa2-8863-4c93-861e-fb7be8f610ed" &&
+                school.id != "d9772da7-1142-4002-8b86-b694b431dfed")
+              WaitingButton(
+                onTap: onDelete,
+                builder: (child) => SchoolButton(
+                  gradient1: ColorConstants.gradient1,
+                  gradient2: ColorConstants.gradient2,
+                  child: child,
+                ),
+                child: const HeroIcon(
+                  HeroIcons.xMark,
+                  color: Colors.white,
+                ),
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
