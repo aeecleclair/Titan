e26234e851278e424e2735cb427074044703becc
Maxime Roucher <maximeroucher@gmail.com>
2024-11-20 17:45:35 +0100

    feat: adding transaction and store card lists

---
+++ lib/paiement/providers/my_stores_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myecl/paiement/class/store.dart';
+import 'package:myecl/paiement/repositories/users_me_repository.dart';
+import 'package:myecl/tools/providers/list_notifier.dart';
+
+class MyStoresNotifier extends ListNotifier<Store> {
+  final UsersMeRepository usersMeRepository;
+  MyStoresNotifier({required this.usersMeRepository})
+      : super(const AsyncValue.loading());
+
+  Future<AsyncValue<List<Store>>> getMyStores() async {
+    return await loadList(usersMeRepository.getMyStores);
+  }
+}
+
+final myMyStoresProvider =
+    StateNotifierProvider<MyStoresNotifier, AsyncValue<List<Store>>>((ref) {
+  final myStoresRepository = ref.watch(usersMeRepositoryProvider);
+  return MyStoresNotifier(usersMeRepository: myStoresRepository)..getMyStores();
+});
