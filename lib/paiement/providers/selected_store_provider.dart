4ff33ecf60239610a705be8d6f711ac6e8578341
Maxime Roucher <maximeroucher@gmail.com>
2024-11-22 23:18:09 +0100

    feat: adding store selection

---
+++ lib/paiement/providers/selected_store_provider.dart
@@ -0,0 +1,25 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/paiement/class/store.dart';
+import 'package:myecl/paiement/providers/my_stores_provider.dart';
+
+class SelectedStoreNotifier extends StateNotifier<Store> {
+  SelectedStoreNotifier(super.store);
+
+  void updateStore(Store store) {
+    state = store;
+  }
+}
+
+final selectedStoreProvider = StateNotifierProvider<SelectedStoreNotifier, Store>((ref) {
+  final myStores = ref.watch(myStoresProvider);
+  final store = myStores.maybeWhen<Store>(
+    orElse: () => Store.empty(),
+    data: (value) {
+      if (value.isEmpty) {
+        return Store.empty();
+      }
+      return value.first;
+    },
+  );
+  return SelectedStoreNotifier(store);
+});
