03c6f5fa22ee5058922aeb763b1998735c128881
Maxime Roucher <maximeroucher@gmail.com>
2024-11-23 16:34:25 +0100

    feat: adding stats page

--- lib/paiement/tools/functions.dart
+++ lib/paiement/tools/functions.dart
@@ -1,51 +1,27 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:myecl/paiement/class/qr_code_data.dart';
 import 'package:myecl/paiement/class/qr_code_signature_data.dart';
 import 'package:myecl/paiement/class/wallet_device.dart';
 import 'package:myecl/paiement/tools/key_service.dart';
 
 String getMonth(int m) {
-  switch (m) {
-    case 0:
-      return "Décembre";
-    case 1:
-      return "Janvier";
-    case 2:
-      return "Février";
-    case 3:
-      return "Mars";
-    case 4:
-      return "Avril";
-    case 5:
-      return "Mai";
-    case 6:
-      return "Juin";
-    case 7:
-      return "Juillet";
-    case 8:
-      return "Août";
-    case 9:
-      return "Septembre";
-    case 10:
-      return "Octobre";
-    case 11:
-      return "Novembre";
-    case 12:
-      return "Décembre";
-    default:
-      return "";
-  }
-}
-
-String castListInt(List<int> list) {
-  return String.fromCharCodes(list);
-}
-
-List<int> castStringToListInt(String str) {
-  return str.codeUnits;
+  final months = [
+    "Décembre",
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+  ];
+  return months[m];
 }
 
 Widget getStatusTag(WalletDeviceStatus status) {
--- lib/paiement/ui/pages/stats_page/month_bar.dart
+++ lib/paiement/ui/pages/stats_page/month_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:myecl/paiement/providers/selected_month_provider.dart';
 import 'package:myecl/paiement/tools/functions.dart';
 
 class MonthBar extends HookConsumerWidget {
@@ -8,8 +8,8 @@ class MonthBar extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentDay = useState(DateTime.now());
-    final currentMonth = useState(currentDay.value.month);
+    final currentMonth = ref.watch(selectedMonthProvider);
+    final currentMonthNotifier = ref.watch(selectedMonthProvider.notifier);
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -21,10 +21,16 @@ class MonthBar extends HookConsumerWidget {
             ),
             GestureDetector(
               onTap: () {
-                currentMonth.value = (currentMonth.value - 1) % 12;
+                final isPreviousYear = currentMonth.month == 0;
+                currentMonthNotifier.updateSelectedMonth(
+                  DateTime(
+                    currentMonth.year - (isPreviousYear ? 1 : 0),
+                    (currentMonth.month - 1) % 12,
+                  ),
+                );
               },
               child: Text(
-                getMonth((currentMonth.value - 1) % 12),
+                getMonth((currentMonth.month - 1) % 12),
                 style: const TextStyle(
                   fontSize: 13,
                   fontWeight: FontWeight.bold,
@@ -35,7 +41,7 @@ class MonthBar extends HookConsumerWidget {
           ],
         ),
         Text(
-          getMonth(currentMonth.value % 12),
+          getMonth(currentMonth.month % 12),
           style: const TextStyle(
             fontSize: 20,
             color: Colors.black,
@@ -46,10 +52,16 @@ class MonthBar extends HookConsumerWidget {
           children: [
             GestureDetector(
               onTap: () {
-                currentMonth.value = (currentMonth.value + 1) % 12;
+                final isNextYear = currentMonth.month == 11;
+                currentMonthNotifier.updateSelectedMonth(
+                  DateTime(
+                    currentMonth.year + (isNextYear ? 1 : 0),
+                    (currentMonth.month + 1) % 12,
+                  ),
+                );
               },
               child: Text(
-                getMonth((currentMonth.value + 1) % 12),
+                getMonth((currentMonth.month + 1) % 12),
                 style: const TextStyle(
                   fontSize: 13,
                   fontWeight: FontWeight.bold,
