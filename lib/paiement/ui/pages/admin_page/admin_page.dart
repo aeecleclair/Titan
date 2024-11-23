f73ae5ca18db03ad9bb797c8b037be842c677cc0
Maxime Roucher <maximeroucher@gmail.com>
2024-11-23 21:57:54 +0100

    feat: adding store admin page

---
+++ lib/paiement/ui/pages/admin_page/admin_page.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:myecl/paiement/ui/paiement.dart';
+
+// class AdminPage extends ConsumerWidget {
+//   const AdminPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final admins = ref.watch()
+//     return PaymentTemplate(child: Container());
+//   }
+// }
