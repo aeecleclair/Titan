558e441ca568393dd89234f3145bdab669999f7a
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-11-25 12:27:02 +0100

    Notification working

--- lib/service/class/message.dart
+++ lib/service/class/message.dart
@@ -1,22 +1,14 @@
-import 'package:myecl/tools/functions.dart';
-
 class Message {
   late final String? title;
   late final String? content;
   late final String? actionModule;
   late final String? actionTable;
-  late final bool isVisible;
-  late final String context;
-  late final DateTime? deliveryDateTime;
 
   Message({
     required this.title,
     required this.content,
     required this.actionModule,
     required this.actionTable,
-    required this.context,
-    required this.isVisible,
-    this.deliveryDateTime,
   });
 
   Message.fromJson(Map<String, dynamic> json) {
@@ -24,11 +16,6 @@ class Message {
     content = json['content'];
     actionModule = json['action_module'];
     actionTable = json['action_table'];
-    context = json['context'];
-    isVisible = json['is_visible'];
-    deliveryDateTime = json['delivery_datetime'] != null
-        ? processDateFromAPI(json['delivery_datetime'])
-        : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -37,15 +24,11 @@ class Message {
     data['content'] = content;
     data['action_module'] = actionModule;
     data['action_table'] = actionTable;
-    data['context'] = context;
-    data['is_visible'] = isVisible;
-    data['delivery_datetime'] =
-        (deliveryDateTime != null) ? processDateToAPI(deliveryDateTime!) : null;
     return data;
   }
 
   @override
   String toString() {
-    return 'Message{title: $title, content: $content, actionModule: $actionModule, actionTable: $actionTable, context: $context, isVisible: $isVisible}';
+    return 'Message{title: $title, content: $content, actionModule: $actionModule, actionTable: $actionTable}';
   }
 }
