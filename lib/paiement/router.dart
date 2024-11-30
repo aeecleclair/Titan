7437bb7710e579cf23870d038896cd04fc1fa2a6
Maxime Roucher <maximeroucher@gmail.com>
2024-11-30 23:26:07 +0100

    feat: adding add edit store

--- lib/paiement/router.dart
+++ lib/paiement/router.dart
@@ -2,6 +2,11 @@ import 'package:either_dart/either.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:myecl/drawer/class/module.dart';
+import 'package:myecl/paiement/providers/is_payment_admin.dart';
+import 'package:myecl/paiement/ui/pages/admin_page/admin_page.dart'
+    deferred as admin_page;
+import 'package:myecl/paiement/ui/pages/store_pages/add_edit_store.dart'
+    deferred as add_edit_page;
 import 'package:myecl/paiement/ui/pages/store_admin_page/store_admin_page.dart'
     deferred as store_admin_page;
 import 'package:myecl/paiement/ui/pages/devices_page/devices_page.dart'
@@ -12,6 +17,7 @@ import 'package:myecl/paiement/ui/pages/stats_page/stats_page.dart'
     deferred as stats_page;
 import 'package:myecl/paiement/ui/pages/store_stats_page/store_stats_page.dart'
     deferred as store_stats_page;
+import 'package:myecl/tools/middlewares/admin_middleware.dart';
 import 'package:myecl/tools/middlewares/authenticated_middleware.dart';
 import 'package:myecl/tools/middlewares/deferred_middleware.dart';
 import 'package:qlevar_router/qlevar_router.dart';
@@ -22,6 +28,7 @@ class PaymentRouter {
   static const String stats = '/stats';
   static const String devices = '/devices';
   static const String admin = '/admin';
+  static const String addEditStore = '/addEditStore';
   static const String storeAdmin = '/storeAdmin';
   static const String storeStats = '/storeStats';
   static final Module module = Module(
@@ -60,6 +67,23 @@ class PaymentRouter {
             builder: () => store_admin_page.StoreAdminPage(),
             middleware: [
               DeferredLoadingMiddleware(store_admin_page.loadLibrary),
+            ],
+          ),
+          QRoute(
+            path: PaymentRouter.admin,
+            builder: () => admin_page.AdminPage(),
+            middleware: [
+              DeferredLoadingMiddleware(admin_page.loadLibrary),
+              AdminMiddleware(ref, isPaymentAdminProvider)
+            ],
+            children: [
+              QRoute(
+                path: PaymentRouter.addEditStore,
+                builder: () => add_edit_page.AddEditStorePage(),
+                middleware: [
+                  DeferredLoadingMiddleware(add_edit_page.loadLibrary),
+                ],
+              ),
             ],
           ),
           QRoute(
