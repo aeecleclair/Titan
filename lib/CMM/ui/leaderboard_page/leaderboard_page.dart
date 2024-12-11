944c64214f07800b0f46445610f9898ec410dcb3
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-12-12 00:19:32 +0100

    First commit

---
+++ lib/CMM/ui/leaderboard_page/leaderboard_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:myecl/CMM/ui/cmm.dart';
+
+class CMMLeaderboardPage extends StatelessWidget {
+  const CMMLeaderboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CMMTemplate(child: Scaffold());
+  }
+}
