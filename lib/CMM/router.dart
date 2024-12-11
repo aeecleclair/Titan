2165fb314bc12fa0a3c22f6e37a2a36bb2fdbc2d
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-12-12 00:19:32 +0100

    First commit

---
+++ lib/CMM/router.dart
@@ -0,0 +1,39 @@
+import 'package:either_dart/either.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:myecl/tools/middlewares/deferred_middleware.dart';
+import 'package:qlevar_router/qlevar_router.dart';
+import 'package:myecl/drawer/class/module.dart';
+import 'package:myecl/CMM/ui/main_page/main_page.dart' deferred as main_page;
+import 'package:myecl/CMM/ui/leaderboard_page/leaderboard_page.dart'
+    deferred as leaderboard_page;
+
+class CMMRouter {
+  final ProviderRef ref;
+  static const String root = '/CMM';
+  static const String leaderboard = '/leaderboard';
+  static final Module module = Module(
+    name: "CMM",
+    icon: const Left(HeroIcons.newspaper),
+    root: CMMRouter.root,
+    selected: false,
+  );
+  CMMRouter(this.ref);
+  QRoute route() => QRoute(
+        name: "CMM",
+        path: CMMRouter.root,
+        builder: () => main_page.CMMMainPage(),
+        middleware: [
+          DeferredLoadingMiddleware(main_page.loadLibrary),
+        ],
+        children: [
+          QRoute(
+            path: CMMRouter.leaderboard,
+            builder: () => leaderboard_page.CMMLeaderboardPage(),
+            middleware: [
+              DeferredLoadingMiddleware(leaderboard_page.loadLibrary),
+            ],
+          )
+        ],
+      );
+}
