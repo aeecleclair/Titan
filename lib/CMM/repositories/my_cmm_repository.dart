f65ad3f7388e0c53a556e0990a323e5d5e00be79
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-12-23 19:45:23 +0100

    WIP repo

--- lib/CMM/repositories/my_cmm_repository.dart
+++ lib/CMM/repositories/my_cmm_repository.dart
@@ -1,14 +1,18 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:myecl/tools/repository/logo_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/CMM/class/cmm.dart';
 import 'package:myecl/admin/class/account_type.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
-import 'package:myecl/tools/repository/repository.dart';
 import 'package:myecl/user/class/list_users.dart';
 
-class MyCMMRepository extends Repository {
+class MyCMMRepository extends LogoRepository {
   @override
   // ignore: overridden_fields
-  final ext = "cmm/me";
+  final ext = "cmm/";
 
   Future<List<CMM>> getMyCMM() async {
     //return (await getList(suffix: '')).map((e) => CMM.fromJson(e)).toList();
@@ -44,8 +48,13 @@ class MyCMMRepository extends Repository {
     ];
   }
 
-  Future<CMM> addCMM(CMM cmm) async {
-    return CMM.fromJson(await create(cmm.toJson(), suffix: ''));
+  Future<Image> addCMM(Uint8List bytes) async {
+    final uint8List = await addLogo(bytes, "", suffix: "memes");
+    return Image.memory(uint8List);
+  }
+
+  reactToCMM(String id, bool positive) async {
+    await create(cmm.toJson(), suffix: 'memes/$id/vote');
   }
 
   Future<bool> deleteCMM(String id) async {
