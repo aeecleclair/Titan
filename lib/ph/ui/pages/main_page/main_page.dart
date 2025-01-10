315d1f94f82544434b0c7ee059b175081771c278
Foucauld Bellanger <63885990+foucblg@users.noreply.github.com>
2025-01-10 12:57:14 +0100

    Fix scrolling (#468)
    Co-authored-by: Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
    Co-authored-by: Armand Didierjean <95971503+armanddidierjean@users.noreply.github.com>

--- lib/ph/ui/pages/main_page/main_page.dart
+++ lib/ph/ui/pages/main_page/main_page.dart
@@ -60,7 +60,7 @@ class PhMainPage extends HookConsumerWidget {
                   value: lastPhPdf,
                   builder: (context, value) => Expanded(
                     child: PdfView(
-                      pageSnapping: false,
+                      pageSnapping: !kIsWeb,
                       controller: PdfController(
                         document: PdfDocument.openData(value),
                       ),
