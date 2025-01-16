0c6927f7cd13ec59436510185b7794a279677029
Maxime Roucher <maximeroucher@gmail.com>
2025-01-16 18:05:38 +0100

    fix: payment admin

--- lib/paiement/providers/is_payment_admin.dart
+++ lib/paiement/providers/is_payment_admin.dart
@@ -1,14 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myecl/paiement/providers/my_stores_provider.dart';
+import 'package:myecl/paiement/providers/structure_list_provider.dart';
 import 'package:myecl/user/providers/user_provider.dart';
 
 final isPaymentAdminProvider = StateProvider((ref) {
   final user = ref.watch(userProvider);
-  final myStores = ref.watch(myStoresProvider);
-  return myStores.when(
-    data: (stores) => (stores
-        .map((store) => store.structure.managerUserId)
-        .contains(user.id)),
+  final structures = ref.watch(structureListProvider);
+  return structures.when(
+    data: (structures) => structures
+        .map((structure) => structure.managerUserId)
+        .contains(user.id),
     loading: () => false,
     error: (error, stack) => false,
   );
