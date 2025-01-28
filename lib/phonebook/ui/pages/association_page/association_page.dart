7cfd7cbec06a4b22e9e5fe93b2530c4b25b3b80a
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-01-28 20:45:06 +0100

    Fix phonebook ui

--- lib/phonebook/ui/pages/association_page/association_page.dart
+++ lib/phonebook/ui/pages/association_page/association_page.dart
@@ -43,99 +43,102 @@ class AssociationPage extends HookConsumerWidget {
           await associationPictureNotifier
               .getAssociationPicture(association.id);
         },
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  association.name,
-                  style: const TextStyle(fontSize: 40, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  association.kind,
-                  style: const TextStyle(fontSize: 20, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  association.description,
-                  style: const TextStyle(fontSize: 15, color: Colors.black),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "${PhonebookTextConstants.activeMandate} ${association.mandateYear}",
-                  style: const TextStyle(fontSize: 15, color: Colors.black),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                AsyncChild(
-                  value: associationMemberList,
-                  builder: (context, associationMembers) =>
-                      associationMembers.isEmpty
-                          ? const Text(PhonebookTextConstants.noMember)
-                          : Column(
-                              children: associationMemberSortedList
-                                  .map(
-                                    (member) => kIsWeb
-                                        ? WebMemberCard(
-                                            member: member,
-                                            association: association,
-                                          )
-                                        : MemberCard(
-                                            member: member,
-                                            association: association,
-                                          ),
-                                  )
-                                  .toList(),
-                            ),
-                ),
-              ],
-            ),
-            if (isPresident)
-              Positioned(
-                top: 20,
-                right: 20,
-                child: GestureDetector(
-                  onTap: () {
-                    kindNotifier.setKind(association.kind);
-                    QR.to(
-                      PhonebookRouter.root +
-                          PhonebookRouter.associationDetail +
-                          PhonebookRouter.editAssociation,
-                    );
-                  },
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: const RadialGradient(
-                        colors: [
-                          Color.fromARGB(255, 98, 98, 98),
-                          Color.fromARGB(255, 27, 27, 27),
-                        ],
-                        center: Alignment.topLeft,
-                        radius: 1.3,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color.fromARGB(255, 27, 27, 27)
-                              .withValues(alpha: 0.3),
-                          spreadRadius: 5,
-                          blurRadius: 10,
-                          offset:
-                              const Offset(3, 3), // changes position of shadow
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    association.name,
+                    style: const TextStyle(fontSize: 40, color: Colors.black),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    association.kind,
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    association.description,
+                    style: const TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "${PhonebookTextConstants.activeMandate} ${association.mandateYear}",
+                    style: const TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  AsyncChild(
+                    value: associationMemberList,
+                    builder: (context, associationMembers) =>
+                        associationMembers.isEmpty
+                            ? const Text(PhonebookTextConstants.noMember)
+                            : Column(
+                                children: associationMemberSortedList
+                                    .map(
+                                      (member) => kIsWeb
+                                          ? WebMemberCard(
+                                              member: member,
+                                              association: association,
+                                            )
+                                          : MemberCard(
+                                              member: member,
+                                              association: association,
+                                            ),
+                                    )
+                                    .toList(),
+                              ),
+                  ),
+                ],
+              ),
+              if (isPresident)
+                Positioned(
+                  top: 20,
+                  right: 20,
+                  child: GestureDetector(
+                    onTap: () {
+                      kindNotifier.setKind(association.kind);
+                      QR.to(
+                        PhonebookRouter.root +
+                            PhonebookRouter.associationDetail +
+                            PhonebookRouter.editAssociation,
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: const RadialGradient(
+                          colors: [
+                            Color.fromARGB(255, 98, 98, 98),
+                            Color.fromARGB(255, 27, 27, 27),
+                          ],
+                          center: Alignment.topLeft,
+                          radius: 1.3,
                         ),
-                      ],
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 27, 27, 27)
+                                .withValues(alpha: 0.3),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(
+                                3, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child:
+                          const HeroIcon(HeroIcons.pencil, color: Colors.white),
                     ),
-                    child:
-                        const HeroIcon(HeroIcons.pencil, color: Colors.white),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
