d16f6c39e6643a35041c74df491bef1b3d6a9ead
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 18:52:32 +0100

    feat: removing moking

--- lib/paiement/repositories/users_me_repository.dart
+++ lib/paiement/repositories/users_me_repository.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
 import 'package:myecl/paiement/class/history.dart';
-import 'package:myecl/paiement/class/store.dart';
+import 'package:myecl/paiement/class/user_store.dart';
+import 'package:myecl/paiement/class/wallet.dart';
 import 'package:myecl/tools/repository/repository.dart';
 
 class UsersMeRepository extends Repository {
@@ -14,82 +15,19 @@ class UsersMeRepository extends Repository {
   }
 
   Future<List<History>> getMyHistory() async {
-    return [
-      History(
-        id: "1",
-        type: HistoryType.given,
-        otherWalletName: 'WEI',
-        total: 20000,
-        creation: DateTime.now(),
-        status: TransactionStatus.confirmed,
-      ),
-      History(
-        id: "2",
-        type: HistoryType.transfer,
-        otherWalletName: 'Recharge',
-        total: 5000,
-        creation: DateTime.now().subtract(const Duration(days: 1)),
-        status: TransactionStatus.confirmed,
-      ),
-      History(
-        id: "3",
-        type: HistoryType.received,
-        otherWalletName: 'Bar//',
-        total: 200,
-        creation: DateTime.now().subtract(const Duration(days: 2)),
-        status: TransactionStatus.confirmed,
-      ),
-      History(
-        id: "4",
-        type: HistoryType.given,
-        otherWalletName: 'Commuz',
-        total: 900,
-        creation: DateTime.now().subtract(const Duration(days: 30)),
-        status: TransactionStatus.canceled,
-      ),
-      History(
-        id: "4",
-        type: HistoryType.received,
-        otherWalletName: 'Commuz',
-        total: 900,
-        creation: DateTime.now().subtract(const Duration(days: 35)),
-        status: TransactionStatus.canceled,
-      ),
-    ];
-    // return List<History>.from(
-    //   (await getList(
-    //     suffix: 'wallet/history',
-    //   ))
-    //       .map((e) => History.fromJson(e)),
-    // );
+    return List<History>.from(
+      (await getList(suffix: 'wallet/history')).map((e) => History.fromJson(e)),
+    );
   }
 
-  Future<List<Store>> getMyStores() async {
-    return [
-      Store(
-          id: "1",
-          name: "WEI",
-          walletId: "a",
-          membership: AvailableAssociationMembership.AEECL),
-      Store(
-          id: "2",
-          name: "Bar",
-          walletId: "b",
-          membership: AvailableAssociationMembership.AEECL),
-      Store(
-          id: "3",
-          name: "Commuz",
-          walletId: "c",
-          membership: AvailableAssociationMembership.AEECL),
-      Store(
-          id: "4",
-          name: "Raid",
-          walletId: "d",
-          membership: AvailableAssociationMembership.USEECL),
-    ];
-    // return List<Store>.from(
-    //   (await getList(suffix: "stores")).map((e) => Store.fromJson(e)),
-    // );
+  Future<List<UserStore>> getMyStores() async {
+    return List<UserStore>.from(
+      (await getList(suffix: "stores")).map((e) => UserStore.fromJson(e)),
+    );
+  }
+
+  Future<Wallet> getMyWallet() async {
+    return Wallet.fromJson(await getOne("wallet"));
   }
 }
 
