5c4a831072c503f33bf2cebdd4b0edd9d04ef10b
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 18:50:37 +0100

    feat: applying formatter

--- lib/paiement/ui/pages/qr_page/qr_code.dart
+++ lib/paiement/ui/pages/qr_page/qr_code.dart
@@ -11,33 +11,37 @@ class QrCode extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(10),
       decoration: BoxDecoration(
-          color: Colors.grey.shade50,
-          borderRadius: const BorderRadius.all(Radius.circular(30)),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10,
-              offset: Offset(0, 5),
-            ),
-          ],),
+        color: Colors.grey.shade50,
+        borderRadius: const BorderRadius.all(Radius.circular(30)),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
       child: ClipRRect(
         borderRadius: const BorderRadius.all(Radius.circular(30)),
         child: Center(
-            child: QrImageView(
-          data:
-              "Explicabo eos culpa hic aliquid aliquam cupiditate. Laboriosam aperiam ullam in reprehenderit. Ut et laudantium nihil sapiente dolor tenetur suscipit ut maxime. Et rem repellendus repudiandae incidunt ab natus nobis expedita temporibus. Ullam consequatur sint quasi.",
-          version: QrVersions.auto,
-          size: min(MediaQuery.of(context).size.width * 0.8,
-              MediaQuery.of(context).size.height * 0.8,),
-          eyeStyle: const QrEyeStyle(
-            color: Colors.black,
-            eyeShape: QrEyeShape.square,
+          child: QrImageView(
+            data:
+                "Explicabo eos culpa hic aliquid aliquam cupiditate. Laboriosam aperiam ullam in reprehenderit. Ut et laudantium nihil sapiente dolor tenetur suscipit ut maxime. Et rem repellendus repudiandae incidunt ab natus nobis expedita temporibus. Ullam consequatur sint quasi.",
+            version: QrVersions.auto,
+            size: min(
+              MediaQuery.of(context).size.width * 0.8,
+              MediaQuery.of(context).size.height * 0.8,
+            ),
+            eyeStyle: const QrEyeStyle(
+              color: Colors.black,
+              eyeShape: QrEyeShape.square,
+            ),
+            dataModuleStyle: const QrDataModuleStyle(
+              dataModuleShape: QrDataModuleShape.square,
+              color: Colors.black,
+            ),
           ),
-          dataModuleStyle: const QrDataModuleStyle(
-            dataModuleShape: QrDataModuleShape.square,
-            color: Colors.black,
-          ),
-        ),),
+        ),
       ),
     );
   }
