e54a6ea0a0da1116e8ee9ee05f6a03f4a1ecf185
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 18:52:25 +0100

    feat: adding store admin page

--- lib/paiement/ui/pages/main_page/seller_card/store_seller_card.dart
+++ lib/paiement/ui/pages/main_page/seller_card/store_seller_card.dart
@@ -12,45 +12,45 @@ class StoreSellerCard extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedStore = ref.watch(selectedStoreProvider);
-    final selectedStoreNotifier = ref.watch(selectedStoreProvider.notifier);
-    return Container(
-      height: 70,
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        children: [
-          const CircleAvatar(
-            radius: 27,
-            backgroundColor: Color.fromARGB(255, 6, 75, 75),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          Expanded(
-            child: AutoSizeText(
-              store.name,
-              maxLines: 2,
-              style: const TextStyle(
-                color: Color.fromARGB(255, 0, 29, 29),
-                fontSize: 14,
+    final selectedStoreNotifier = ref.read(selectedStoreProvider.notifier);
+    return GestureDetector(
+      onTap: () {
+        selectedStoreNotifier.updateStore(store);
+      },
+      child: Container(
+        height: 70,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          children: [
+            const CircleAvatar(
+              radius: 27,
+              backgroundColor: Color.fromARGB(255, 6, 75, 75),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Expanded(
+              child: AutoSizeText(
+                store.name,
+                maxLines: 2,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 0, 29, 29),
+                  fontSize: 14,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          if (store.id != selectedStore.id)
-            GestureDetector(
-              onTap: () {
-                selectedStoreNotifier.updateStore(store);
-              },
-              child: const HeroIcon(
+            const SizedBox(
+              width: 10,
+            ),
+            if (store.id != selectedStore.id)
+              const HeroIcon(
                 HeroIcons.arrowRight,
                 color: Color.fromARGB(255, 0, 29, 29),
                 size: 25,
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
