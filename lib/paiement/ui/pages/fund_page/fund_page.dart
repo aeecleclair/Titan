acaec5aad494f8637b751e95a83bcb3c9a0b3a56
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 19:06:40 +0100

    feat: adding indication on fund and pay page

--- lib/paiement/ui/pages/fund_page/fund_page.dart
+++ lib/paiement/ui/pages/fund_page/fund_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:myecl/paiement/providers/fund_amount_provider.dart';
+import 'package:myecl/paiement/providers/my_wallet_provider.dart';
 import 'package:myecl/paiement/ui/components/digit_fade_in_animation.dart';
 import 'package:myecl/paiement/ui/components/keyboard.dart';
 import 'package:myecl/paiement/ui/pages/fund_page/confirm_button.dart';
@@ -12,6 +14,15 @@ class FundPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final fundAmount = ref.watch(fundAmountProvider);
     final fundAmountNotifier = ref.watch(fundAmountProvider.notifier);
+    final myWallet = ref.watch(myWalletProvider);
+    final currentAmount =
+        myWallet.maybeWhen(orElse: () => 0, data: (wallet) => wallet.balance);
+    final formatter = NumberFormat("#,##0.00", "fr_FR");
+
+    final amountToAdd = double.tryParse(fundAmount.replaceAll(",", ".")) ?? 0;
+
+    final isValid = amountToAdd + currentAmount <= 80;
+
     return ClipRRect(
       borderRadius: const BorderRadius.only(
         topLeft: Radius.circular(40),
@@ -30,6 +41,27 @@ class FundPage extends ConsumerWidget {
         ),
         child: Column(
           children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Recharge',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Solde théorique : ${formatter.format(amountToAdd + currentAmount)} € (max: 80,00 €)',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
             Expanded(
               child: Center(
                 child: Row(
@@ -39,8 +71,10 @@ class FundPage extends ConsumerWidget {
                       return DigitFadeInAnimation(
                         child: Text(
                           e,
-                          style: const TextStyle(
-                            color: Colors.white,
+                          style: TextStyle(
+                            color: isValid
+                                ? Colors.white
+                                : const Color.fromARGB(255, 91, 6, 0),
                             fontSize: 50,
                             fontWeight: FontWeight.bold,
                           ),
@@ -48,10 +82,12 @@ class FundPage extends ConsumerWidget {
                       );
                     }),
                     if (fundAmount.isNotEmpty)
-                      const Text(
+                      Text(
                         ' €',
                         style: TextStyle(
-                          color: Colors.white,
+                          color: isValid
+                              ? Colors.white
+                              : const Color.fromARGB(255, 91, 6, 0),
                           fontSize: 50,
                           fontWeight: FontWeight.bold,
                         ),
