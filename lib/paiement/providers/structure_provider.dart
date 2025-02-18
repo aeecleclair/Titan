27f7d7f5ea5cd92012daf7b687a3407ca960f83d
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 18:57:32 +0100

    feat: adding structure provider

---
+++ lib/paiement/providers/structure_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myecl/paiement/class/structure.dart';
+import 'package:myecl/paiement/repositories/structures_repository.dart';
+import 'package:myecl/tools/providers/list_notifier.dart';
+
+class StructureListNotifier extends ListNotifier<Structure> {
+  final StructuresRepository structuresRepository;
+  StructureListNotifier({required this.structuresRepository})
+      : super(const AsyncValue.loading());
+
+  Future<AsyncValue<List<Structure>>> getStores() async {
+    return await loadList(structuresRepository.getStructures);
+  }
+
+  Future<bool> updateStructure(Structure structure) async {
+    return await update(
+      structuresRepository.updateStructure,
+      (structures, structure) => structures
+        ..[structures.indexWhere((s) => s.id == structure.id)] = structure,
+      structure,
+    );
+  }
+
+  Future<bool> deleteStructure(Structure structure) async {
+    return await delete(
+      (id) => structuresRepository.deleteStructure(id),
+      (structures, structure) =>
+          structures..removeWhere((s) => s.id == structure.id),
+      structure.id,
+      structure,
+    );
+  }
+
+  Future<bool> createStructure(Structure structure) async {
+    return await add(structuresRepository.createStructure, structure);
+  }
+}
+
+final structureListProvider =
+    StateNotifierProvider<StructureListNotifier, AsyncValue<List<Structure>>>(
+        (ref) {
+  final structureRepository = ref.watch(structuresRepositoryProvider);
+  final notifier =
+      StructureListNotifier(structuresRepository: structureRepository);
+  return notifier;
+});
