bc57b48580eab89b4c5eb159068f372aff223edf
Maxime Roucher <maximeroucher@gmail.com>
2025-02-18 18:44:01 +0100

    Updating pay page UI

---
+++ lib/paiement/ui/pages/pay_page/confirm_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
+import 'package:myecl/paiement/providers/paiement_page_provider.dart';
+
+class ConfirmButton extends ConsumerWidget {
+  const ConfirmButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pageNotifier = ref.read(paiementPageProvider.notifier);
+    final LocalAuthentication auth = LocalAuthentication();
+    return GestureDetector(
+      onTap: () async {
+        final bool didAuthenticate = await auth.authenticate(
+            localizedReason: 'Veuillez vous authentifier pour payer',
+            authMessages: [
+              const AndroidAuthMessages(
+                signInTitle: 'L\'authentification est requise pour payer',
+                cancelButton: 'No thanks',
+              ),
+              const IOSAuthMessages(
+                cancelButton: 'No thanks',
+              ),
+            ]);
+        if (didAuthenticate) {
+          pageNotifier.setPaiementPage(PaiementPage.pay);
+        }
+      },
+      child: Container(
+        height: 75,
+        width: 75,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 5,
+              offset: const Offset(1, 2),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: const HeroIcon(
+          HeroIcons.qrCode,
+          color: Color(0xff017f80),
+          size: 65,
+        ),
+      ),
+    );
+  }
+}
--- lib/paiement/ui/pages/pay_page/pay_page.dart
+++ lib/paiement/ui/pages/pay_page/pay_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/paiement/providers/pay_amount_provider.dart';
+import 'package:myecl/paiement/ui/pages/pay_page/confirm_button.dart';
 import 'package:myecl/paiement/ui/pages/pay_page/keyboard.dart';
 // import 'package:myecl/paiement/ui/pages/pay_page/qrcode.dart';
 
@@ -12,36 +13,62 @@ class PayPage extends ConsumerWidget {
     final payAmount = ref.watch(payAmountProvider);
     final payAmountNotifier = ref.watch(payAmountProvider.notifier);
     return Expanded(
-      child: Column(
-        children: [
-          Expanded(
+      child: Container(
+        margin: const EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [
+              Color(0xff017f80),
+              Color.fromARGB(255, 9, 103, 103),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 9, 103, 103).withOpacity(0.3),
+              blurRadius: 20,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Expanded(
               child: Center(
-            child: Text(
-              '$payAmount ${payAmount.isNotEmpty ? '€' : ''}',
-              style: const TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+                child: Text(
+                  '$payAmount ${payAmount.isNotEmpty ? '€' : ''}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
-          )),
-          NumericKeyboard(
-            onKeyboardTap: (e) {
-              if (e != "," || !payAmount.contains(",")) {
-                if (payAmount.contains(",") &&
-                    payAmount.substring(payAmount.indexOf(",")).length < 3) {
-                  payAmountNotifier.setPayAmount(payAmount + e.toString());
-                } else if (!payAmount.contains(",")) {
-                  payAmountNotifier.setPayAmount(payAmount + e.toString());
+            NumericKeyboard(
+              onKeyboardTap: (e) {
+                if (e != "," || !payAmount.contains(",")) {
+                  if (payAmount.contains(",") &&
+                      payAmount.substring(payAmount.indexOf(",")).length < 3) {
+                    payAmountNotifier.setPayAmount(payAmount + e.toString());
+                  } else if (!payAmount.contains(",")) {
+                    payAmountNotifier.setPayAmount(payAmount + e.toString());
+                  }
                 }
-              }
-            },
-            rightButtonFn: () {
-              payAmountNotifier
-                  .setPayAmount(payAmount.substring(0, payAmount.length - 1));
-            },
-          ),
-          const SizedBox(height: 70),
-        ],
+              },
+              rightButtonFn: () {
+                payAmountNotifier
+                    .setPayAmount(payAmount.substring(0, payAmount.length - 1));
+              },
+            ),
+            const Expanded(child: Center(child: ConfirmButton())),
+          ],
+        ),
       ),
     );
   }
