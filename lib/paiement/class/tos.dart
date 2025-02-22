a75bc17acb9447a72a8ff0da868833057ce7fb93
Maxime Roucher <maximeroucher@gmail.com>
2025-02-23 00:37:47 +0100

    feat: using backend max balance value

--- lib/paiement/class/tos.dart
+++ lib/paiement/class/tos.dart
@@ -2,43 +2,61 @@ class TOS {
   final int acceptedTosVersion;
   final int latestTosVersion;
   final String tosContent;
+  final int maxTransactionTotal;
+  final int maxWalletBalance;
 
   TOS({
     required this.acceptedTosVersion,
     required this.latestTosVersion,
     required this.tosContent,
+    required this.maxTransactionTotal,
+    required this.maxWalletBalance,
   });
 
   TOS.fromJson(Map<String, dynamic> json)
       : acceptedTosVersion = json['accepted_tos_version'],
         latestTosVersion = json['latest_tos_version'],
-        tosContent = json['tos_content'];
+        tosContent = json['tos_content'],
+        maxTransactionTotal = json['max_transaction_total'],
+        maxWalletBalance = json['max_wallet_balance'];
 
   Map<String, dynamic> toJson() {
     return {
       'accepted_tos_version': acceptedTosVersion,
       'latest_tos_version': latestTosVersion,
       'tos_content': tosContent,
+      'max_transaction_total': maxTransactionTotal,
+      'max_wallet_balance': maxWalletBalance,
     };
   }
 
   @override
   String toString() {
-    return 'TOS{acceptedTosVersion: $acceptedTosVersion, latestTosVersion: $latestTosVersion, tosContent: $tosContent}';
+    return 'TOS{acceptedTosVersion: $acceptedTosVersion, latestTosVersion: $latestTosVersion, tosContent: $tosContent, maxTransactionTotal: $maxTransactionTotal, maxWalletBalance: $maxWalletBalance}';
   }
 
   TOS copyWith({
     int? acceptedTosVersion,
     int? latestTosVersion,
     String? tosContent,
+    int? maxTransactionTotal,
+    int? maxWalletBalance,
   }) {
     return TOS(
       acceptedTosVersion: acceptedTosVersion ?? this.acceptedTosVersion,
       latestTosVersion: latestTosVersion ?? this.latestTosVersion,
       tosContent: tosContent ?? this.tosContent,
+      maxTransactionTotal: maxTransactionTotal ?? this.maxTransactionTotal,
+      maxWalletBalance: maxWalletBalance ?? this.maxWalletBalance,
     );
   }
 
   TOS.empty()
-      : this(acceptedTosVersion: 0, latestTosVersion: 0, tosContent: '');
+      : this(
+          acceptedTosVersion: 0,
+          latestTosVersion: 0,
+          tosContent: '',
+          maxTransactionTotal: 0,
+          maxWalletBalance: 0,
+        );
 }
--- lib/paiement/ui/pages/fund_page/fund_page.dart
+++ lib/paiement/ui/pages/fund_page/fund_page.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:myecl/paiement/providers/fund_amount_provider.dart';
 import 'package:myecl/paiement/providers/my_wallet_provider.dart';
+import 'package:myecl/paiement/providers/tos_provider.dart';
 import 'package:myecl/paiement/ui/components/digit_fade_in_animation.dart';
 import 'package:myecl/paiement/ui/components/keyboard.dart';
 import 'package:myecl/paiement/ui/pages/fund_page/confirm_button.dart';
@@ -15,13 +16,16 @@ class FundPage extends ConsumerWidget {
     final fundAmount = ref.watch(fundAmountProvider);
     final fundAmountNotifier = ref.watch(fundAmountProvider.notifier);
     final myWallet = ref.watch(myWalletProvider);
-    final currentAmount =
-        myWallet.maybeWhen(orElse: () => 0, data: (wallet) => wallet.balance / 100);
+    final tos = ref.watch(tosProvider);
+    final maxBalanceAmount = tos.maybeWhen(
+        orElse: () => 0, data: (tos) => tos.maxWalletBalance / 100);
+    final currentAmount = myWallet.maybeWhen(
+        orElse: () => 0, data: (wallet) => wallet.balance / 100);
     final formatter = NumberFormat("#,##0.00", "fr_FR");
 
     final amountToAdd = double.tryParse(fundAmount.replaceAll(",", ".")) ?? 0;
 
-    final isValid = amountToAdd + currentAmount <= 80;
+    final isValid = amountToAdd + currentAmount <= maxBalanceAmount;
 
     return ClipRRect(
       borderRadius: const BorderRadius.only(
@@ -56,7 +60,7 @@ class FundPage extends ConsumerWidget {
               height: 5,
             ),
             Text(
-              'Solde théorique : ${formatter.format(amountToAdd + currentAmount)} € (max: 80,00 €)',
+              'Solde théorique : ${formatter.format(amountToAdd + currentAmount)} € (max: ${formatter.format(maxBalanceAmount)} €)',
               style: const TextStyle(
                 color: Colors.white,
                 fontSize: 15,
