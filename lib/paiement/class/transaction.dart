4159be00ef31a81d76dddfb52c28b6a426c1a2cd
Maxime Roucher <maximeroucher@gmail.com>
2025-02-23 06:34:43 +0100

    fix: typo

--- lib/paiement/class/transaction.dart
+++ lib/paiement/class/transaction.dart
@@ -1,10 +1,11 @@
 import 'package:myecl/paiement/class/history.dart';
+import 'package:myecl/tools/functions.dart';
 
 enum TransactionType { direct, request, refund }
 
 class Transaction {
   final String id;
-  final String debitedWallerId;
+  final String debitedWalletId;
   final String creditedWalletId;
   final TransactionType transactionType;
   final String? sellerUserId;
@@ -14,7 +15,7 @@ class Transaction {
 
   Transaction({
     required this.id,
-    required this.debitedWallerId,
+    required this.debitedWalletId,
     required this.creditedWalletId,
     required this.transactionType,
     this.sellerUserId,
@@ -25,37 +26,37 @@ class Transaction {
 
   Transaction.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        debitedWallerId = json['debited_wallet_id'],
+        debitedWalletId = json['debited_wallet_id'],
         creditedWalletId = json['credited_wallet_id'],
         transactionType = TransactionType.values.firstWhere(
           (e) => e.toString().split('.').last == json['transaction_type'],
         ),
         sellerUserId = json['seller_user_id'],
         total = json['total'],
-        creation = json['creation'],
+        creation = processDateFromAPI(json['creation']),
         status = TransactionStatus.values.firstWhere(
           (e) => e.toString().split('.').last == json['status'],
         );
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'debited_wallet_id': debitedWallerId,
+        'debited_wallet_id': debitedWalletId,
         'credited_wallet_id': creditedWalletId,
         'transaction_type': transactionType.toString().split('.').last,
         'seller_user_id': sellerUserId,
         'total': total,
-        'creation': creation,
+        'creation': processDateToAPI(creation),
         'status': status.toString().split('.').last,
       };
 
   @override
   String toString() {
-    return 'Transaction {id: $id, debitedWallerId: $debitedWallerId, creditedWalletId: $creditedWalletId, transactionType: $transactionType, sellerUserId: $sellerUserId, total: $total, creation: $creation, status: $status}';
+    return 'Transaction {id: $id, debitedWalletId: $debitedWalletId, creditedWalletId: $creditedWalletId, transactionType: $transactionType, sellerUserId: $sellerUserId, total: $total, creation: $creation, status: $status}';
   }
 
   Transaction.empty()
       : id = '',
-        debitedWallerId = '',
+        debitedWalletId = '',
         creditedWalletId = '',
         transactionType = TransactionType.direct,
         sellerUserId = '',
