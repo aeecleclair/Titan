5231575fc7b041d394d75d7aca8fe8cb85fc75da
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-02-23 03:56:35 +0100

    update repo

--- lib/tools/repository/repository.dart
+++ lib/tools/repository/repository.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:titan/auth/providers/openid_provider.dart';
 import 'package:titan/tools/cache/cache_manager.dart';
 import 'package:titan/tools/exception.dart';
 import 'package:titan/tools/functions.dart';
@@ -260,6 +261,8 @@ abstract class Repository {
     final channel = WebSocketChannel.connect(
       Uri.parse('ws://172.20.10.2:8000/rplace/ws'),
     );
+    channel.sink
+        .add(jsonEncode({"token": headers["Authorization"]?.split(" ")[1]}));
     return channel;
   }
 }
