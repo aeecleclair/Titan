cc27487b81e37b9e6c7fec061a3e495c8230263f
Mickael <mickael.shonouda@etu.ec-lyon.fr>
2025-02-28 15:27:19 +0100

    fix typo

--- lib/rplace/class/pixel.dart
+++ lib/rplace/class/pixel.dart
@@ -2,17 +2,14 @@ class Pixel {
   late final int x;
   late final int y;
   late final String color;
-  //late final SimpleUser user;
 
   Pixel({
     required this.x,
     required this.y,
     required this.color,
-    //required this.user,
   });
 
   Pixel.fromJson(Map<String, dynamic> json) {
-    //user = SimpleUser.fromJson(json['user']);
     x = json['x'];
     y = json['y'];
     color = json['color'];
@@ -20,7 +17,6 @@ class Pixel {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    //data['user_id'] = user.id;
     data['x'] = x;
     data['y'] = y;
     data['color'] = color;
@@ -28,5 +24,5 @@ class Pixel {
   }
 
   @override
-  String toString() => 'Score(offset: ($x, $y), color: $color)';
+  String toString() => 'Pixel(offset: ($x, $y), color: $color)';
 }
