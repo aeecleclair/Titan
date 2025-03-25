767290281024bb1d09a3a5e980ac72cdf7ff5366
Maxime Roucher <maximeroucher@gmail.com>
2025-03-25 13:13:13 +0100

    fix: overflow

--- lib/paiement/ui/pages/devices_page/add_device_button.dart
+++ lib/paiement/ui/pages/devices_page/add_device_button.dart
@@ -10,51 +10,55 @@ class AddDeviceButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(25)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-          child: Container(
-            color: Colors.grey.shade200.withValues(alpha: 0.5),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                WaitingButton(
-                  onTap: onTap,
-                  builder: (child) => SizedBox(
-                    height: MediaQuery.of(context).size.height - 250,
-                    child: child,
-                  ),
-                  child: const Column(
-                    children: [
-                      Spacer(flex: 3),
-                      Center(
-                        child: HeroIcon(
-                          HeroIcons.plus,
-                          color: Color.fromARGB(255, 2, 2, 2),
-                          size: 65,
-                        ),
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 40),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(25)),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                color: Colors.grey.shade200.withValues(alpha: 0.5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    WaitingButton(
+                      onTap: onTap,
+                      builder: (child) => SizedBox(
+                        height: constraint.maxHeight - 250,
+                        child: child,
                       ),
-                      Spacer(),
-                      Text(
-                        "Ajouter cet appareil",
-                        style: TextStyle(
-                          color: Color(0xff204550),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: const Column(
+                        children: [
+                          Spacer(flex: 3),
+                          Center(
+                            child: HeroIcon(
+                              HeroIcons.plus,
+                              color: Color.fromARGB(255, 2, 2, 2),
+                              size: 65,
+                            ),
+                          ),
+                          Spacer(),
+                          Text(
+                            "Ajouter cet appareil",
+                            style: TextStyle(
+                              color: Color(0xff204550),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Spacer(),
+                        ],
                       ),
-                      Spacer(),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
