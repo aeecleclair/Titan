0d3ea79870a9cd2d1bcfa0cbef1b32f804bbc791
Maxime Roucher <maximeroucher@gmail.com>
2025-06-07 14:28:57 +0200

    fix: limiting web interaction with critical component

--- lib/paiement/ui/pages/devices_page/devices_page.dart
+++ lib/paiement/ui/pages/devices_page/devices_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -90,7 +91,7 @@ class DevicesPage extends HookConsumerWidget {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    if (shouldDisplayAddDevice)
+                    if (shouldDisplayAddDevice && !kIsWeb)
                       AddDeviceButton(
                         onTap: () async {
                           if (!hasAcceptedToS) {
