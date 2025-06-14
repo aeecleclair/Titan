e600706d4559bad31f46a3feb7bda1cc7ce06e40
Shaîhh <guillaume.tavernier@etu.ec-lyon.fr>
2025-06-14 23:47:38 +0200

    fix(loading): wait until the user is fetched when logged in

--- lib/routing/providers/is_app_loading_provider.dart
+++ lib/routing/providers/is_app_loading_provider.dart
@@ -1,9 +1,14 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myecl/auth/providers/openid_provider.dart';
+import 'package:myecl/user/providers/user_provider.dart';
 import 'package:myecl/version/providers/version_verifier_provider.dart';
 
 final isAppLoadingProvider = StateProvider<bool>((ref) {
   final versionVerifier = ref.watch(versionVerifierProvider);
   final isAuthLoading = ref.watch(isAuthLoadingProvider);
-  return versionVerifier.isLoading || isAuthLoading;
+  final isLoggedIn = ref.watch(isLoggedInProvider);
+  final asyncUser = ref.watch(asyncUserProvider);
+  return versionVerifier.isLoading ||
+      isAuthLoading ||
+      (isLoggedIn && asyncUser.isLoading);
 });
