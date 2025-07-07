33185d08ab2af28c51df358f6dd3e59d98be68fe
Maxime Roucher <maximeroucher@gmail.com>
2025-07-07 18:59:29 +0200

    fix: input design

--- lib/tools/ui/styleguide/text_entry.dart
+++ lib/tools/ui/styleguide/text_entry.dart
@@ -30,8 +30,8 @@ class TextEntry extends StatelessWidget {
     this.isDouble = false,
     this.keyboardType = TextInputType.text,
     this.canBeEmpty = false,
-    this.color = ColorConstants.tertiary,
-    this.enabledColor = ColorConstants.tertiary,
+    this.color = ColorConstants.onTertiary,
+    this.enabledColor = ColorConstants.onTertiary,
     this.errorColor = ColorConstants.main,
     this.noValueError = "No value",
     this.suffixIcon,
@@ -41,76 +41,84 @@ class TextEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      minLines: minLines,
-      maxLines: maxLines,
-      controller: controller,
-      keyboardType: keyboardType,
-      cursorColor: color,
-      onChanged: onChanged,
-      textInputAction: (keyboardType == TextInputType.multiline)
-          ? TextInputAction.newline
-          : TextInputAction.next,
-      enabled: enabled,
-      decoration: InputDecoration(
-        label: Text(
-          canBeEmpty ? '$label (optionnel)' : label,
-          style: TextStyle(color: color, height: 0.5),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        minLines: minLines,
+        maxLines: maxLines,
+        controller: controller,
+        keyboardType: keyboardType,
+        cursorColor: color,
+        onChanged: onChanged,
+        textInputAction: (keyboardType == TextInputType.multiline)
+            ? TextInputAction.newline
+            : TextInputAction.next,
+        enabled: enabled,
+        decoration: InputDecoration(
+          label: Text(
+            canBeEmpty ? '$label (optionnel)' : label,
+            style: TextStyle(
+              color: color,
+              height: 0.5,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          suffix: suffixIcon == null && suffix.isEmpty
+              ? null
+              : Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  child:
+                      suffixIcon ??
+                      Text(suffix, style: TextStyle(color: color)),
+                ),
+          prefix: prefix.isEmpty
+              ? null
+              : Container(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Text(prefix, style: TextStyle(color: color)),
+                ),
+          floatingLabelStyle: TextStyle(
+            color: color,
+            fontWeight: FontWeight.w900,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: enabledColor),
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: errorColor, width: 2.0),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color, width: 2.0),
+          ),
         ),
-        suffix: suffixIcon == null && suffix.isEmpty
-            ? null
-            : Container(
-                padding: const EdgeInsets.only(left: 10),
-                child:
-                    suffixIcon ?? Text(suffix, style: TextStyle(color: color)),
-              ),
-        prefix: prefix.isEmpty
-            ? null
-            : Container(
-                padding: const EdgeInsets.only(right: 10),
-                child: Text(prefix, style: TextStyle(color: color)),
-              ),
-        floatingLabelStyle: const TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-        ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: enabledColor),
-        ),
-        errorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: errorColor, width: 2.0),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: color, width: 2.0),
-        ),
-      ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          if (canBeEmpty) {
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            if (canBeEmpty) {
+              return null;
+            }
+            return noValueError;
+          }
+
+          if (isInt) {
+            final intValue = int.tryParse(value);
+            if (intValue == null || (intValue < 0 && !isNegative)) {
+              return "Invalid number";
+            }
+          }
+
+          if (isDouble) {
+            final doubleValue = double.tryParse(value.replaceAll(',', '.'));
+            if (doubleValue == null || (doubleValue < 0 && !isNegative)) {
+              return "Invalid number";
+            }
+          }
+
+          if (validator == null) {
             return null;
           }
-          return noValueError;
-        }
-
-        if (isInt) {
-          final intValue = int.tryParse(value);
-          if (intValue == null || (intValue < 0 && !isNegative)) {
-            return "Invalid number";
-          }
-        }
-
-        if (isDouble) {
-          final doubleValue = double.tryParse(value.replaceAll(',', '.'));
-          if (doubleValue == null || (doubleValue < 0 && !isNegative)) {
-            return "Invalid number";
-          }
-        }
-
-        if (validator == null) {
-          return null;
-        }
-        return validator!(value);
-      },
+          return validator!(value);
+        },
+      ),
     );
   }
 }
