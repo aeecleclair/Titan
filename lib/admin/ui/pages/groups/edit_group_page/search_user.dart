750ae09d99a8992fb02d88c78403b624bb3be244
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-07-30 00:11:22 +0200

    Remove useless import

--- lib/admin/ui/pages/groups/edit_group_page/search_user.dart
+++ lib/admin/ui/pages/groups/edit_group_page/search_user.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:titan/admin/class/group.dart';
 import 'package:titan/admin/providers/group_from_simple_group_provider.dart';
-import 'package:titan/admin/providers/group_id_provider.dart';
 import 'package:titan/admin/providers/group_provider.dart';
 import 'package:titan/admin/ui/components/user_ui.dart';
 import 'package:titan/admin/ui/pages/groups/edit_group_page/results.dart';
