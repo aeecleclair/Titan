eaca960cd7ee408b759b6d6499a856193d25c2ba
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-08-09 17:15:33 +0200

    fix : import

--- lib/login/ui/components/password_strength.dart
+++ lib/login/ui/components/password_strength.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:titan/settings/ui/pages/main_page/secure_bar.dart';
+import 'package:titan/login/ui/components/secure_bar.dart';
 import 'package:titan/tools/ui/widgets/align_left_text.dart';
 import 'package:titan/l10n/app_localizations.dart';
 
