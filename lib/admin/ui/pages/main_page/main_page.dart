5657caff229d6c0ddddd6f0f3c5e4f6bb80647fd
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-08-10 02:01:18 +0200

    remove useless imports

--- lib/admin/ui/pages/main_page/main_page.dart
+++ lib/admin/ui/pages/main_page/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 import 'package:titan/admin/admin.dart';
@@ -7,17 +6,10 @@ import 'package:titan/admin/providers/all_groups_list_provider.dart';
 import 'package:titan/admin/router.dart';
 import 'package:titan/admin/ui/pages/advertisers/load_switch_advertisers.dart';
 import 'package:titan/admin/ui/pages/users_management_page/users_management_page.dart';
-import 'package:titan/advert/class/announcer.dart';
-import 'package:titan/advert/providers/all_announcer_list_provider.dart';
-import 'package:titan/advert/providers/announcer_list_provider.dart';
 import 'package:titan/l10n/app_localizations.dart';
-import 'package:titan/tools/constants.dart';
-import 'package:titan/tools/functions.dart';
-import 'package:titan/tools/token_expire_wrapper.dart';
 import 'package:titan/tools/ui/styleguide/bottom_modal_template.dart';
 import 'package:titan/tools/ui/styleguide/list_item.dart';
 import 'package:titan/tools/ui/styleguide/list_item_template.dart';
-import 'package:titan/tools/ui/widgets/custom_dialog_box.dart';
 
 import 'package:titan/user/providers/user_list_provider.dart';
 
