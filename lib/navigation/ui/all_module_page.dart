165d58acd010806b6378eefbb440e6a106a83d94
Foucauld Bellanger <63885990+foucblg@users.noreply.github.com>
2025-08-10 14:25:25 +0200

    navbar height (#20)
    * navbar height
    
    # Conflicts:
    #	lib/navigation/ui/all_module_page.dart
    
    * fix : rebase
    
    * fix : rebase error
    
    ---------
    
    Co-authored-by: Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>

--- lib/navigation/ui/all_module_page.dart
+++ lib/navigation/ui/all_module_page.dart
@@ -12,7 +12,6 @@ import 'package:titan/tools/constants.dart';
 import 'package:titan/tools/providers/path_forwarding_provider.dart';
 import 'package:titan/tools/providers/prefered_module_root_list_provider.dart';
 import 'package:titan/tools/ui/styleguide/list_item.dart';
-import 'package:titan/tools/ui/styleguide/searchbar.dart';
 import 'package:titan/tools/ui/widgets/top_bar.dart';
 
 class AllModulePage extends HookConsumerWidget {
@@ -34,81 +33,81 @@ class AllModulePage extends HookConsumerWidget {
     );
     return Container(
       color: ColorConstants.background,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(height: 40),
-          TopBar(root: AppRouter.allModules),
-          Expanded(
-            child: ScrollToHideNavbar(
-              controller: scrollController,
-              child: SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TopBar(root: AppRouter.allModules),
+            Expanded(
+              child: ScrollToHideNavbar(
                 controller: scrollController,
-                physics: const BouncingScrollPhysics(),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      CustomSearchBar(
-                        onSearch: (String query) {},
-                        onFilter: () {},
-                      ),
-                      SizedBox(height: 30),
-                      ...modules.map(
-                        (module) => Row(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                if (preferedModuleRootList.contains(
-                                  module.root,
-                                )) {
-                                  preferedModuleRootListNotifier
-                                      .removePreferedModulesRoot(module.root);
-                                } else if (preferedModuleRootList.length < 2) {
-                                  preferedModuleRootListNotifier
-                                      .addPreferedModulesRoot(module.root);
-                                }
-                              },
-                              child: HeroIcon(
-                                HeroIcons.bookmark,
-                                style:
-                                    preferedModuleRootList.contains(module.root)
-                                    ? HeroIconStyle.solid
-                                    : HeroIconStyle.outline,
-                                size: 20,
-                                color:
-                                    preferedModuleRootList.contains(module.root)
-                                    ? ColorConstants.main
-                                    : ColorConstants.secondary,
-                              ),
-                            ),
-                            Expanded(
-                              child: ListItem(
-                                title: module.getName(context),
-                                subtitle: module.description,
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  controller: scrollController,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: [
+                        ...modules.map(
+                          (module) => Row(
+                            children: [
+                              GestureDetector(
                                 onTap: () {
-                                  navbarListModuleNotifier.pushModule(module);
-                                  final pathForwardingNotifier = ref.watch(
-                                    pathForwardingProvider.notifier,
-                                  );
-                                  pathForwardingNotifier.forward(module.root);
-
-                                  QR.to(module.root);
-                                  navbarVisibilityNotifier.show();
+                                  if (preferedModuleRootList.contains(
+                                    module.root,
+                                  )) {
+                                    preferedModuleRootListNotifier
+                                        .removePreferedModulesRoot(module.root);
+                                  } else if (preferedModuleRootList.length <
+                                      2) {
+                                    preferedModuleRootListNotifier
+                                        .addPreferedModulesRoot(module.root);
+                                  }
                                 },
+                                child: HeroIcon(
+                                  HeroIcons.bookmark,
+                                  style:
+                                      preferedModuleRootList.contains(
+                                        module.root,
+                                      )
+                                      ? HeroIconStyle.solid
+                                      : HeroIconStyle.outline,
+                                  size: 20,
+                                  color:
+                                      preferedModuleRootList.contains(
+                                        module.root,
+                                      )
+                                      ? ColorConstants.main
+                                      : ColorConstants.secondary,
+                                ),
                               ),
-                            ),
-                          ],
+                              Expanded(
+                                child: ListItem(
+                                  title: module.getName(context),
+                                  subtitle: module.description,
+                                  onTap: () {
+                                    navbarListModuleNotifier.pushModule(module);
+                                    final pathForwardingNotifier = ref.watch(
+                                      pathForwardingProvider.notifier,
+                                    );
+                                    pathForwardingNotifier.forward(module.root);
+
+                                    QR.to(module.root);
+                                    navbarVisibilityNotifier.show();
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 80),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
