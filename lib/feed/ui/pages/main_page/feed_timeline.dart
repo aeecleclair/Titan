e55276f169e9a5fe0e4995ec8bbf27c0153c2d2c
Maxime Roucher <maximeroucher@gmail.com>
2025-08-10 15:08:36 +0200

    feat: ordering news by dates

--- lib/feed/ui/pages/main_page/feed_timeline.dart
+++ lib/feed/ui/pages/main_page/feed_timeline.dart
@@ -7,10 +7,24 @@ class FeedTimeline extends StatelessWidget {
   final Function(News item)? onItemTap;
   final bool isAdmin;
 
-  const FeedTimeline({super.key, required this.items, this.onItemTap, required this.isAdmin});
+  const FeedTimeline({
+    super.key,
+    required this.items,
+    this.onItemTap,
+    required this.isAdmin,
+  });
 
   @override
   Widget build(BuildContext context) {
+    items.sort((a, b) {
+      if (a.start == b.start) {
+        if (a.end == null && b.end == null) return 0;
+        if (a.end == null) return -1;
+        if (b.end == null) return 1;
+        return a.end!.compareTo(b.end!);
+      }
+      return a.start.compareTo(b.start);
+    });
     return Column(
       children: [
         ...items.map(
