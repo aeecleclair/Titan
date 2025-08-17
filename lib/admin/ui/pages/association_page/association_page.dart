df098c305d08e5e375b626fca8c5d4f1662e50d6
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-08-17 11:53:43 +0200

    Association adding part.1

--- lib/admin/ui/pages/association_page/association_page.dart
+++ lib/admin/ui/pages/association_page/association_page.dart
@@ -1,11 +1,63 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:titan/admin/admin.dart';
+import 'package:titan/admin/providers/all_groups_list_provider.dart';
+import 'package:titan/admin/providers/assocation_list_provider.dart';
+import 'package:titan/admin/ui/pages/association_page/add_association_modal.dart';
+import 'package:titan/tools/ui/builders/async_child.dart';
+import 'package:titan/tools/ui/styleguide/bottom_modal_template.dart';
+import 'package:titan/tools/ui/styleguide/icon_button.dart';
 
-class AssociationPage extends StatelessWidget {
+class AssociationPage extends ConsumerWidget {
   const AssociationPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return AdminTemplate(child: Text("yo"));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final associationList = ref.watch(associationListProvider);
+    final groups = ref.watch(allGroupList);
+    return AdminTemplate(
+      child: AsyncChild(
+        value: associationList,
+        builder: (BuildContext context, associationList) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Association",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const Spacer(),
+                    CustomIconButton(
+                      icon: HeroIcon(
+                        HeroIcons.plus,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () async {
+                        await showCustomBottomModal(
+                          context: context,
+                          ref: ref,
+                          modal: AddAssociationModal(
+                            groups: groups,
+                            onSubmit: (group, name) {},
+                            ref: ref,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                ...associationList.map((association) => Text(association.name)),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
