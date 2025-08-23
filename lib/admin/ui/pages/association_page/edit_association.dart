1bfbdcc5f77f7b47788487c5cddd4fb8347e2df1
Maxime Roucher <maximeroucher@gmail.com>
2025-08-24 00:20:20 +0200

    feat: association pages ui

--- lib/admin/ui/pages/association_page/edit_association.dart
+++ lib/admin/ui/pages/association_page/edit_association.dart
@@ -45,175 +45,176 @@ class EditAssociation extends HookConsumerWidget {
     final localizeWithContext = AppLocalizations.of(context)!;
     final navigatorWithContext = Navigator.of(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            if (View.of(context).viewInsets.bottom == 0)
-              AsyncChild(
-                value: associationLogo,
-                builder: (context, associationLogo) {
-                  return Center(
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withValues(alpha: 0.1),
-                                spreadRadius: 5,
-                                blurRadius: 10,
-                                offset: const Offset(2, 3),
-                              ),
-                            ],
-                          ),
-                          child: CircleAvatar(
-                            radius: 80,
-                            backgroundImage: Image(
-                              image: associationLogo.image,
-                            ).image,
-                          ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          if (View.of(context).viewInsets.bottom == 0)
+            AsyncChild(
+              value: associationLogo,
+              builder: (context, associationLogo) {
+                return Center(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.1),
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                              offset: const Offset(2, 3),
+                            ),
+                          ],
                         ),
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          child: GestureDetector(
-                            onTap: () async {
-                              final value = await associationLogoNotifier
-                                  .setLogo(ImageSource.gallery, association.id);
-                              if (value != null) {
-                                if (value) {
-                                  displayToastWithContext(
-                                    TypeMsg.msg,
-                                    localizeWithContext
-                                        .adminUpdatedAssociationLogo,
-                                  );
-                                } else {
-                                  displayToastWithContext(
-                                    TypeMsg.error,
-                                    localizeWithContext.adminTooHeavyLogo,
-                                  );
-                                }
+                        child: CircleAvatar(
+                          radius: 80,
+                          backgroundImage: Image(
+                            image: associationLogo.image,
+                          ).image,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        child: GestureDetector(
+                          onTap: () async {
+                            final value = await associationLogoNotifier.setLogo(
+                              ImageSource.gallery,
+                              association.id,
+                            );
+                            if (value != null) {
+                              if (value) {
+                                displayToastWithContext(
+                                  TypeMsg.msg,
+                                  localizeWithContext
+                                      .adminUpdatedAssociationLogo,
+                                );
                               } else {
                                 displayToastWithContext(
                                   TypeMsg.error,
-                                  localizeWithContext
-                                      .adminFailedToUpdateAssociationLogo,
+                                  localizeWithContext.adminTooHeavyLogo,
                                 );
                               }
-                            },
-                            child: const PictureButton(icon: HeroIcons.photo),
-                          ),
+                            } else {
+                              displayToastWithContext(
+                                TypeMsg.error,
+                                localizeWithContext
+                                    .adminFailedToUpdateAssociationLogo,
+                              );
+                            }
+                          },
+                          child: const PictureButton(icon: HeroIcons.photo),
                         ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: GestureDetector(
-                            onTap: () async {
-                              final value = await associationLogoNotifier
-                                  .setLogo(ImageSource.camera, association.id);
-                              if (value != null) {
-                                if (value) {
-                                  displayToastWithContext(
-                                    TypeMsg.msg,
-                                    localizeWithContext
-                                        .adminUpdatedAssociationLogo,
-                                  );
-                                } else {
-                                  displayToastWithContext(
-                                    TypeMsg.error,
-                                    localizeWithContext.adminTooHeavyLogo,
-                                  );
-                                }
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: GestureDetector(
+                          onTap: () async {
+                            final value = await associationLogoNotifier.setLogo(
+                              ImageSource.camera,
+                              association.id,
+                            );
+                            if (value != null) {
+                              if (value) {
+                                displayToastWithContext(
+                                  TypeMsg.msg,
+                                  localizeWithContext
+                                      .adminUpdatedAssociationLogo,
+                                );
                               } else {
                                 displayToastWithContext(
                                   TypeMsg.error,
-                                  localizeWithContext
-                                      .adminFailedToUpdateAssociationLogo,
+                                  localizeWithContext.adminTooHeavyLogo,
                                 );
                               }
-                            },
-                            child: const PictureButton(icon: HeroIcons.camera),
-                          ),
+                            } else {
+                              displayToastWithContext(
+                                TypeMsg.error,
+                                localizeWithContext
+                                    .adminFailedToUpdateAssociationLogo,
+                              );
+                            }
+                          },
+                          child: const PictureButton(icon: HeroIcons.camera),
                         ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            SizedBox(height: View.of(context).viewInsets.bottom == 0 ? 30 : 10),
-            TextEntry(
-              label: localizeWithContext.adminAssociationName,
-              controller: nameController,
-            ),
-            SizedBox(height: 20),
-            ListItem(
-              title: localizeWithContext.adminManagerGroup(
-                chosenGroup.value!.name,
-              ),
-              onTap: () async {
-                FocusScope.of(context).unfocus();
-                final ctx = context;
-                await Future.delayed(Duration(milliseconds: 150));
-                if (!ctx.mounted) return;
-
-                await showCustomBottomModal(
-                  context: ctx,
-                  ref: ref,
-                  modal: BottomModalTemplate(
-                    title: localizeWithContext.adminChooseGroup,
-                    child: Column(
-                      children: [
-                        ...groups.map(
-                          (e) => ListItem(
-                            title: e.name,
-                            onTap: () {
-                              chosenGroup.value = e;
-                              Navigator.of(ctx).pop();
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 );
               },
             ),
-            SizedBox(height: 20),
-            Button(
-              text: localizeWithContext.globalConfirm,
-              disabled:
-                  !(nameController.value.text != association.name ||
-                      chosenGroup.value!.id != association.groupId),
-              onPressed: () async {
-                await tokenExpireWrapper(ref, () async {
-                  final newAssociation = association.copyWith(
-                    name: nameController.value.text,
-                    groupId: chosenGroup.value!.id,
-                  );
-                  final value = await associationListNotifier.updateAssociation(
-                    newAssociation,
-                  );
-                  if (value) {
-                    displayToastWithContext(
-                      TypeMsg.msg,
-                      localizeWithContext.adminAssociationUpdated,
-                    );
-                    navigatorWithContext.pop();
-                  } else {
-                    displayToastWithContext(
-                      TypeMsg.error,
-                      localizeWithContext.adminAssociationUpdateError,
-                    );
-                  }
-                });
-              },
+          SizedBox(height: View.of(context).viewInsets.bottom == 0 ? 30 : 10),
+          TextEntry(
+            label: localizeWithContext.adminAssociationName,
+            controller: nameController,
+          ),
+          SizedBox(height: 20),
+          ListItem(
+            title: localizeWithContext.adminManagerGroup(
+              chosenGroup.value!.name,
             ),
-          ],
-        ),
+            onTap: () async {
+              FocusScope.of(context).unfocus();
+              final ctx = context;
+              await Future.delayed(Duration(milliseconds: 150));
+              if (!ctx.mounted) return;
+
+              await showCustomBottomModal(
+                context: ctx,
+                ref: ref,
+                modal: BottomModalTemplate(
+                  title: localizeWithContext.adminChooseGroup,
+                  child: Column(
+                    children: [
+                      ...groups.map(
+                        (e) => ListItem(
+                          title: e.name,
+                          onTap: () {
+                            chosenGroup.value = e;
+                            Navigator.of(ctx).pop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 20),
+          Button(
+            text: localizeWithContext.globalConfirm,
+            disabled:
+                !(nameController.value.text != association.name ||
+                    chosenGroup.value!.id != association.groupId),
+            onPressed: () async {
+              await tokenExpireWrapper(ref, () async {
+                final newAssociation = association.copyWith(
+                  name: nameController.value.text,
+                  groupId: chosenGroup.value!.id,
+                );
+                final value = await associationListNotifier.updateAssociation(
+                  newAssociation,
+                );
+                if (value) {
+                  displayToastWithContext(
+                    TypeMsg.msg,
+                    localizeWithContext.adminAssociationUpdated,
+                  );
+                  navigatorWithContext.pop();
+                } else {
+                  displayToastWithContext(
+                    TypeMsg.error,
+                    localizeWithContext.adminAssociationUpdateError,
+                  );
+                }
+              });
+            },
+          ),
+        ],
       ),
     );
   }
