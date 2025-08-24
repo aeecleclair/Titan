6d967a36e45aa86d6809fdbf892bbe2e86680044
Thonyk <timr.web@free.fr>
2025-08-25 00:46:18 +0200

    fix: clean up

--- lib/login/class/screen_shot.dart
+++ lib/login/class/screen_shot.dart
@@ -1,11 +1,6 @@
 class ScreenShot {
   final String path;
-  final String description;
   final String title;
 
-  ScreenShot({
-    required this.path,
-    required this.description,
-    required this.title,
-  });
+  ScreenShot({required this.path, required this.title});
 }
