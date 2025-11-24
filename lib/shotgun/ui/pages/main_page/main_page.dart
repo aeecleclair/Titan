82ebc1b33bd43130e08936384c6db238638c5e10
Rayan <rbelahcene10@gmail.com>
2025-11-24 19:28:55 +0100

    wip

--- lib/shotgun/ui/pages/main_page/main_page.dart
+++ lib/shotgun/ui/pages/main_page/main_page.dart
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:heroicons/heroicons.dart';
-import 'package:titan/booking/providers/is_admin_provider.dart';
-=======
 import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
@@ -14,20 +7,15 @@ import 'package:titan/booking/providers/is_admin_provider.dart';
 import 'package:titan/shotgun/ui/components/shotgun_card.dart';
 import 'package:titan/shotgun/ui/shotgun.dart';
 import 'package:titan/tools/ui/builders/async_child.dart';
->>>>>>> Stashed changes
 import 'package:titan/tools/ui/widgets/admin_button.dart';
 import 'package:titan/tools/ui/widgets/top_bar.dart';
 import 'package:titan/tools/ui/layouts/column_refresher.dart';
 import 'package:titan/shotgun/router.dart';
 import 'package:titan/shotgun/providers/is_shotgun_admin_provider.dart';
-<<<<<<< Updated upstream
-import 'package:titan/shotgun/ui/pages/main_page/main_page.dart';
-=======
 import 'package:titan/shotgun/providers/shotgun_list_provider.dart';
 import 'package:titan/shotgun/providers/shotgun_provider.dart';
 import 'package:titan/shotgun/ui/pages/main_page/main_page.dart';
 import 'package:titan/shotgun/ui/components/announcer_bar.dart';
->>>>>>> Stashed changes
 import 'package:titan/shotgun/tools/constants.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
@@ -38,8 +26,6 @@ class ShotgunMainPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // final isShotgunAdmin = ref.watch(isShotgunAdminProvider);
     final isAdmin = ref.watch(isAdminProvider);
-<<<<<<< Updated upstream
-=======
 
     final shotgunNotifier = ref.watch(shotgunProvider.notifier);
     final shotgunList = ref.watch(shotgunListProvider);
@@ -47,29 +33,11 @@ class ShotgunMainPage extends HookConsumerWidget {
     final selected = ref.watch(announcerProvider);
     final selectedNotifier = ref.watch(announcerProvider.notifier);
 
->>>>>>> Stashed changes
     double width = 300;
     double height = 300;
     double imageHeight = 175;
     double maxHeight = MediaQuery.of(context).size.height - 344;
 
-<<<<<<< Updated upstream
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          TopBar(title: 'Shotgun', root: ShotgunRouter.root),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: width,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-=======
     return ShotgunTemplate(
       child: Stack(
         children: [
@@ -95,7 +63,7 @@ class ShotgunMainPage extends HookConsumerWidget {
                     width: 300,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
->>>>>>> Stashed changes
+
                       children: [
                         if (isAdmin)
                           AdminButton(
@@ -103,37 +71,6 @@ class ShotgunMainPage extends HookConsumerWidget {
                               QR.to(ShotgunRouter.root + ShotgunRouter.admin);
                             },
                           ),
-<<<<<<< Updated upstream
-                        // if (isShotgunAdmin)
-                        //   AdminButton(
-                        //     onTap: () {
-                        //       QR.to(
-                        //         ShotgunRouter.root +
-                        //             ShotgunRouter.addEditMember,
-                        //       );
-                        //     },
-                        //     text: ShotgunTextConstants.management,
-                        //   ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            ShotgunTextConstants.news,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-=======
                       ],
                     ),
                   ),
@@ -152,28 +89,18 @@ class ShotgunMainPage extends HookConsumerWidget {
                           QR.to(ShotgunRouter.root + ShotgunRouter.admin);
                         },
                         shotgun: shotgun,
->>>>>>> Stashed changes
                       ),
                     ),
                   ),
                 ],
-<<<<<<< Updated upstream
-              ),
-            ),
-          ),
-        ],
-=======
               );
             },
           ),
-        ],  
->>>>>>> Stashed changes
+        ],
       ),
     );
   }
 }
-<<<<<<< Updated upstream
-=======
 //     SafeArea(
 //       child: Column(
 //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -232,4 +159,3 @@ class ShotgunMainPage extends HookConsumerWidget {
 //     );
 //   }
 // }
->>>>>>> Stashed changes
