5548f69ec49bf276590459e58adb71441e924d01
Rayan <rbelahcene10@gmail.com>
2025-11-30 09:17:33 +0100

    add : button to show/hide revoked devices

--- lib/paiement/ui/pages/devices_page/device_item.dart
+++ lib/paiement/ui/pages/devices_page/device_item.dart
@@ -21,55 +21,68 @@ class DeviceItem extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(15)),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-          child: Container(
-            color: Colors.grey.shade200.withValues(alpha: 0.5),
-            child: SizedBox(
-              height: isActual ? 80 : 70,
-              child: Row(
-                children: [
-                  const SizedBox(width: 20),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        device.name,
-                        style: const TextStyle(
-                          color: Color(0xff204550),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      if (isActual)
-                        const Text(
-                          '(cet appareil)',
-                          style: TextStyle(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.1),
+              spreadRadius: 1,
+              blurRadius: 8,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: Container(
+              color: Colors.grey.shade200.withValues(alpha: 0.5),
+              child: SizedBox(
+                height: isActual ? 80 : 70,
+                child: Row(
+                  children: [
+                    const SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          device.name,
+                          style: const TextStyle(
                             color: Color(0xff204550),
-                            fontSize: 15,
+                            fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                    ],
-                  ),
-                  const SizedBox(width: 10),
-                  Spacer(),
-                  getStatusTag(device.status),
-                  if (device.status != WalletDeviceStatus.revoked) ...[
-                    const SizedBox(width: 20),
-                    GestureDetector(
-                      onTap: onRevoke,
-                      child: const HeroIcon(
-                        HeroIcons.trash,
-                        size: 25,
-                        color: Color(0xff204550),
-                      ),
+                        if (isActual)
+                          const Text(
+                            '(cet appareil)',
+                            style: TextStyle(
+                              color: Color(0xff204550),
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                      ],
                     ),
+                    const SizedBox(width: 10),
+                    Spacer(),
+                    getStatusTag(device.status),
+                    if (device.status != WalletDeviceStatus.revoked) ...[
+                      const SizedBox(width: 20),
+                      GestureDetector(
+                        onTap: onRevoke,
+                        child: const HeroIcon(
+                          HeroIcons.trash,
+                          size: 25,
+                          color: Color(0xff204550),
+                        ),
+                      ),
+                    ],
+                    const SizedBox(width: 20),
                   ],
-                  const SizedBox(width: 20),
-                ],
+                ),
               ),
             ),
           ),
--- lib/paiement/ui/pages/devices_page/devices_page.dart
+++ lib/paiement/ui/pages/devices_page/devices_page.dart
@@ -4,6 +4,7 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:titan/paiement/class/create_device.dart';
 import 'package:titan/paiement/class/wallet_device.dart';
@@ -19,6 +20,7 @@ import 'package:titan/paiement/ui/paiement.dart';
 import 'package:titan/tools/functions.dart';
 import 'package:titan/tools/token_expire_wrapper.dart';
 import 'package:titan/tools/ui/builders/async_child.dart';
+import 'package:titan/tools/ui/layouts/button.dart';
 import 'package:titan/tools/ui/layouts/refresher.dart';
 import 'package:titan/tools/ui/widgets/custom_dialog_box.dart';
 
@@ -34,6 +36,7 @@ class DevicesPage extends HookConsumerWidget {
     final deviceKey = keyService.getKeyId();
     final displayAddDevice = useState(true);
     final hasAcceptedToS = ref.watch(hasAcceptedTosProvider);
+    final showRovokedDevices = useState(false);
 
     void displayToastWithContext(TypeMsg type, String msg) {
       displayToast(context, type, msg);
@@ -61,32 +64,39 @@ class DevicesPage extends HookConsumerWidget {
             return AsyncChild(
               value: devices,
               builder: (context, devices) {
-                final sortedDevices = devices.toList()
-                  ..sort((a, b) {
-                    if (a.id == snapshot.data) return -1;
-                    if (b.id == snapshot.data) return 1;
-                    return statusOrder(
-                      a.status,
-                    ).compareTo(statusOrder(b.status));
-                  });
+                final activeDevices = devices
+                    .where(
+                      (device) => device.status != WalletDeviceStatus.revoked,
+                    )
+                    .toList();
 
-                final firstDevice =
-                    devices.map((e) => e.id).contains(snapshot.data)
-                    ? devices
-                          .where((element) => element.id == snapshot.data)
-                          .first
-                    : null;
+                final inactiveDevices = devices
+                    .where(
+                      (device) => device.status == WalletDeviceStatus.revoked,
+                    )
+                    .toList();
+
+                activeDevices.sort((a, b) {
+                  if (a.id == snapshot.data) return -1;
+                  if (b.id == snapshot.data) return 1;
+                  return statusOrder(a.status).compareTo(statusOrder(b.status));
+                });
+
+                inactiveDevices.sort((a, b) {
+                  if (a.id == snapshot.data) return -1;
+                  if (b.id == snapshot.data) return 1;
+                  return statusOrder(a.status).compareTo(statusOrder(b.status));
+                });
+
+                final firstDevice = devices
+                    .where((element) => element.id == snapshot.data)
+                    .firstOrNull;
 
                 final shouldDisplayAddDevice =
                     (snapshot.data == null ||
                         firstDevice == null ||
                         firstDevice.status == WalletDeviceStatus.revoked) &&
                     displayAddDevice.value;
-
-                if (firstDevice != null) {
-                  sortedDevices.remove(firstDevice);
-                  sortedDevices.insert(0, firstDevice);
-                }
 
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -126,7 +136,7 @@ class DevicesPage extends HookConsumerWidget {
                                     title:
                                         'Demande d\'activation de l\'appareil',
                                     descriptions:
-                                        "La demande d'activation est prise en compte, veuilliez consulter votre boite mail pour finaliser la démarche",
+                                        "La demande d'activation est prise en compte, veuillez consulter votre boite mail pour finaliser la démarche",
                                     buttonText: "Ok",
                                     onClick: () {
                                       Navigator.of(context).pop();
@@ -138,7 +148,8 @@ class DevicesPage extends HookConsumerWidget {
                           }
                         },
                       ),
-                    ...sortedDevices.map((device) {
+                    // Afficher les appareils actifs
+                    ...activeDevices.map((device) {
                       return DeviceItem(
                         device: device,
                         isActual: device.id == snapshot.data,
@@ -189,6 +200,124 @@ class DevicesPage extends HookConsumerWidget {
                         },
                       );
                     }),
+
+                    if (inactiveDevices.isNotEmpty) ...[
+                      GestureDetector(
+                        onTap: () {
+                          showRovokedDevices.value = !showRovokedDevices.value;
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            bottom: 10,
+                            top: 20,
+                            left: 30,
+                            right: 30,
+                          ),
+                          width: double.infinity,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            gradient: RadialGradient(
+                              colors: const [
+                                Color.fromARGB(255, 9, 103, 103),
+                                Color(0xff017f80),
+                                Color.fromARGB(255, 4, 84, 84),
+                              ],
+                              center: Alignment.topLeft,
+                              radius: 1.5,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0xff017f80).withValues(alpha: 0.2),
+                                spreadRadius: 1,
+                                blurRadius: 3,
+                                offset: const Offset(
+                                  0,
+                                  1,
+                                ), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              showRovokedDevices.value
+                                  ? 'Masquer les appareils revoqués'
+                                  : 'Afficher les appareils revoqués',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      if (showRovokedDevices.value) ...[
+                        const Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            'Appareils révoqués',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        ...inactiveDevices.map((device) {
+                          return DeviceItem(
+                            device: device,
+                            isActual: device.id == snapshot.data,
+                            onRevoke: () async {
+                              if (!hasAcceptedToS) {
+                                displayToastWithContext(
+                                  TypeMsg.error,
+                                  "Veuillez accepter les Conditions Générales d'Utilisation.",
+                                );
+                                return;
+                              }
+                              await showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return CustomDialogBox(
+                                    title: "Révoquer l'appareil ?",
+                                    descriptions:
+                                        "Vous ne pourrez plus utiliser cet appareil pour les paiements",
+                                    onYes: () async {
+                                      tokenExpireWrapper(ref, () async {
+                                        final value = await devicesNotifier
+                                            .revokeDevice(
+                                              device.copyWith(
+                                                status:
+                                                    WalletDeviceStatus.revoked,
+                                              ),
+                                            );
+                                        if (value) {
+                                          displayToastWithContext(
+                                            TypeMsg.msg,
+                                            "Appareil révoqué",
+                                          );
+                                          final savedId = await keyService
+                                              .getKeyId();
+                                          if (savedId == device.id) {
+                                            await keyService.clear();
+                                          }
+                                        } else {
+                                          displayToastWithContext(
+                                            TypeMsg.error,
+                                            "Erreur lors de la révocation de l'appareil",
+                                          );
+                                        }
+                                      });
+                                    },
+                                  );
+                                },
+                              );
+                            },
+                          );
+                        }),
+                      ],
+                    ],
                   ],
                 );
               },
