f2dc8662fbb168e97d975a685b959c5ff3e07f84
maximeroucher <maximeroucher@gmail.com>
2026-04-17 00:09:15 +0200

    feat: casting user store to store

--- lib/mypayment/providers/stores_list_provider.dart
+++ lib/mypayment/providers/stores_list_provider.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:titan/admin/class/assocation.dart';
+import 'package:titan/admin/providers/assocation_list_provider.dart';
 import 'package:titan/mypayment/class/store.dart';
 import 'package:titan/mypayment/class/structure.dart';
+import 'package:titan/mypayment/class/user_store.dart';
 import 'package:titan/mypayment/repositories/stores_repository.dart';
 import 'package:titan/mypayment/repositories/structures_repository.dart';
 import 'package:titan/mypayment/repositories/users_me_repository.dart';
@@ -10,15 +13,34 @@ class StoreListNotifier extends ListNotifier<Store> {
   final StoresRepository storesRepository;
   final StructuresRepository structureRepository;
   final UsersMeRepository usersMeRepository;
+  final List<Association> associations;
   StoreListNotifier({
     required this.storesRepository,
     required this.structureRepository,
     required this.usersMeRepository,
+    required this.associations,
   }) : super(const AsyncValue.loading());
+
+  Store _toStore(UserStore us) {
+    return Store(
+      id: us.id,
+      name: us.name,
+      walletId: us.walletId,
+      structure: us.structure,
+      association: us.associationId == null
+          ? Association.empty()
+          : associations.firstWhere(
+              (a) => a.id == us.associationId,
+              orElse: () => Association.empty(),
+            ),
+    );
+  }
 
   Future<AsyncValue<List<Store>>> getStores() async {
     return await loadList(
-      () async => List<Store>.from(await usersMeRepository.getMyStores()),
+      () async => (await usersMeRepository.getMyStores())
+          .map(_toStore)
+          .toList(),
     );
   }
 
@@ -53,9 +75,12 @@ final storeListProvider =
       final storeListRepository = ref.watch(storesRepositoryProvider);
       final structureRepository = ref.watch(structuresRepositoryProvider);
       final usersMeRepository = ref.watch(usersMeRepositoryProvider);
+      final associations =
+          ref.watch(associationListProvider).asData?.value ?? const [];
       return StoreListNotifier(
         storesRepository: storeListRepository,
         structureRepository: structureRepository,
         usersMeRepository: usersMeRepository,
+        associations: associations,
       )..getStores();
     });
