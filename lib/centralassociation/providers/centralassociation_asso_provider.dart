bf306fd6d5a9ca786ba971137d09eab202bda279
maximeroucher <maximeroucher@gmail.com>
2026-06-26 09:09:37 +0200

    feat: migrating centralassociation

--- lib/centralassociation/providers/centralassociation_asso_provider.dart
+++ lib/centralassociation/providers/centralassociation_asso_provider.dart
@@ -3,27 +3,26 @@ import 'package:titan/centralassociation/class/link.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:titan/centralassociation/repositories/asso_repository.dart';
 import 'package:titan/tools/providers/list_notifier.dart';
-import 'package:titan/tools/token_expire_wrapper.dart';
 
 class AssoNotifier extends ListNotifier<Asso> {
-  AssoRepository assoRepository = AssoRepository();
-  AssoNotifier() : super(const AsyncValue.loading());
+  final AssoRepository assoRepository = AssoRepository();
 
-  late List<Asso> allAssos = [];
-  late List<Link> allLinks = [];
+  List<Asso> allAssos = [];
+  List<Link> allLinks = [];
 
-  Future initState() async {
+  @override
+  AsyncValue<List<Asso>> build() {
+    initState();
+    return const AsyncValue.loading();
+  }
+
+  Future<void> initState() async {
     allAssos = await assoRepository.getAssoList();
     allLinks = allAssos.expand((element) => element.linkList).toList();
     state = AsyncValue.data(allAssos);
   }
 }
 
-final assoProvider =
-    StateNotifierProvider<AssoNotifier, AsyncValue<List<Asso>>>((ref) {
-      AssoNotifier notifier = AssoNotifier();
-      tokenExpireWrapperAuth(ref, () async {
-        await notifier.initState();
-      });
-      return notifier;
-    });
+final assoProvider = NotifierProvider<AssoNotifier, AsyncValue<List<Asso>>>(
+  AssoNotifier.new,
+);
